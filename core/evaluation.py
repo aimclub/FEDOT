90c61f537c98bb62c08aaa361d37b1abe6cee1ed
Irina <irlania@mail.ru>
2020-02-05 13:57:52 +0300

    modified logreg fit, add test model metrics

--- core/evaluation.py
+++ core/evaluation.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from sklearn.linear_model import LogisticRegression as SklearnLogReg
+from sklearn import preprocessing
 import numpy as np
 from core.datastream import DataStream
 
@@ -12,18 +13,17 @@ class EvaluationStrategy(ABC):
 
 class LogRegression(EvaluationStrategy):
     def __init__(self, seed=None):
-        self.model = SklearnLogReg(random_state=seed, solver='sag', max_iter=10000)
-        self.model_fit = None
+        self.model = SklearnLogReg(random_state=seed, solver='liblinear', max_iter=10000)
         self.model_predict = None
 
     def evaluate(self, data: DataStream) -> np.array:
-        train_data_x, train_data_y, test_data_x = test_train_data_setup(data)
+        train_data_x, train_data_y, test_data_x = train_test_data_setup(data)
         self.fit(train_data_x, train_data_y)
         return self.predict(test_data_x)
 
     def fit(self, fit_data_x: np.array, fit_data_y: np.array):
-        self.model_fit = self.model.fit(fit_data_x, fit_data_y)
-        print('Model fit: ', self.model_fit)
+        self.model.fit(fit_data_x, fit_data_y)
+        print('Model fit: ', self.model)
 
     def predict(self, data: np.array):
         self.model_predict = self.model.predict(data)
@@ -59,12 +59,17 @@ class XGBoost(EvaluationStrategy):
         return 'XGBoostPredict'
 
 
-def test_train_data_setup(data: DataStream):
-    train_data_x, test_data_x = splitted_train_test(data.x)
-    train_data_y, _ = splitted_train_test(data.y)
-    return train_data_x, train_data_y, test_data_x
+def train_test_data_setup(data: DataStream):
+    train_data_x, test_data_x = split_train_test(data.x)
+    train_data_y, _ = split_train_test(data.y)
+    return normalize(train_data_x), train_data_y, normalize(test_data_x)
 
 
-def splitted_train_test(data, split_ratio=0.8):
+def split_train_test(data, split_ratio=0.8):
     split_point = int(len(data) * split_ratio)
     return data[:split_point], data[split_point:]
+
+
+def normalize(x):
+    """Normalize data with sklearn.preprocessing.scale()"""
+    return preprocessing.scale(x)
--- test/test_node.py
+++ test/test_node.py
@@ -1,20 +1,28 @@
 import pytest
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
-from core.evaluation import (LinRegression, LogRegression, XGBoost, splitted_train_test)
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import cross_val_score
+from core.evaluation import (LinRegression, LogRegression, XGBoost, split_train_test,
+                             normalize)
 from core.node import (NodeFactory, OperationNode, ModelNode, )
 from core.datastream import DataStream
+import numpy as np
 
 
 @pytest.fixture()
 def data_setup():
-    test_data = load_iris()
-    predictors = test_data['data']
-    response = test_data['target']
-    train_data_x, test_data_x = splitted_train_test(predictors)
-    train_data_y, test_data_y = splitted_train_test(response)
+    predictors, response = load_iris(return_X_y=True)
+    np.random.seed(1)
+    np.random.shuffle(predictors)
+    np.random.shuffle(response)
+    response = response[:100]
+    predictors = predictors[:100]
+    train_data_x, test_data_x = split_train_test(predictors)
+    train_data_y, test_data_y = split_train_test(response)
     data_stream = DataStream(x=predictors, y=response)
-    return train_data_x, train_data_y, test_data_x, test_data_y, data_stream
+    return normalize(train_data_x), train_data_y, normalize(
+        test_data_x), test_data_y, data_stream
 
 
 def test_node_log_reg():
@@ -33,13 +41,19 @@ def test_node_xgboost():
     assert test_node.eval_strategy.__class__ == XGBoost
 
 
-def test_eval_strategy_log(data_setup):
-    train_data_x, train_data_y, test_data_x, expected_y, data_stream = data_setup
-    test_skl_model = LogisticRegression(random_state=1)
+def test_eval_strategy_logreg(data_setup):
+    train_data_x, train_data_y, test_data_x, true_y, data_stream = data_setup
+    test_skl_model = LogisticRegression(C=10., random_state=1, solver='liblinear',
+                                        max_iter=10000, verbose=0)
     test_skl_model.fit(train_data_x, train_data_y)
     expected_result = test_skl_model.predict(test_data_x)
 
     test_model_node = ModelNode(nodes_from=None, nodes_to=None, data_stream=data_stream,
                                 eval_strategy=LogRegression(seed=1))
     actual_result = test_model_node.apply()
+    print('\n', '#test_eval_strategy_logreg_log')
+    print(classification_report(true_y, actual_result,
+                                target_names=['Class 0', 'Class 1', 'Class 2']))
+    print('Test model accuracy: ', accuracy_score(true_y, actual_result))
+    print('Test model score', test_skl_model.score(train_data_x, train_data_y))
     assert actual_result.all() == expected_result.all()
