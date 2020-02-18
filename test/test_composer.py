a734d2d79c423aeb5e5903a811d7d2b52a746837
Nikolay Nikitin <nicl.nno@gmail.com>
2020-02-18 19:21:13 +0300

    Test extended

--- test/test_composer.py
+++ test/test_composer.py
@@ -1,5 +1,6 @@
 from core.composer.composer import DummyComposer
 from core.model import XGBoost, LogRegression
+from core.node import PrimaryNode, SecondaryNode
 
 
 def test_composer():
@@ -10,3 +11,9 @@ def test_composer():
                                        metrics=None)
 
     assert len(new_chain.nodes) == 3
+    assert isinstance(new_chain.nodes[0], PrimaryNode)
+    assert isinstance(new_chain.nodes[1], PrimaryNode)
+    assert isinstance(new_chain.nodes[2], SecondaryNode)
+    assert new_chain.nodes[2].nodes_from[0] is new_chain.nodes[0]
+    assert new_chain.nodes[2].nodes_from[1] is new_chain.nodes[1]
+    assert new_chain.nodes[1].nodes_from is None
