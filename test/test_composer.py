9263b269f4851e1356a9cdc46b027ef65d3d06e9
YanaPolonskaya <yanapolonskaya1413@yandex.ru>
2020-03-13 18:27:04 +0300

    test for functions of getting node depth and high was added

--- test/test_composer.py
+++ test/test_composer.py
@@ -191,7 +191,7 @@ def test_gp_composer_quality(data_fixture, request):
     composer_requirements = GPComposerRequirements(
         primary=models_impl,
         secondary=models_impl, max_arity=2,
-        max_depth=3, pop_size=10, num_of_generations=10,
+        max_depth=3, pop_size=2, num_of_generations=2,
         crossover_prob=0.8, mutation_prob=0.8)
 
     # Create GP-based composer
