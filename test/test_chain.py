e052c79a0f34c24aaac0a1250709981f69d62a3d
YanaPolonskaya <yanapolonskaya1413@yandex.ru>
2020-03-16 20:44:33 +0300

    test chain fix1

--- test/test_chain.py
+++ test/test_chain.py
@@ -7,6 +7,7 @@ from sklearn.datasets import load_iris
 
 from core.composer.chain import Chain
 from core.composer.node import NodeGenerator
+from core.composer.visualisation import ComposerVisualiser
 from core.models.data import InputData
 from core.models.model import LogRegression, KNN, LDA, XGBoost
 
@@ -90,34 +91,83 @@ def test_models_chain_seq(data_setup):
 def test_chain_comparison_equals_case():
     chain1 = Chain()
     root_of_tree1 = NodeGenerator.secondary_node(XGBoost())
-    last_node_child1 = NodeGenerator.secondary_node(XGBoost())
-    last_node_child2 = NodeGenerator.secondary_node(KNN())
-    for node in (root_of_tree1, last_node_child1, last_node_child2):
+    root_child1 = NodeGenerator.secondary_node(XGBoost())
+    root_child2 = NodeGenerator.secondary_node(KNN())
+    for node in (root_of_tree1, root_child1, root_child2):
         node.nodes_from = []
-    for last_node_child in (last_node_child1, last_node_child2):
+    for root_node_child in (root_child1, root_child2):
         for requirement_model in (LogRegression(), LDA()):
             new_node = NodeGenerator.primary_node(requirement_model, input_data=None)
-            last_node_child.nodes_from.append(new_node)
+            root_node_child.nodes_from.append(new_node)
             chain1.add_node(new_node)
-        chain1.add_node(last_node_child)
-        root_of_tree1.nodes_from.append(last_node_child)
+        chain1.add_node(root_node_child)
+        root_of_tree1.nodes_from.append(root_node_child)
     chain1.add_node(root_of_tree1)
 
     chain2 = Chain()
     root_of_tree2 = NodeGenerator.secondary_node(XGBoost())
-    last_node_child1 = NodeGenerator.secondary_node(LDA())
-    last_node_child2 = NodeGenerator.secondary_node(KNN())
-    for node in (root_of_tree2, last_node_child1, last_node_child2):
+    root_child1 = NodeGenerator.secondary_node(XGBoost())
+    root_child2 = NodeGenerator.secondary_node(KNN())
+    for node in (root_of_tree2, root_child1, root_child2):
         node.nodes_from = []
-    for last_node_child in (last_node_child1, last_node_child2):
+
+    for root_node_child in (root_child1, root_child2):
         for requirement_model in (LogRegression(), LDA()):
             new_node = NodeGenerator.primary_node(requirement_model, input_data=None)
-            last_node_child.nodes_from.append(new_node)
+            root_node_child.nodes_from.append(new_node)
             chain2.add_node(new_node)
-        chain2.add_node(last_node_child)
-        root_of_tree2.nodes_from.append(last_node_child)
+        chain2.add_node(root_node_child)
+        root_of_tree2.nodes_from.append(root_node_child)
     chain2.add_node(root_of_tree2)
 
     assert chain1.equals(chain2)
 
 
+def test_chain_comparison_diffenrent_case():
+    chain1 = Chain()
+    root_of_tree1 = NodeGenerator.secondary_node(XGBoost())
+    root_child1 = NodeGenerator.secondary_node(XGBoost())
+    root_child2 = NodeGenerator.secondary_node(KNN())
+    for node in (root_of_tree1, root_child1, root_child2):
+        node.nodes_from = []
+    for root_node_child in (root_child1, root_child2):
+        for requirement_model in (LogRegression(), LDA()):
+            new_node = NodeGenerator.primary_node(requirement_model, input_data=None)
+            root_node_child.nodes_from.append(new_node)
+            chain1.add_node(new_node)
+        chain1.add_node(root_node_child)
+        root_of_tree1.nodes_from.append(root_node_child)
+    chain1.add_node(root_of_tree1)
+
+    chain2 = Chain()
+    root_of_tree2 = NodeGenerator.secondary_node(XGBoost())
+    root_child1 = NodeGenerator.secondary_node(XGBoost())
+    root_child2 = NodeGenerator.secondary_node(KNN())
+    for node in (root_of_tree2, root_child1, root_child2):
+        node.nodes_from = []
+    new_node = NodeGenerator.primary_node(LogRegression(), input_data=None)
+    root_child1.nodes_from.append(new_node)
+    chain2.add_node(new_node)
+    new_node = NodeGenerator.secondary_node(XGBoost())
+    new_node.nodes_from = []
+    new_node.nodes_from.append(NodeGenerator.primary_node(KNN(), input_data=None))
+    new_node.nodes_from.append(NodeGenerator.primary_node(LDA(), input_data=None))
+    chain2.add_node(new_node.nodes_from[0])
+    chain2.add_node(new_node.nodes_from[1])
+    chain2.add_node(new_node)
+    chain2.add_node(root_child1)
+    root_child1.nodes_from.append(new_node)
+    root_of_tree2.nodes_from.append(root_child1)
+    root_child2.nodes_from.append(NodeGenerator.primary_node(LogRegression(), input_data=None))
+    root_child2.nodes_from.append(NodeGenerator.primary_node(LDA(), input_data=None))
+    chain2.add_node(root_child2.nodes_from[0])
+    chain2.add_node(root_child2.nodes_from[1])
+    chain2.add_node(root_child2)
+    root_of_tree2.nodes_from.append(root_child2)
+    chain2.add_node(root_of_tree2)
+
+    ComposerVisualiser.visualise(chain1)
+    ComposerVisualiser.visualise(chain2)
+    similar_nodes = chain1.root_node.get_similar_nodes(chain2.root_node)
+
+    assert (not chain1.equals(chain2)) and len(similar_nodes) == 6
