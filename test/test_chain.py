0871a5daf128856aced57a91d0cfea9aaccfabd5
YanaPolonskaya <yanapolonskaya1413@yandex.ru>
2020-03-16 16:55:03 +0300

    function to chains equality check adeed

--- test/test_chain.py
+++ test/test_chain.py
@@ -8,7 +8,7 @@ from sklearn.datasets import load_iris
 from core.composer.chain import Chain
 from core.composer.node import NodeGenerator
 from core.models.data import InputData
-from core.models.model import LogRegression
+from core.models.model import LogRegression, KNN, LDA, XGBoost
 
 
 @pytest.fixture()
@@ -85,3 +85,39 @@ def test_models_chain_seq(data_setup):
     assert chain.length == 4
     assert chain.depth == 4
     assert len(results.predict) == len(data.target)
+
+
+def test_chain_comparison_equals_case():
+    chain1 = Chain()
+    root_of_tree1 = NodeGenerator.secondary_node(XGBoost())
+    last_node_child1 = NodeGenerator.secondary_node(XGBoost())
+    last_node_child2 = NodeGenerator.secondary_node(KNN())
+    for node in (root_of_tree1, last_node_child1, last_node_child2):
+        node.nodes_from = []
+    for last_node_child in (last_node_child1, last_node_child2):
+        for requirement_model in (LogRegression(), LDA()):
+            new_node = NodeGenerator.primary_node(requirement_model, input_data=None)
+            last_node_child.nodes_from.append(new_node)
+            chain1.add_node(new_node)
+        chain1.add_node(last_node_child)
+        root_of_tree1.nodes_from.append(last_node_child)
+    chain1.add_node(root_of_tree1)
+
+    chain2 = Chain()
+    root_of_tree2 = NodeGenerator.secondary_node(XGBoost())
+    last_node_child1 = NodeGenerator.secondary_node(LDA())
+    last_node_child2 = NodeGenerator.secondary_node(KNN())
+    for node in (root_of_tree2, last_node_child1, last_node_child2):
+        node.nodes_from = []
+    for last_node_child in (last_node_child1, last_node_child2):
+        for requirement_model in (LogRegression(), LDA()):
+            new_node = NodeGenerator.primary_node(requirement_model, input_data=None)
+            last_node_child.nodes_from.append(new_node)
+            chain2.add_node(new_node)
+        chain2.add_node(last_node_child)
+        root_of_tree2.nodes_from.append(last_node_child)
+    chain2.add_node(root_of_tree2)
+
+    assert chain1.equals(chain2)
+
+
--- test/test_composer.py
+++ test/test_composer.py
@@ -203,9 +203,9 @@ def test_gp_composer_quality(data_fixture, request):
 
     predicted_created_by_evo_alg = chain_created_by_evo_alg.predict(dataset_to_validate).predict
 
+
     roc_auc_chain_created_by_hand = roc_auc(y_true=dataset_to_validate.target, y_score=predict_created_by_hand)
     roc_auc_chain_evo_alg = roc_auc(y_true=dataset_to_validate.target, y_score=predicted_created_by_evo_alg)
     print("model created by hand prediction:", roc_auc_chain_created_by_hand)
     print("gp composed model prediction:", roc_auc_chain_evo_alg)
-    similar_nodes = chain_created_by_hand.get_similar_nodes(chain_created_by_evo_alg)
     assert abs(roc_auc_chain_created_by_hand - roc_auc_chain_evo_alg) < 0.2
