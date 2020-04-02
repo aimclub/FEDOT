4ea42d475d3d0580cd1c0e31e220d5eb8a47f249
YanaPolonskaya <yanapolonskaya1413@yandex.ru>
2020-04-02 23:40:53 +0300

    equivalent subtree function and evolution were modified

--- core/composer/optimisers/selection.py
+++ core/composer/optimisers/selection.py
@@ -6,6 +6,7 @@ from typing import (
 )
 
 import numpy as np
+from copy import deepcopy
 
 
 def random_selection(pop_size: int, group_size: int) -> List[int]:
@@ -22,6 +23,6 @@ def tournament_selection(fitness: List[float], population: List[Any], group_size
         for _ in range(num_of_parents):
             group = random_selection(len(fitness), group_size)
             num_of_chosen_ind = group[np.argmin([fitness[ind_num] for ind_num in group])]
-            choice_item.append(population[num_of_chosen_ind])
+            choice_item.append(deepcopy(population[num_of_chosen_ind]))
         chosen.append(tuple(parent for parent in choice_item))
     return chosen
