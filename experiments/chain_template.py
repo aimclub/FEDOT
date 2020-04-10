0134456b2695ebda756da4ad1377a4e59d190929
J3FALL <crisp.coad@gmail.com>
2020-04-10 15:27:13 +0300

    Implement chain template fitting

--- experiments/chain_template.py
+++ experiments/chain_template.py
@@ -1,8 +1,12 @@
+import itertools
 from dataclasses import dataclass
 
 import numpy as np
 
+from core.models.model import InputData
+from core.models.model import sklearn_model_by_type
 from core.repository.model_types_repository import ModelTypesIdsEnum
+from experiments.generate_data import synthetic_dataset
 
 
 @dataclass
@@ -14,7 +18,8 @@ class ModelTemplate:
         self.parents = []
 
 
-def chain_template(model_types, depth, models_per_level):
+def chain_template(model_types, depth, models_per_level,
+                   samples, features):
     models_by_level = []
 
     for level in range(depth - 1):
@@ -26,9 +31,13 @@ def chain_template(model_types, depth, models_per_level):
     models_by_level.append([ModelTemplate(model_type=last_model)])
 
     models_by_level = with_random_links(models_by_level)
+
+    features_shape = [samples, features]
+    # TODO: change target_shape later if non-classification problems will be used
+    target_shape = [samples, 1]
     models_by_level = with_calculated_shapes(models_by_level,
-                                             source_features=[100, 10],
-                                             source_target=[100, 1])
+                                             source_features=features_shape,
+                                             source_target=target_shape)
     return models_by_level
 
 
@@ -67,14 +76,38 @@ def with_calculated_shapes(models_by_level, source_features, source_target):
     return models_by_level
 
 
-def print_chain_template(models_by_level):
+def show_chain_template(models_by_level):
     for level in range(len(models_by_level)):
         print(f'Level = {level}')
         for model in models_by_level[level]:
             print(f'{model.model_type}, input = {model.input_shape}, output = {model.output_shape}')
 
 
+def fit_template(chain_template, classes):
+    templates_by_models = []
+    for model_template in itertools.chain.from_iterable(chain_template):
+        model_instance = sklearn_model_by_type(model_type=model_template.model_type)
+        templates_by_models.append((model_template, model_instance))
+
+    for template, instance in templates_by_models:
+        samples, features = template.input_shape
+
+        features, target = synthetic_dataset(samples_amount=samples,
+                                             features_amount=features_amount,
+                                             classes_amount=classes)
+        target = np.expand_dims(target, axis=1)
+        data_train = InputData(idx=np.arange(0, samples),
+                               features=features, target=target)
+        print(f'Fit {instance}')
+        fitted_model, predictions = instance.fit(data=data_train)
+        print(f'Predictions: {predictions[:10]}')
+
+
 if __name__ == '__main__':
     model_types = [ModelTypesIdsEnum.logit, ModelTypesIdsEnum.knn, ModelTypesIdsEnum.xgboost]
-    chain = chain_template(model_types=model_types, depth=4, models_per_level=3)
-    print_chain_template(chain)
+    samples, features_amount, classes = 1000, 10, 2
+
+    chain = chain_template(model_types=model_types, depth=4, models_per_level=3,
+                           samples=samples, features=features_amount)
+    show_chain_template(chain)
+    fit_template(chain_template=chain, classes=classes)
