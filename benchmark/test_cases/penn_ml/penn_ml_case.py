891b85c9ff1735ae8c3c878a3706cd77fcfdee2b
v1docq <ir@raxeltelematics.com>
2020-05-08 20:56:11 +0300

    penn_ml runner

--- benchmark/test_cases/penn_ml/penn_ml_case.py
+++ benchmark/test_cases/penn_ml/penn_ml_case.py
@@ -2,29 +2,41 @@ from benchmark.benchmark_model_types import ModelTypesEnum
 from benchmark.benchmark_utils import get_penn_case_data_paths, save_metrics_result_file, get_models_hyperparameters
 from benchmark.executor import CaseExecutor
 from core.repository.task_types import MachineLearningTasksEnum
+from pmlb import classification_dataset_names, regression_dataset_names
+from pmlb import fetch_data
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        raise TypeError('Please enter a type of ML_problem as argument.You can choose: classification or regression')
-    type_flag = str(sys.argv[1])
-    train_file, test_file = get_penn_case_data_paths(type_flag)
-    config_models_data = get_models_hyperparameters()
-    if type_flag == 'classification':
-        problem_class = MachineLearningTasksEnum.classification
-    else:
-        problem_class = MachineLearningTasksEnum.regression
+    with open('./datasets.txt', 'r') as f:
+        datasets = f.read().splitlines()
+    print(datasets)
+    if len(datasets) == 0:
+        datasets=classification_dataset_names+regression_dataset_names
+
+    adult_data = fetch_data('adult')
+    print(adult_data.describe())
+
+    for name_of_dataset in datasets:
+        if name_of_dataset in classification_dataset_names:
+            problem_class=MachineLearningTasksEnum.classification
+        else:
+            problem_class=MachineLearningTasksEnum.regression
+        print(name_of_dataset)
+        train_file,test_file=get_penn_case_data_paths(name_of_dataset)
+        config_models_data=get_models_hyperparameters()
+        case_name='penn_ml'+str(name_of_dataset)
+
         
-    result_metrics = CaseExecutor(train_file=train_file,
-                                  test_file = test_file,
-                                  task = problem_class,
-                                  models=[ModelTypesEnum.tpot,
-                                          ModelTypesEnum.h2o,
-                                          ModelTypesEnum.fedot,
-                                          ModelTypesEnum.autokeras,
-                                          ModelTypesEnum.mlbox],
-                                  target_name='target',
-                                  case_label='penn_ml', hyperparameters=config_models_data).execute()
+        result_metrics=CaseExecutor(train_file=train_file,
+                                      test_file=test_file,
+                                      task=problem_class,
+                                      models=[ModelTypesEnum.tpot,
+                                              ModelTypesEnum.h2o,
+                                              ModelTypesEnum.fedot,
+                                              ModelTypesEnum.autokeras,
+                                              ModelTypesEnum.mlbox],
+                                      target_name='target',
+                                      case_label=case_name,hyperparameters=config_models_data).execute()
 
-    result_metrics['hyperparameters'] = config_models_data
+        result_metrics['hyperparameters']=config_models_data
 
-    save_metrics_result_file(result_metrics, file_name='penn_ml_metrics')
+        save_metrics_result_file(result_metrics,file_name='penn_ml_metrics_for '+str(name_of_dataset))
