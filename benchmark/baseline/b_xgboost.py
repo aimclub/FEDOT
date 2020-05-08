341d70ca6e7dd33330eaf281373cba81d4b84f18
v1docq <ir@raxeltelematics.com>
2020-05-08 20:53:10 +0300

    xgb baseline

--- benchmark/baseline/b_xgboost.py
+++ benchmark/baseline/b_xgboost.py
@@ -1,29 +1,31 @@
 import pandas as pd
 import xgboost as xgb
 from sklearn.metrics import roc_auc_score
-from xgboost import XGBClassifier
-
+from xgboost import XGBClassifier,XGBRegressor
+from sklearn.metrics import mean_squared_error as mse
 from benchmark.benchmark_utils import get_scoring_case_data_paths
 from core.models.data import InputData
 
 
-def run_xgboost(train_file_path: str, test_file_path: str, target_name: str):
+def run_xgboost(train_file_path: str, test_file_path: str, target_name: str, task_flag: int):
     train_dataframe = pd.read_csv(train_file_path)
     test_dataframe = pd.read_csv(test_file_path)
-
-    train_data = xgb.DMatrix(train_dataframe, label=train_dataframe[target_name])
-    test_data = xgb.DMatrix(test_dataframe, label=test_dataframe[target_name])
-
-    xgb_params = {'max_depth': 2, 'eta': 1, 'objective': 'binary:logistic'}
-    num_round = 20
-
-    trained_model = xgb.train(params=xgb_params, dtrain=train_data, num_boost_round=num_round)
-
-    predicted = trained_model.predict(test_data)
-
-    roc_auc_value = round(roc_auc_score(test_data.get_label(), predicted), 3)
-
-    return roc_auc_value
+    if task_flag == 1:
+        train_data = xgb.DMatrix(train_dataframe, label=train_dataframe[target_name])
+        test_data = xgb.DMatrix(test_dataframe, label=test_dataframe[target_name])
+        xgb_params = {'max_depth': 2, 'eta': 1, 'objective': 'binary:logistic'}
+        num_round = 20
+        trained_model = xgb.train(params=xgb_params, dtrain=train_data, num_boost_round=num_round)
+        predicted = trained_model.predict(test_data)
+        result_metric  = round(roc_auc_score(test_data.get_label(), predicted), 3)
+    else:
+        xgbr = xgb.XGBRegressor(max_depth=3,learning_rate=0.3, n_estimators=300,objective='reg:squarederror')
+        x_train,y_train = train_dataframe.iloc[:,:-1], train_dataframe.iloc[:,-1]
+        x_test,y_test = test_dataframe.iloc[:,:-1], test_dataframe.iloc[:,-1]
+        xgbr.fit(x_train, y_train)
+        predicted = xgbr.predict(x_test)
+        result_metric = round(mse(y_test, predicted), 3)
+    return result_metric 
 
 
 def run_xgb_classifier(train_file: str, test_file: str):
