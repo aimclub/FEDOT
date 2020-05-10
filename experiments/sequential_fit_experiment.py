436a77b6c16869c308e7607ad18fa4b297f35139
J3FALL <crisp.coad@gmail.com>
2020-05-10 17:32:17 +0300

    Update cluster labeling approach

--- experiments/sequential_fit_experiment.py
+++ experiments/sequential_fit_experiment.py
@@ -1,3 +1,4 @@
+import random
 from random import uniform
 
 import matplotlib.pyplot as plt
@@ -9,11 +10,15 @@ from experiments.generate_data import gauss_quantiles
 def mixed_clusters_dataset(clusters, samples_total, features_amount, classes=2):
     samples_per_cluster = samples_total // clusters
     mixed_features, mixed_target = [], []
+
+    cluster_labels = _cluster_labels(clusters, classes)
     for cluster in range(clusters):
         mean = [uniform(-3, 3) for _ in range(features_amount)]
         cov = 1.
-        features, target = gauss_quantiles(samples_per_cluster, features_amount, classes,
+        features, target = gauss_quantiles(samples_per_cluster, features_amount, 1,
                                            gauss_params=(mean, cov))
+        label = cluster_labels[cluster]
+        target.fill(label)
 
         mixed_features.append(features)
         mixed_target.append(target)
@@ -24,8 +29,22 @@ def mixed_clusters_dataset(clusters, samples_total, features_amount, classes=2):
     return mixed_features, mixed_target
 
 
+def _cluster_labels(clusters_amount, classes_amount=2):
+    assert classes_amount == 2
+
+    label_threshold = random.randint(1, clusters_amount - 1)
+
+    labels = []
+    for cluster_idx in range(clusters_amount):
+        label = 0 if cluster_idx < label_threshold else 1
+        labels.append(label)
+    random.shuffle(labels)
+
+    return labels
+
+
 if __name__ == '__main__':
-    features, target = mixed_clusters_dataset(clusters=4, samples_total=10000, features_amount=2)
+    features, target = mixed_clusters_dataset(clusters=5, samples_total=10000, features_amount=2)
     plt.figure()
     plt.title("Test")
     plt.scatter(features[:, 0], features[:, 1], marker='o', c=target,
