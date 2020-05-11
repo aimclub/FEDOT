2f2a975f37fd440b672d7b65ed00a00d8beb8ec3
J3FALL <crisp.coad@gmail.com>
2020-05-11 18:13:20 +0300

    Refactor mixed_clusters_dataset && add separate model fit

--- experiments/sequential_fit_experiment.py
+++ experiments/sequential_fit_experiment.py
@@ -1,10 +1,46 @@
+import itertools
 import random
+from copy import copy
 from random import uniform
 
-import matplotlib.pyplot as plt
 import numpy as np
 
+from core.models.data import InputData
+from core.models.model import Model
+from core.models.preprocessing import Normalization
+from core.repository.task_types import MachineLearningTasksEnum
+from experiments.chain_template import (
+    chain_template_balanced_tree,
+    show_chain_template,
+    fit_template,
+    real_chain
+)
 from experiments.generate_data import gauss_quantiles
+
+
+def source_chain(model_types, samples, features, classes):
+    template = chain_template_balanced_tree(model_types=model_types, depth=4, models_per_level=[8, 4, 2, 1],
+                                            samples=samples, features=features)
+    show_chain_template(template)
+    fit_template(template, classes=classes, with_gaussian=True, skip_fit=True)
+    initialized_chain = real_chain(template)
+
+    return initialized_chain
+
+
+def fit_model_templates(templates, data_fit, preprocessor):
+    templates_by_models = []
+    for model_template in itertools.chain.from_iterable(templates):
+        model_instance = Model(model_type=model_template.model_type)
+        model_template.model_instance = model_instance
+        templates_by_models.append((model_template, model_instance))
+
+    for template, instance in templates_by_models:
+        print(f'Fit {instance}')
+        fitted_model, predictions = instance.fit(data=data_fit)
+        template.fitted_model = fitted_model
+        template.data_fit = data_fit
+        template.preprocessor = preprocessor
 
 
 def mixed_clusters_dataset(clusters, samples_total, features_amount, classes=2):
@@ -13,7 +49,7 @@ def mixed_clusters_dataset(clusters, samples_total, features_amount, classes=2):
 
     cluster_labels = _cluster_labels(clusters, classes)
     for cluster in range(clusters):
-        mean = [uniform(-3, 3) for _ in range(features_amount)]
+        mean = [uniform(-4, 4) for _ in range(features_amount)]
         cov = 1.
         features, target = gauss_quantiles(samples_per_cluster, features_amount, 1,
                                            gauss_params=(mean, cov))
@@ -26,7 +62,16 @@ def mixed_clusters_dataset(clusters, samples_total, features_amount, classes=2):
     mixed_features = np.concatenate(mixed_features)
     mixed_target = np.concatenate(mixed_target)
 
-    return mixed_features, mixed_target
+    mixed_target = np.expand_dims(mixed_target, axis=1)
+    data_train = InputData(idx=np.arange(0, samples_total),
+                           features=mixed_features, target=mixed_target,
+                           task_type=MachineLearningTasksEnum.classification)
+
+    mixed_dataset = copy(data_train)
+    preprocessor = Normalization().fit(mixed_dataset.features)
+    mixed_dataset.features = preprocessor.apply(mixed_dataset.features)
+
+    return mixed_dataset, preprocessor
 
 
 def _cluster_labels(clusters_amount, classes_amount=2):
@@ -44,9 +89,4 @@ def _cluster_labels(clusters_amount, classes_amount=2):
 
 
 if __name__ == '__main__':
-    features, target = mixed_clusters_dataset(clusters=5, samples_total=10000, features_amount=2)
-    plt.figure()
-    plt.title("Test")
-    plt.scatter(features[:, 0], features[:, 1], marker='o', c=target,
-                s=25, edgecolor='k')
-    plt.show()
+    data_fit, preprocessor = mixed_clusters_dataset(clusters=5, samples_total=10000, features_amount=2)
