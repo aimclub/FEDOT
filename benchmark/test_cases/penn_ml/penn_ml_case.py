f66f9f07d241edc30e4b587508825d30172a29d0
v1docq <48867680+v1docq@users.noreply.github.com>
2020-05-11 16:14:10 +0300

    Update

--- benchmark/test_cases/penn_ml/penn_ml_case.py
+++ benchmark/test_cases/penn_ml/penn_ml_case.py
@@ -8,19 +8,14 @@ from pmlb import fetch_data
 if __name__ == '__main__':
     with open('./datasets.txt', 'r') as f:
         datasets = f.read().splitlines()
-    print(datasets)
     if len(datasets) == 0:
         datasets=classification_dataset_names+regression_dataset_names
-
-    adult_data = fetch_data('adult')
-    print(adult_data.describe())
 
     for name_of_dataset in datasets:
         if name_of_dataset in classification_dataset_names:
             problem_class=MachineLearningTasksEnum.classification
         else:
             problem_class=MachineLearningTasksEnum.regression
-        print(name_of_dataset)
         train_file,test_file=get_penn_case_data_paths(name_of_dataset)
         config_models_data=get_models_hyperparameters()
         case_name='penn_ml'+str(name_of_dataset)
