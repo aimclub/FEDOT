a7e2781b4aef7a8d1b3441f778b5ae00319e7791
mik985 <68260257+mik985@users.noreply.github.com>
2020-08-06 17:49:11 +0500

    Preprocessing csv file from https://www.kaggle.com/ugocupcic/grasping-dataset

--- cases/grasp_robustness_problem.py
+++ cases/grasp_robustness_problem.py
@@ -1,1 +1,84 @@
+import datetime
+import os
+import random
 
+from cases.credit_scoring_problem import calculate_validation_metric
+from core.composer.gp_composer.gp_composer import GPComposer, GPComposerRequirements
+from core.composer.visualisation import ComposerVisualiser
+from core.models.data import InputData
+from core.repository.model_types_repository import (
+    ModelTypesRepository
+)
+from core.repository.quality_metrics_repository import ClassificationMetricsEnum, MetricsRepository
+from core.repository.tasks import Task, TaskTypesEnum
+from core.utils import project_root
+
+import numpy as np
+
+random.seed(1)
+np.random.seed(1)
+
+
+def run_grasp_robustness_problem(train_file_path, test_file_path,
+                                 max_lead_time: datetime.timedelta = datetime.timedelta(minutes=5),
+                                 is_visualise=False):
+    task = Task(TaskTypesEnum.classification)
+    dataset_to_compose = InputData.from_csv(train_file_path, task=task)
+    dataset_to_validate = InputData.from_csv(test_file_path, task=task)
+
+    # the search of the models provided by the framework that can be used as nodes in a chain for the selected task
+    available_model_types, _ = ModelTypesRepository().suitable_model(task_type=task.task_type)
+
+    # the choice of the metric for the chain quality assessment during composition
+    metric_function = MetricsRepository().metric_by_id(ClassificationMetricsEnum.ROCAUC_penalty)
+
+    # the choice and initialisation of the GP search
+    composer_requirements = GPComposerRequirements(
+        primary=available_model_types,
+        secondary=available_model_types, max_arity=3,
+        max_depth=3, pop_size=20, num_of_generations=20,
+        crossover_prob=0.8, mutation_prob=0.8, max_lead_time=max_lead_time)
+
+    # Create GP-based composer
+    composer = GPComposer()
+
+    # the optimal chain generation by composition - the most time-consuming task
+    chain_evo_composed = composer.compose_chain(data=dataset_to_compose,
+                                                initial_chain=None,
+                                                composer_requirements=composer_requirements,
+                                                metrics=metric_function,
+                                                is_visualise=False)
+
+    chain_evo_composed.fine_tune_primary_nodes(input_data=dataset_to_compose,
+                                               iterations=50)
+
+    chain_evo_composed.fit(input_data=dataset_to_compose, verbose=True)
+
+    if is_visualise:
+        ComposerVisualiser.visualise(chain_evo_composed)
+
+    # the quality assessment for the obtained composite models
+    roc_on_valid_evo_composed = calculate_validation_metric(chain_evo_composed,
+                                                            dataset_to_validate)
+
+    print(f'Composed ROC AUC is {round(roc_on_valid_evo_composed, 3)}')
+
+    return roc_on_valid_evo_composed
+
+
+if __name__ == '__main__':
+    # the dataset was obtained from https://www.kaggle.com/ugocupcic/grasping-dataset
+
+    # a dataset that will be used as a train and test set during composition
+
+    #    dataset = np.loadtxt("../input/grasping-dataset/shadow_robot_dataset.csv", skiprows=1, usecols=range(1, 30),
+    #                                    delimiter = ",")
+
+    file_path_train = 'cases/data/robotics/robotics_data_train.csv'
+    full_path_train = os.path.join(str(project_root()), file_path_train)
+
+    # a dataset for a final validation of the composed model
+    file_path_test = 'cases/data/robotics/robotics_data_test.csv'
+    full_path_test = os.path.join(str(project_root()), file_path_test)
+
+    run_grasp_robustness_problem(full_path_train, full_path_test, is_visualise=True)
