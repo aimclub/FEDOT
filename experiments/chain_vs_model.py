8ac090524095350e63e64a28f992bec6b1e30d92
J3FALL <crisp.coad@gmail.com>
2020-08-14 17:00:34 +0300

    Add fedot chain fit/predict on MDC datasets

--- experiments/chain_vs_model.py
+++ experiments/chain_vs_model.py
@@ -2,8 +2,11 @@ from typing import Tuple
 
 import numpy as np
 from numpy import ndarray
+from sklearn.metrics import accuracy_score
 
-from core.models.data import InputData
+from core.composer.chain import Chain
+from core.composer.node import PrimaryNode, SecondaryNode
+from core.models.data import InputData, train_test_data_setup
 from core.repository.dataset_types import DataTypesEnum
 from core.repository.tasks import TaskTypesEnum, Task
 from experiments.synth_generator.generators.mdc import generated_dataset
@@ -33,11 +36,38 @@ def fedot_input_data_format(mdc_dataset: Tuple[ndarray, ndarray]):
 
     idx = np.arange(0, samples.shape[0])
     target = labels.reshape(-1)
-
+    target += 1
     fedot_input = InputData(idx=idx, features=samples, target=target,
                             task=Task(TaskTypesEnum.classification),
                             data_type=DataTypesEnum.table)
     return fedot_input
 
 
-fedot_input_data_format(default_mdc_dataset())
+def default_fedot_chain():
+    first = PrimaryNode(model_type='logit')
+    second = PrimaryNode(model_type='knn')
+    third = SecondaryNode(model_type='xgboost', nodes_from=[first, second])
+
+    chain = Chain()
+    for node in [first, second, third]:
+        chain.add_node(node)
+
+    return chain
+
+
+def accuracy(fitted_chain, data_true):
+    predicted = fitted_chain.predict(data_true)
+    predicted_classes = np.argmax(predicted.predict, axis=1)
+    score = accuracy_score(data_true.target, predicted_classes)
+
+    return score
+
+
+if __name__ == '__main__':
+    generated_data = fedot_input_data_format(default_mdc_dataset())
+    data_compose, data_validate = train_test_data_setup(generated_data)
+
+    chain = default_fedot_chain()
+
+    chain.fit(data_compose)
+    print(accuracy(chain, data_validate))
