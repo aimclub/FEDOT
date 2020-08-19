6184404fa4175f48916400ed828f1f810acd03e9
BarabanovaIrina <irlania@mail.ru>
2020-08-19 17:24:50 +0300

    fix test_logger

--- test/test_logger.py
+++ test/test_logger.py
@@ -4,7 +4,7 @@ import os
 
 
 @pytest.fixture()
-def test_config_file():
+def get_config_file():
     test_file_path = str(os.path.dirname(__file__))
     path = os.path.join(test_file_path, 'data', 'logging.json')
     if os.path.exists(path):
@@ -16,7 +16,7 @@ def test_loger_setup():
     assert logger.logger.level == 20
 
 
-@pytest.mark.parametrize('data_fixture', ['test_config_file'])
+@pytest.mark.parametrize('data_fixture', ['get_config_file'])
 def test_logger_setup_from_file(data_fixture, request):
     test_file = request.getfixturevalue(data_fixture)
     logger = Logger('test_logger', path=test_file)
