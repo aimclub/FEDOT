9226cd414b9b3e8a17bd0438748ee2d92ad76640
mik985 <68260257+mik985@users.noreply.github.com>
2020-09-04 10:32:26 +0300

    final work version with visualisation and performance model creation functionality.

--- cases/grasp_robustness_problem.py
+++ cases/grasp_robustness_problem.py
@@ -23,6 +23,108 @@ random.seed(1)
 np.random.seed(1)
 
 
+def copy(data: InputData) -> InputData:
+    """
+    Make copy of passed data.
+
+    :param data: InputData class instance data will be copied.
+    :return: InputData
+    """
+    data_copied = InputData(idx=data.idx, features=data.features, target=data.target,
+                            task=data.task, data_type=data.data_type)
+    return data_copied
+
+
+def create_performance_model(dataset: InputData, path_to_save: str,
+                             time_limit: int, list_of_nodes: list,
+                             percent: float, top_percent: float,
+                             percent_step: float, feature_top=None, feature_step: int = 1,
+                             path_to_save_figure: str = None) -> pd.DataFrame:
+    """
+    Visualise function time=f(dataset_size, features_number).
+
+    :param dataset: InputData instance which used to fit given chain.
+    :param path_to_save: Path to save csv-file with the return value.
+    :param time_limit: Time limit of execution in minutes.
+    :param list_of_nodes: List of model_id strings or a single model_id string
+        from which the fixed structure chain will be constructed. The last model in the list_of_nodes is a chain root,
+        the others are his children. Max chain depth is equal to 2.
+    :param percent: The low border of the input data to start tuning. It has to be float in range from 0.0 to 1.0.
+        It should be mentioned that this is not initial data to fit because of the prior data splitting into train and
+        validation samples.
+    :param top_percent: The top border of the input data to finish tuning. It has to be float in range from 0.0 to 1.0
+        and more than param percent. It should be mentioned that this is not final data to fit because of the prior
+        data splitting into train and validation samples.
+    :param percent_step: Step in dataset size. It has to be float in range from 0.0 to 1.0.
+    :param feature_top: The top limit of features to be used. The features are chosen consistently in the order
+        defined by input data features from left to right.
+    :param feature_step: Step in features. Default value is 1.
+    :param path_to_save_figure: Path to save visualisation result. The default value is None which means that
+        the plot will not be saved in any file but visualisation will be available in the console.
+    :return: pd.DataFrame: table with the following columns: 'time', 'num_lines', 'num_features', 'roc_auc',
+                                         'accuracy', 'f1', 'precision', 'recall'.
+    """
+    initial_time = datetime.datetime.now()
+    current_time = 0
+    arr = []
+    features_count = dataset.features.shape[1]
+    if feature_top is None:
+        feature_top = features_count
+    if feature_top > features_count:
+        raise ValueError('Invalid value of param feature_top')
+    dataset_original = copy(dataset)
+    initial_percent = percent
+    for i in np.arange(1, feature_top+1, feature_step):
+        dataset.features = dataset_original.features[:, :i]
+        while current_time <= time_limit and percent <= top_percent:
+            # decreasing number of dataset lines
+            dat, _ = train_test_data_setup(dataset, split_ratio=percent)
+
+            # split dataset to train and test sets
+            dataset_to_compose, dataset_to_validate = train_test_data_setup(dat)
+            num_lines = dataset_to_compose.target.shape[0]
+
+            # calculate parameters optimization time
+            chain = fixed_chain(list_of_nodes)
+            start_time = datetime.datetime.now()
+            chain.fit(input_data=dataset_to_compose, verbose=True)
+            time = datetime.datetime.now() - start_time
+            roc_on_valid_evo_composed = RocAucMetric.get_value(chain, dataset_to_validate)
+            acc_on_valid_evo_composed = AccuracyScore.get_value(chain, dataset_to_validate)
+            f1_on_valid_evo_composed = F1Metric.get_value(chain, dataset_to_validate)
+            precis_on_valid_evo_composed = PrecisionMetric.get_value(chain, dataset_to_validate)
+            recall_on_valid_evo_composed = RecallMetric.get_value(chain, dataset_to_validate)
+            arr.append([time.total_seconds(), num_lines, i, -roc_on_valid_evo_composed, -acc_on_valid_evo_composed,
+                        -f1_on_valid_evo_composed, -precis_on_valid_evo_composed, -recall_on_valid_evo_composed])
+            print(f'Dataset size: {percent * 100}%, number of features: {i},'
+                  f' required time: {time}, elapsed time: {datetime.datetime.now() - initial_time}')
+            print('--------------------')
+            percent += percent_step
+            current_time = round((datetime.datetime.now() - initial_time).seconds / 60)
+        percent = initial_percent
+        print('===============================')
+    if len(list_of_nodes) == 1:
+        print(f'Time is up or process has finished for model: {list_of_nodes[0]}')
+    else:
+        print(f'Time is up or process has finished for chain: {list_of_nodes}')
+    print('===============================')
+    results = pd.DataFrame(arr, columns=['time', 'num_lines', 'num_features', 'roc_auc',
+                                         'accuracy', 'f1', 'precision', 'recall'])
+    results.to_csv(path_to_save, index=False)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(results['num_lines'], results['num_features'], results['time'], c='r')
+    ax.set_title(f'Performance model for {list_of_nodes}')
+    ax.set_xlabel('num_lines')
+    ax.set_ylabel('num_features')
+    ax.set_zlabel('time [seconds]')
+    ax.grid()
+    plt.show()
+    if path_to_save_figure:
+        fig.savefig(path_to_save_figure)
+    return results
+
+
 def evolution_visualisation(path_to_history_file, path_to_save_image=None,
                             indices: list = None, flag: bool = False) -> None:
     """
@@ -86,9 +188,11 @@ def fixed_chain(nodes: list) -> Chain:
     """
     Fixed structure chain creation. Chain can consists of a single model.
     The last model in the list nodes is a chain root, the others are his children.
-    Max chain depth is equal to 1.
+    Max chain depth is equal to 2.
 
-    :param nodes: list of model_id strings from which the chain will be constructed.
+    :param nodes: List of model_id strings or a single model_id string
+        from which the fixed structure chain will be constructed. The last model in the list_of_nodes is a chain root,
+        the others are his children. Max chain depth is equal to 2.
     :return: Chain
     """
     if len(nodes) > 1:
@@ -117,21 +221,21 @@ def run_grasp_robustness_problem(dataset_path, list_of_nodes: list,
     dataset size due to its time-consuming. The last one is to fit composed chain at 80% percent of dataset size
     to get the best results for 5-th metrics.
 
-    :param dataset_path: original dataset csv-file location.
-    :param list_of_nodes: list of model_id strings or a single model_id string
+    :param dataset_path: Original dataset csv-file location.
+    :param list_of_nodes: List of model_id strings or a single model_id string
         from which the fixed structure chain will be constructed. The last model in the list_of_nodes is a chain root,
-        the others are his children. Max chain depth is equal to 1.
+        the others are his children. Max chain depth is equal to 2.
     :param part_of_dataset_to_compose: Part of original dataset to use in chain composing.
         Default value is equal to 0.05 to accelerate chain composing which is the most time-consuming task.
         It's necessary thing because original dataset size is too big.
         The parameter value upper limit is set to 0.8 because of the validation set size is automatically defined as 20%
         of the original dataset. In oder to use the upper limit value it's need to set param full_dataset to True.
-    :param full_dataset: if true then the original dataset is fully used. By default, 80% to compose and fit
+    :param full_dataset: If true then the original dataset is fully used. By default, 80% to compose and fit
         and the last 20% percent to validate.
-    :param max_lead_time: time limit of program execution. The real time of execution can differ from this value due to
+    :param max_lead_time: Time limit of program execution. The real time of execution can differ from this value due to
         local features.
-    :param is_visualise: if true then visualize obtained chain.
-    :param fit_fixed_chain: if true then the process consists of only tuning without composition.
+    :param is_visualise: If true then visualize obtained chain.
+    :param fit_fixed_chain: If true then the process consists of only tuning without composition.
     :return: Chain
     """
 
@@ -167,8 +271,9 @@ def run_grasp_robustness_problem(dataset_path, list_of_nodes: list,
         composer_requirements = GPComposerRequirements(
             primary=available_model_types,
             secondary=available_model_types, max_arity=2,
-            max_depth=3, pop_size=10, num_of_generations=20,
-            crossover_prob=0.8, mutation_prob=0.8, max_lead_time=max_lead_time)
+            max_depth=3, pop_size=20, num_of_generations=20,
+            crossover_prob=0.8, mutation_prob=0.8,
+            max_lead_time=max_lead_time, add_single_model_chains=False)
 
         # Create GP-based composer
         composer = GPComposer()
@@ -187,9 +292,9 @@ def run_grasp_robustness_problem(dataset_path, list_of_nodes: list,
 
     start_time = datetime.datetime.now()
     if not full_dataset:
-        chain_evo_composed.fit(input_data=dataset_train_fit, verbose=True)
+        chain_evo_composed.fit(input_data=dataset_train_fit, verbose=True, use_cache=False)
     else:
-        chain_evo_composed.fit(input_data=dataset_to_compose, verbose=True)
+        chain_evo_composed.fit(input_data=dataset_to_compose, verbose=True, use_cache=False)
     time = datetime.datetime.now() - start_time
     print(f'Required time for fitting composed chain with fit method: {time}')
 
@@ -227,3 +332,7 @@ if __name__ == '__main__':
                                            part_of_dataset_to_compose=0.05, full_dataset=False,
                                            is_visualise=True, fit_fixed_chain=False,
                                            max_lead_time=datetime.timedelta(minutes=180))
+
+    # evolution visualisation
+    history_file = 'C:\\Users\\Михаил\\PycharmProjects\\tmp\\history.csv'
+    evolution_visualisation(history_file)
