76cc7abf49065f05becef9385cc5936160f4e437
magleb <maximovglebigorevich@gmail.com>
2020-09-21 19:23:49 +0300

    refactor tests extract model_id + change to custom params

--- test/test_json_vice_versa.py
+++ test/test_json_vice_versa.py
@@ -1,4 +1,5 @@
 import json
+
 from core.composer.chain import Chain
 from core.composer.node import PrimaryNode, SecondaryNode
 from utilities.synthetic.chain_template_new import ChainTemplate
@@ -6,15 +7,15 @@ from utilities.synthetic.chain_template_new import ChainTemplate
 
 def create_static_chain_1() -> Chain:
     chain = Chain()
-    node_tpot = PrimaryNode('lda')
-    node_tpot.custom_params = {'max_run_time_sec': 60}
-
     node_lda = PrimaryNode('lda')
-    node_lda.custom_params = {'n_components': 60, 'max_iter': 30, 'n_jobs': 6, 'learning_method': 'batch', 'verbose': 1}
+    node_lda.custom_params = {'max_run_time_sec': 60}
+
+    node_lda_second = PrimaryNode('lda')
+    node_lda_second.custom_params = {'n_components': 60, 'max_iter': 30, 'n_jobs': 6, 'learning_method': 'batch', 'verbose': 1}
 
     node_rf = SecondaryNode('rf')
     node_rf.custom_params = {'n_estimators': 100}
-    node_rf.nodes_from = [node_tpot, node_lda]
+    node_rf.nodes_from = [node_lda, node_lda_second]
 
     node_dt = SecondaryNode('dt')
     node_dt.nodes_from = [node_rf]
@@ -28,13 +29,62 @@ def create_static_chain_1() -> Chain:
     return chain
 
 
-def test_chain_convert_to_chain_template():
+def create_static_chain_2() -> Chain:
+    chain = Chain()
+    node_lda = PrimaryNode('lda')
+    node_lda.custom_params = {'max_run_time_sec': 60}
+
+    node_lda_second = PrimaryNode('lda')
+    node_lda_second.custom_params = {'n_components': 100}
+
+    node_lda_third = PrimaryNode('lda')
+    node_lda_third.custom_params = {'n_components': 101}
+
+    node_dt = PrimaryNode('dt')
+    node_dt.custom_params = {'n_estimators': 99}
+
+    node_dt_second = SecondaryNode('dt')
+    node_dt_second.custom_params = {'n_estimators': 102}
+    node_dt_second.nodes_from = [node_lda, node_lda_second]
+
+    node_rf = SecondaryNode('rf')
+    node_rf.custom_params = {'n_estimators': 103}
+    node_rf.nodes_from = [node_lda_third, node_lda_second]
+
+    node_rf_second = SecondaryNode('rf')
+    node_rf_second.custom_params = {'n_estimators': 104}
+    node_rf_second.nodes_from = [node_dt_second, node_rf, node_dt]
+
+    chain.add_node(node_rf_second)
+
+    return chain
+
+
+def test_static_chain_convert_to_json_correctly_1():
     chain = create_static_chain_1()
     chain_template = ChainTemplate(chain)
     json_object_actual = chain_template.export_to_json()
 
+
+
     with open('test/data/chain_test_1.json', 'r') as json_file:
         json_object_expected = json.load(json_file)
+
+    assert json_object_actual == json.dumps(json_object_expected)
+
+
+def test_static_chain_convert_to_json_correctly_2():
+    chain = create_static_chain_2()
+    chain_template = ChainTemplate(chain)
+    json_object_actual = chain_template.export_to_json()
+
+    print(json_object_actual)
+
+    with open('test/data/chain_test_2.json', 'r') as json_file:
+        json_object_expected = json.load(json_file)
+
+    print()
+    print(json_object_expected)
 
     assert json_object_actual == json.dumps(json_object_expected)
 
--- utilities/synthetic/chain_template_new.py
+++ utilities/synthetic/chain_template_new.py
@@ -1,26 +1,30 @@
-import uuid
 import json
 
 from core.composer.chain import Chain
 from core.composer.node import PrimaryNode, SecondaryNode, Node
-from core.models.model import DEFAULT_PARAMS_STUB
 
 
 class ChainTemplate:
-    def __init__(self, chain: Chain = None):
+    def __init__(self, instance):
         self.total_model_types = {}
         self.depth = None
         self.nodes = []
+        # TODO think about new fields
         # self.number_primary_nodes = None
         # self.number_secondary_nodes = None
         # self.task_type = None
         # self.training_time = training_time
         # self.input_shape = input_shape
 
-        if chain:
-            self._chain_to_chain_template(chain)
+        if isinstance(instance, Chain):
+            self._chain_to_chain_template(instance)
+        elif isinstance(instance, object):
+            self._json_to_chain_template(instance)
 
     def _chain_to_chain_template(self, chain: Chain):
+        counter = 0
+        visited_nodes = []
+
         def add_model_type_to_state(model_type):
             if model_type in self.total_model_types:
                 self.total_model_types[model_type] += 1
@@ -28,23 +32,30 @@ class ChainTemplate:
                 self.total_model_types[model_type] = 1
 
         def extract_chain_structure(node: Node, model_id):
+            nonlocal counter
             model_template = ModelTemplate(node, model_id)
-            add_model_type_to_state(model_template.model_type)
 
             if node.nodes_from:
-                nodes_from = [extract_chain_structure(node_parent, model_id + len(node.nodes_from) - index)
-                              for index, node_parent in enumerate(node.nodes_from)]
+                nodes_from = []
+                for index, node_parent in enumerate(node.nodes_from):
+                    if node_parent.descriptive_id in visited_nodes:
+                        nodes_from.append(visited_nodes.index(node_parent.descriptive_id))
+                    else:
+                        counter += 1
+                        visited_nodes.append(node_parent.descriptive_id)
+                        nodes_from.append(counter)
+                        extract_chain_structure(node_parent, counter)
             else:
                 nodes_from = []
 
-            model_template.nodes_from = list(map(lambda node_from: node_from.model_id, nodes_from[::-1]))
+            model_template.nodes_from = sorted(nodes_from)
 
-            if model_template not in self.nodes:
-                self.nodes.append(model_template)
+            self.nodes.append(model_template)
+            add_model_type_to_state(model_template.model_type)
 
             return model_template
 
-        extract_chain_structure(chain.root_node, 0)
+        extract_chain_structure(chain.root_node, counter)
         self.depth = chain.depth
 
     def export_to_json(self):
@@ -56,7 +67,7 @@ class ChainTemplate:
 
         return json.dumps(json_object)
 
-    def json_to_chain_template(self, chain_json):
+    def _json_to_chain_template(self, chain_json):
 
         def roll_chain_structure(node_object: dict) -> Node:
             if node_object['nodes_from']:
@@ -65,10 +76,7 @@ class ChainTemplate:
                     # TODO import fitted model
                 else:
                     secondary_node = SecondaryNode(node_object['model_type'])
-                    if node_object['params']:
-                        secondary_node.custom_params = node_object['params']
-                    else:
-                        secondary_node.custom_params = DEFAULT_PARAMS_STUB
+                    secondary_node.custom_params = node_object['params']
                     secondary_node.nodes_from = [roll_chain_structure(node_from) for node_from
                                                  in node_object['nodes_from']]
                     return secondary_node
@@ -78,10 +86,7 @@ class ChainTemplate:
                     # TODO import fitted model
                 else:
                     primary_node = PrimaryNode(node_object['model_type'])
-                    if node_object['params']:
-                        primary_node.custom_params = node_object['params']
-                    else:
-                        primary_node.custom_params = DEFAULT_PARAMS_STUB
+                    primary_node.custom_params = node_object['params']
                     primary_node.nodes_from = []
                     return primary_node
 
@@ -94,19 +99,20 @@ class ModelTemplate:
     def __init__(self, node: Node, model_id: str):
         self.model_id = model_id
         self.model_type = node.model.model_type
-        self.params = node.model.params
-        # TODO where located trained_model_path
-        # self.trained_model_path = node.cache.actual_cached_state
+        self.custom_params = node.model.params
+        self.full_params = None
+        # TODO where located trained_model_path (node.cache.actual_cached_state)
+        self.trained_model_path = None
         self.nodes_from = None
 
     def to_json_object(self):
         model_object = {
             "model_id": self.model_id,
             "model_type": self.model_type,
-            "params": self.params,
+            "custom_params": self.custom_params,
+            # "full_params": self.full_params,
             "nodes_from": self.nodes_from,
             # "trained_model_path": self.trained_model_path,
         }
 
         return model_object
-
