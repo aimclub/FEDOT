974a08aecbc8f229eb463705006a8ff1e9f3cb3c
J3FALL <crisp.coad@gmail.com>
2020-11-30 16:38:34 +0300

    Change version for CI test

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 
 # The text of the README file
 NAME = "fedot"
-VERSION = "0.0.5"
+VERSION = "0.0.2"
 AUTHOR = "NSS Lab"
 SHORT_DESCRIPTION = "Evolutionary structural learning framework FEDOT"
 README = (Path(os.path.join(HERE, "README.rst"))).read_text()
