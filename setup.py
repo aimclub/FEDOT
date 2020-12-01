ae0b7bf15ce5a289ba57de0447bde79d978df5ff
Pavel <crisp.coad@gmail.com>
2020-12-01 14:05:14 +0300

    New Fedot version && pypi url hotfix (#191)
    * Release Fedot v.0.1.1 (#189)
    
    * Add PyPi publish package trigger in CI (#188)
    
    * Move tensorflow to extra requirements
    
    * Implement build package github action
    
    * Add pypi publish action
    
    * Fix typos in publish action
    
    * Update name and author in setup.py
    
    * Change version for CI test
    
    * Fix pypi package naming
    
    * Get rid of old_setup config && test push with tag
    
    * Test CI with tag
    
    * Change repository url
    
    * Fix name in setup.py
    
    * Fix typos in configs
    
    * Update fedot package version
    
    * Fix pypi repository url (#190)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 
 # The text of the README file
 NAME = "fedot"
-VERSION = "0.0.2"
+VERSION = "0.1.1"
 AUTHOR = "NSS Lab"
 SHORT_DESCRIPTION = "Evolutionary structural learning framework FEDOT"
 README = (Path(os.path.join(HERE, "README.rst"))).read_text()
