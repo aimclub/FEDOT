959667bbd7d0014465d942e714f471500e2a777c
BarabanovaIrina <irlania@mail.ru>
2020-12-15 12:13:36 +0300

    fix reqs

--- fedot/core/data/data.py
+++ fedot/core/data/data.py
@@ -1,4 +1,5 @@
 import os
+import re
 import warnings
 from copy import copy
 from dataclasses import dataclass
@@ -9,14 +10,13 @@ import numpy as np
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 
 from fedot.core.algorithms.time_series.lagged_features import prepare_lagged_ts_for_prediction
 from fedot.core.data.preprocessing import ImputationStrategy
 from fedot.core.repository.dataset_types import DataTypesEnum
 from fedot.core.repository.tasks import Task, TaskTypesEnum
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-import re
 
 
 @dataclass
