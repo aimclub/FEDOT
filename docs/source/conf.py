203e0df9fdb700463f3760300ec264e2ebe1f35b
Nikolay Nikitin <nicl.nno@gmail.com>
2021-04-02 18:15:49 +0300

    Version updated to 0.2.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,6 @@ author = 'NSS Lab'
 
 # The full version, including alpha/beta/rc tags
 release = '0.3.0'
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
