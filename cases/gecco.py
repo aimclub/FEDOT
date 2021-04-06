297d48d8521ca60ba845cff3baabe5dac56f989e
Nikolay Nikitin <nicl.nno@gmail.com>
2021-04-07 01:51:20 +0300

    gecco exp upd

--- cases/gecco.py
+++ cases/gecco.py
@@ -1,13 +1,11 @@
-import pandas as pd
-
 from fedot.api.main import Fedot
 
-train_data = pd.read_csv(f'./data/ssh.data')
-test_data = pd.read_csv(f'./data/ssh.data')
+# train_data = pd.read_csv(f'./data/ssh.csv')
+# test_data = pd.read_csv(f'./data/ssh.csv')
 problem = 'ts_forecasting'
 
-auto_model = Fedot(problem=problem, learning_time=1, preset='light',
-                   composer_params={'metric': ['rmse', 'node_num']}, seed=42)
-auto_model.fit(features=train_data, target='class')
+auto_model = Fedot(problem=problem, learning_time=30, preset='light',
+                   composer_params={'metric': ['rmse', 'node_num']}, seed=42, verbose_level=3)
+auto_model.fit(features='./data/ssh.csv')
 
 auto_model.best_models.show()
