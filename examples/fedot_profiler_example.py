8232e23e3cbec6bced2993658962ac3f41cab44d
magleb <maximovglebigorevich@gmail.com>
2021-04-28 15:40:24 +0300

    uncommit profiler example

--- examples/fedot_profiler_example.py
+++ examples/fedot_profiler_example.py
@@ -15,11 +15,11 @@ if __name__ == '__main__':
     # JUST UNCOMMENT WHAT TYPE OF PROFILER DO YOU NEED
     # EXAMPLE of MemoryProfiler.
 
-    # path = os.path.join(os.path.expanduser("~"), 'memory_profiler')
-    # full_path_train, full_path_test = get_scoring_data()
-    # arguments = {'train_file_path': full_path_train, 'test_file_path': full_path_test}
-    # MemoryProfiler(run_credit_scoring_problem, kwargs=arguments, path=path,
-    #                roots=[run_credit_scoring_problem], max_depth=8)
+    path = os.path.join(os.path.expanduser("~"), 'memory_profiler')
+    full_path_train, full_path_test = get_scoring_data()
+    arguments = {'train_file_path': full_path_train, 'test_file_path': full_path_test}
+    MemoryProfiler(run_credit_scoring_problem, kwargs=arguments, path=path,
+                   roots=[run_credit_scoring_problem], max_depth=8)
 
     # EXAMPLE of TimeProfiler.
 
