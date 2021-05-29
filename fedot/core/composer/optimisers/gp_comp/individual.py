d32e2e4ba214c0fe8dd677d538799dbb199366d7
Nikolay Nikitin <nicl.nno@gmail.com>
2021-05-29 19:39:19 +0300

    Fix after rebase

--- fedot/core/composer/optimisers/gp_comp/individual.py
+++ fedot/core/composer/optimisers/gp_comp/individual.py
@@ -3,15 +3,16 @@ from typing import List
 
 from fedot.core.chains.chain import Chain
 from fedot.core.composer.composing_history import ParentOperator
+from fedot.core.graphs.graph import GraphObject
 
 
 class Individual:
-    def __init__(self, chain: Chain, fitness: List[float] = None,
+    def __init__(self, chain: GraphObject, fitness: List[float] = None,
                  parent_operators: List[ParentOperator] = None):
         self.parent_operators = parent_operators if parent_operators is not None else []
         self.fitness = fitness
 
-        self.chain = _release_chain_resources(chain)
+        self.chain = _release_chain_resources(chain) if isinstance(chain, Chain) else chain
 
 
 def _release_chain_resources(chain: Chain):
