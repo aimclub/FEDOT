bahia14/Fedot_Times_Series_Forecast

in-sample validation added

examples/ts_forecasting_tuning.py
@@ -48,10 +48,8 @@ def make_forecast_with_tuning(chain, train_input, predict_input, task):
                                    loss_function=mean_squared_error,
                                    loss_params={'squared': False})
 
-    print('\nChain parameters after tuning')
-    for node in chain.nodes:
-        print(f' Operation {node.operation}, - {node.custom_params}')
-
+    # Fit chain on the entire train data
+    chain.fit_from_scratch(train_input)
     # Predict
     predicted_values = chain.predict(predict_input)
     new_predicted_values = predicted_values.predict

fedot/core/chains/tuning/tuner_interface.py
@@ -6,6 +6,8 @@ import numpy as np
 
 from fedot.core.log import Log, default_log
 from fedot.core.repository.tasks import TaskTypesEnum
+from fedot.core.validation.simple_validation import in_sample_ts_validation, \
+    fit_predict_one_fold
 
 
 class HyperoptTuner(ABC):
@@ -48,30 +50,28 @@ class HyperoptTuner(ABC):
         raise NotImplementedError()
 
     @staticmethod
-    def get_metric_value(train_input, predict_input, test_target,
-                         chain, loss_function, loss_params):
+    def get_metric_value(data, chain, loss_function, loss_params):
         """
         Method calculates metric for algorithm validation
 
-        :param train_input: data for train chain
-        :param predict_input: data for prediction
-        :param test_target: target array for validation
-        :param chain: chain to process
+        :param data: InputData for validation
+        :param chain: Chain to validate
         :param loss_function: function to minimize (or maximize)
         :param loss_params: parameters for loss function
 
         :return : value of loss function
         """
 
-        chain.fit_from_scratch(train_input)
-
         # Make prediction
-        if train_input.task.task_type == TaskTypesEnum.classification:
-            predicted_labels = chain.predict(predict_input)
-            preds = np.array(predicted_labels.predict)
+        if data.task.task_type == TaskTypesEnum.classification:
+            test_target, preds = fit_predict_one_fold(chain, data)
+        elif data.task.task_type == TaskTypesEnum.ts_forecasting:
+            # For time series forecasting task in-sample forecasting is provided
+            test_target, preds = in_sample_ts_validation(chain, data)
         else:
-            predicted_values = chain.predict(predict_input)
-            preds = np.ravel(np.array(predicted_values.predict))
+            test_target, preds = fit_predict_one_fold(chain, data)
+            # Convert predictions into one dimensional array
+            preds = np.ravel(np.array(preds))
             test_target = np.ravel(test_target)
 
         # Calculate metric
@@ -81,14 +81,11 @@ class HyperoptTuner(ABC):
             metric = loss_function(test_target, preds, **loss_params)
         return metric
 
-    def init_check(self, train_input, predict_input,
-                   test_target, loss_function, loss_params) -> None:
+    def init_check(self, data, loss_function, loss_params) -> None:
         """
         Method get metric on validation set before start optimization
 
-        :param train_input: data for train chain
-        :param predict_input: data for prediction
-        :param test_target: target array for validation
+        :param data: InputData for validation
         :param loss_function: function to minimize (or maximize)
         :param loss_params: parameters for loss function
         """
@@ -97,29 +94,22 @@ class HyperoptTuner(ABC):
         # Train chain
         self.init_chain = deepcopy(self.chain)
 
-        self.init_metric = self.get_metric_value(train_input=train_input,
-                                                 predict_input=predict_input,
-                                                 test_target=test_target,
+        self.init_metric = self.get_metric_value(data=data,
                                                  chain=self.init_chain,
                                                  loss_function=loss_function,
                                                  loss_params=loss_params)
 
-    def final_check(self, train_input, predict_input, test_target,
-                    tuned_chain, loss_function, loss_params):
+    def final_check(self, data, tuned_chain, loss_function, loss_params):
         """
         Method propose final quality check after optimization process
 
-        :param train_input: data for train chain
-        :param predict_input: data for prediction
-        :param test_target: target array for validation
+        :param data: InputData for validation
         :param tuned_chain: tuned chain
         :param loss_function: function to minimize (or maximize)
         :param loss_params: parameters for loss function
         """
 
-        obtained_metric = self.get_metric_value(train_input=train_input,
-                                                predict_input=predict_input,
-                                                test_target=test_target,
+        obtained_metric = self.get_metric_value(data=data,
                                                 chain=tuned_chain,
                                                 loss_function=loss_function,
                                                 loss_params=loss_params)
@@ -160,12 +150,13 @@ def _greater_is_better(target, loss_function, loss_params) -> bool:
     """ Function checks is metric (loss function) need to be minimized or
     maximized
 
-    :param target: array with target
+    :param target: target for define what problem is solving (max or min)
     :param loss_function: loss function
     :param loss_params: parameters for loss function
 
     :return : bool value is it good to maximize metric or not
     """
+
     if loss_params is None:
         metric = loss_function(target, target)
     else:

fedot/core/chains/tuning/unified.py
@@ -6,7 +6,6 @@ from hyperopt import fmin, tpe, space_eval
 
 from fedot.core.chains.tuning.hyperparams import get_node_params, convert_params
 from fedot.core.chains.tuning.tuner_interface import HyperoptTuner, _greater_is_better
-from fedot.core.data.data_split import train_test_data_setup
 from fedot.core.log import Log
 
 MAX_METRIC_VALUE = 10e6
@@ -27,24 +26,17 @@ class ChainTuner(HyperoptTuner):
 
         parameters_dict = self._get_parameters_for_tune(self.chain)
 
-        # Train test split
-        train_input, predict_input = train_test_data_setup(input_data)
-        test_target = np.array(predict_input.target)
-
-        is_need_to_maximize = _greater_is_better(target=test_target,
+        is_need_to_maximize = _greater_is_better(target=input_data.target,
                                                  loss_function=loss_function,
                                                  loss_params=loss_params)
         self.is_need_to_maximize = is_need_to_maximize
 
         # Check source metrics for data
-        self.init_check(train_input, predict_input, test_target,
-                        loss_function, loss_params)
+        self.init_check(input_data, loss_function, loss_params)
 
         best = fmin(partial(self._objective,
                             chain=self.chain,
-                            train_input=train_input,
-                            predict_input=predict_input,
-                            test_target=test_target,
+                            data=input_data,
                             loss_function=loss_function,
                             loss_params=loss_params),
                     parameters_dict,
@@ -58,9 +50,7 @@ class ChainTuner(HyperoptTuner):
                                          parameters=best)
 
         # Validation is the optimization do well
-        final_chain = self.final_check(train_input=train_input,
-                                       predict_input=predict_input,
-                                       test_target=test_target,
+        final_chain = self.final_check(data=input_data,
                                        tuned_chain=tuned_chain,
                                        loss_function=loss_function,
                                        loss_params=loss_params)
@@ -111,16 +101,14 @@ class ChainTuner(HyperoptTuner):
 
         return parameters_dict
 
-    def _objective(self, parameters_dict, chain, train_input, predict_input,
-                   test_target, loss_function, loss_params: dict):
+    def _objective(self, parameters_dict, chain, data, loss_function,
+                   loss_params: dict):
         """
         Objective function for minimization / maximization problem
 
         :param parameters_dict: dictionary with operation names and parameters
         :param chain: chain to optimize
-        :param train_input: input for train chain model
-        :param predict_input: input for test chain model
-        :param test_target: target for validation
+        :param data: InputData for model train and validation
         :param loss_function: loss function to optimize
         :param loss_params: parameters for loss function
 
@@ -131,9 +119,7 @@ class ChainTuner(HyperoptTuner):
         chain = ChainTuner.set_arg_chain(chain=chain, parameters=parameters_dict)
 
         try:
-            metric_value = ChainTuner.get_metric_value(train_input=train_input,
-                                                       predict_input=predict_input,
-                                                       test_target=test_target,
+            metric_value = ChainTuner.get_metric_value(data=data,
                                                        chain=chain,
                                                        loss_function=loss_function,
                                                        loss_params=loss_params)

aimclub/FEDOT

examples/ts_forecasting_tuning.py
@@ -48,10 +48,8 @@ def make_forecast_with_tuning(pipeline, train_input, predict_input, task):
                                             loss_function=mean_squared_error,
                                             loss_params={'squared': False})
 
-    print('\nPipeline parameters after tuning')
-    for node in pipeline.nodes:
-        print(f' Operation {node.operation}, - {node.custom_params}')
-
+    # Fit chain on the entire train data
+    pipeline.fit_from_scratch(train_input)
     # Predict
     predicted_values = pipeline.predict(predict_input)
     new_predicted_values = predicted_values.predict

fedot/core/pipelines/tuning/tuner_interface.py
@@ -6,6 +6,8 @@ import numpy as np
 
 from fedot.core.log import Log, default_log
 from fedot.core.repository.tasks import TaskTypesEnum
+from fedot.core.validation.simple_validation import in_sample_ts_validation, \
+    fit_predict_one_fold
 
 
 class HyperoptTuner(ABC):
@@ -48,30 +50,28 @@ class HyperoptTuner(ABC):
         raise NotImplementedError()
 
     @staticmethod
-    def get_metric_value(train_input, predict_input, test_target,
-                         pipeline, loss_function, loss_params):
+    def get_metric_value(data, pipeline, loss_function, loss_params):
         """
         Method calculates metric for algorithm validation
 
-        :param train_input: data for train pipeline
-        :param predict_input: data for prediction
-        :param test_target: target array for validation
-        :param pipeline: pipeline to process
+        :param data: InputData for validation
+        :param pipeline: pipeline to validate
         :param loss_function: function to minimize (or maximize)
         :param loss_params: parameters for loss function
 
         :return : value of loss function
         """
 
-        pipeline.fit_from_scratch(train_input)
-
         # Make prediction
-        if train_input.task.task_type == TaskTypesEnum.classification:
-            predicted_labels = pipeline.predict(predict_input)
-            preds = np.array(predicted_labels.predict)
+        if data.task.task_type == TaskTypesEnum.classification:
+            test_target, preds = fit_predict_one_fold(pipeline, data)
+        elif data.task.task_type == TaskTypesEnum.ts_forecasting:
+            # For time series forecasting task in-sample forecasting is provided
+            test_target, preds = in_sample_ts_validation(pipeline, data)
         else:
-            predicted_values = pipeline.predict(predict_input)
-            preds = np.ravel(np.array(predicted_values.predict))
+            test_target, preds = fit_predict_one_fold(pipeline, data)
+            # Convert predictions into one dimensional array
+            preds = np.ravel(np.array(preds))
             test_target = np.ravel(test_target)
 
         # Calculate metric
@@ -81,14 +81,11 @@ class HyperoptTuner(ABC):
             metric = loss_function(test_target, preds, **loss_params)
         return metric
 
-    def init_check(self, train_input, predict_input,
-                   test_target, loss_function, loss_params) -> None:
+    def init_check(self, data, loss_function, loss_params) -> None:
         """
         Method get metric on validation set before start optimization
 
-        :param train_input: data for train pipeline
-        :param predict_input: data for prediction
-        :param test_target: target array for validation
+        :param data: InputData for validation
         :param loss_function: function to minimize (or maximize)
         :param loss_params: parameters for loss function
         """
@@ -97,29 +94,22 @@ class HyperoptTuner(ABC):
         # Train pipeline
         self.init_pipeline = deepcopy(self.pipeline)
 
-        self.init_metric = self.get_metric_value(train_input=train_input,
-                                                 predict_input=predict_input,
-                                                 test_target=test_target,
-                                                 pipeline=self.init_pipeline,
+        self.init_metric = self.get_metric_value(data=data,
+                                                 pipeline=self.init_chain,
                                                  loss_function=loss_function,
                                                  loss_params=loss_params)
 
-    def final_check(self, train_input, predict_input, test_target,
-                    tuned_pipeline, loss_function, loss_params):
+    def final_check(self, data, tuned_pipeline, loss_function, loss_params):
         """
         Method propose final quality check after optimization process
 
-        :param train_input: data for train pipeline
-        :param predict_input: data for prediction
-        :param test_target: target array for validation
+        :param data: InputData for validation
         :param tuned_pipeline: tuned pipeline
         :param loss_function: function to minimize (or maximize)
         :param loss_params: parameters for loss function
         """
 
-        obtained_metric = self.get_metric_value(train_input=train_input,
-                                                predict_input=predict_input,
-                                                test_target=test_target,
+        obtained_metric = self.get_metric_value(data=data,
                                                 pipeline=tuned_pipeline,
                                                 loss_function=loss_function,
                                                 loss_params=loss_params)
@@ -160,12 +150,13 @@ def _greater_is_better(target, loss_function, loss_params) -> bool:
     """ Function checks is metric (loss function) need to be minimized or
     maximized
 
-    :param target: array with target
+    :param target: target for define what problem is solving (max or min)
     :param loss_function: loss function
     :param loss_params: parameters for loss function
 
     :return : bool value is it good to maximize metric or not
     """
+
     if loss_params is None:
         metric = loss_function(target, target)
     else:

fedot/core/pipelines/tuning/unified.py
@@ -4,7 +4,6 @@ from functools import partial
 import numpy as np
 from hyperopt import fmin, space_eval, tpe
 
-from fedot.core.data.data_split import train_test_data_setup
 from fedot.core.log import Log
 from fedot.core.pipelines.tuning.hyperparams import convert_params, get_node_params
 from fedot.core.pipelines.tuning.tuner_interface import HyperoptTuner, _greater_is_better
@@ -27,24 +26,17 @@ class PipelineTuner(HyperoptTuner):
 
         parameters_dict = self._get_parameters_for_tune(self.pipeline)
 
-        # Train test split
-        train_input, predict_input = train_test_data_setup(input_data)
-        test_target = np.array(predict_input.target)
-
-        is_need_to_maximize = _greater_is_better(target=test_target,
+        is_need_to_maximize = _greater_is_better(target=input_data.target,
                                                  loss_function=loss_function,
                                                  loss_params=loss_params)
         self.is_need_to_maximize = is_need_to_maximize
 
         # Check source metrics for data
-        self.init_check(train_input, predict_input, test_target,
-                        loss_function, loss_params)
+        self.init_check(input_data, loss_function, loss_params)
 
         best = fmin(partial(self._objective,
-                            pipeline=self.pipeline,
-                            train_input=train_input,
-                            predict_input=predict_input,
-                            test_target=test_target,
+                            chain=self.pipeline,
+                            data=input_data,
                             loss_function=loss_function,
                             loss_params=loss_params),
                     parameters_dict,
@@ -58,9 +50,7 @@ class PipelineTuner(HyperoptTuner):
                                                parameters=best)
 
         # Validation is the optimization do well
-        final_pipeline = self.final_check(train_input=train_input,
-                                          predict_input=predict_input,
-                                          test_target=test_target,
+        final_pipeline = self.final_check(data=input_data,
                                           tuned_pipeline=tuned_pipeline,
                                           loss_function=loss_function,
                                           loss_params=loss_params)
@@ -111,16 +101,14 @@ class PipelineTuner(HyperoptTuner):
 
         return parameters_dict
 
-    def _objective(self, parameters_dict, pipeline, train_input, predict_input,
-                   test_target, loss_function, loss_params: dict):
+    def _objective(self, parameters_dict, pipeline, data, loss_function,
+                   loss_params: dict):
         """
         Objective function for minimization / maximization problem
 
         :param parameters_dict: dictionary with operation names and parameters
         :param pipeline: pipeline to optimize
-        :param train_input: input for train pipeline model
-        :param predict_input: input for test pipeline model
-        :param test_target: target for validation
+        :param data: InputData for model train and validation
         :param loss_function: loss function to optimize
         :param loss_params: parameters for loss function
 
@@ -128,15 +116,13 @@ class PipelineTuner(HyperoptTuner):
         """
 
         # Set hyperparameters for every node
-        pipeline = PipelineTuner.set_arg_pipeline(pipeline=pipeline, parameters=parameters_dict)
+        pipeline = self.set_arg_pipeline(pipeline=pipeline, parameters=parameters_dict)
 
         try:
-            metric_value = PipelineTuner.get_metric_value(train_input=train_input,
-                                                          predict_input=predict_input,
-                                                          test_target=test_target,
-                                                          pipeline=pipeline,
-                                                          loss_function=loss_function,
-                                                          loss_params=loss_params)
+            metric_value = self.get_metric_value(data=data,
+                                                 pipeline=pipeline,
+                                                 loss_function=loss_function,
+                                                 loss_params=loss_params)
         except Exception:
             if self.is_need_to_maximize is True:
                 metric_value = -MAX_METRIC_VALUE