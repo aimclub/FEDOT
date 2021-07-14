bahia14/Fedot_Times_Series_Forecast

ts cross-val for tuner

fedot/core/composer/gp_composer/gp_composer.py
@@ -21,7 +21,7 @@ from fedot.core.data.data import InputData
 from fedot.core.data.data_split import train_test_data_setup
 from fedot.core.data.multi_modal import MultiModalData
 from fedot.core.log import Log, default_log
-from fedot.core.validation.cross_validation import cross_validation
+from fedot.core.validation.composer_validation import table_cross_validation
 from fedot.core.repository.operation_types_repository import OperationTypesRepository, get_operations_for_task
 from fedot.core.repository.quality_metrics_repository import ClassificationMetricsEnum, MetricsRepository, \
     RegressionMetricsEnum, MetricsEnum
@@ -124,7 +124,7 @@ class GPComposer(Composer):
             if isinstance(data, MultiModalData):
                 raise NotImplementedError('Cross-validation is not supported for multi-modal data')
             self.log.info("KFolds cross validation for chain composing was applied.")
-            metric_function_for_nodes = partial(cross_validation, data,
+            metric_function_for_nodes = partial(table_cross_validation, data,
                                                 self.composer_requirements.cv_folds, self.metrics)
         else:
             self.log.info("Hold out validation for chain composing was applied.")

fedot/core/chains/tuning/tuner_interface.py
@@ -6,8 +6,8 @@ import numpy as np
 
 from fedot.core.log import Log, default_log
 from fedot.core.repository.tasks import TaskTypesEnum
-from fedot.core.validation.simple_validation import in_sample_ts_validation, \
-    fit_predict_one_fold
+from fedot.core.validation.tuner_validation import in_sample_ts_validation, \
+    fit_predict_one_fold, ts_cross_validation
 
 
 class HyperoptTuner(ABC):
@@ -67,7 +67,7 @@ class HyperoptTuner(ABC):
             test_target, preds = fit_predict_one_fold(chain, data)
         elif data.task.task_type == TaskTypesEnum.ts_forecasting:
             # For time series forecasting task in-sample forecasting is provided
-            test_target, preds = in_sample_ts_validation(chain, data)
+            test_target, preds = ts_cross_validation(chain, data)
         else:
             test_target, preds = fit_predict_one_fold(chain, data)
             # Convert predictions into one dimensional array
@@ -98,6 +98,7 @@ class HyperoptTuner(ABC):
                                                  chain=self.init_chain,
                                                  loss_function=loss_function,
                                                  loss_params=loss_params)
+        a = 0
 
     def final_check(self, data, tuned_chain, loss_function, loss_params):
         """

fedot/core/validation/composer_validation.py
@@ -9,8 +9,8 @@ from fedot.core.repository.quality_metrics_repository import MetricsRepository
 from fedot.core.repository.tasks import TaskTypesEnum
 
 
-def cross_validation(reference_data: InputData, cv_folds: int,
-                     metrics: [str, Callable], chain: Optional[Chain]) -> Tuple[float, ...]:
+def table_cross_validation(reference_data: InputData, cv_folds: int,
+                           metrics: [str, Callable], chain: Optional[Chain]) -> Tuple[float, ...]:
     """ TODO add description """
     if reference_data.task.task_type in [TaskTypesEnum.ts_forecasting, TaskTypesEnum.clustering]:
         raise NotImplementedError(f"Cross validation for {reference_data.task.task_type} is not implemented.")

aimclub/FEDOT

fedot/core/composer/gp_composer/gp_composer.py
@@ -22,6 +22,7 @@ from fedot.core.optimisers.gp_comp.operators.regularization import Regularizatio
 from fedot.core.optimisers.gp_comp.param_free_gp_optimiser import GPGraphParameterFreeOptimiser
 from fedot.core.pipelines.pipeline import Pipeline
 from fedot.core.validation.cross_validation import cross_validation
+from fedot.core.validation.composer_validation import table_cross_validation
 from fedot.core.repository.operation_types_repository import OperationTypesRepository, get_operations_for_task
 from fedot.core.repository.quality_metrics_repository import (ClassificationMetricsEnum, MetricsEnum,
                                                               MetricsRepository, RegressionMetricsEnum)
@@ -112,8 +113,8 @@ class GPComposer(Composer):
         if self.composer_requirements.cv_folds is not None:
             if isinstance(data, MultiModalData):
                 raise NotImplementedError('Cross-validation is not supported for multi-modal data')
-            self.log.info("KFolds cross validation for graph composing was applied.")
-            objective_function_for_pipeline = partial(cross_validation, data,
+            self.log.info("KFolds cross validation for chain composing was applied.")
+            objective_function_for_pipeline = partial(table_cross_validation, data,
                                                       self.composer_requirements.cv_folds, self.metrics)
         else:
             self.log.info("Hold out validation for graph composing was applied.")

fedot/core/pipelines/tuning/tuner_interface.py
@@ -6,8 +6,8 @@ import numpy as np
 
 from fedot.core.log import Log, default_log
 from fedot.core.repository.tasks import TaskTypesEnum
-from fedot.core.validation.simple_validation import in_sample_ts_validation, \
-    fit_predict_one_fold
+from fedot.core.validation.tuner_validation import in_sample_ts_validation, \
+    fit_predict_one_fold, ts_cross_validation
 
 
 class HyperoptTuner(ABC):
@@ -67,7 +67,7 @@ class HyperoptTuner(ABC):
             test_target, preds = fit_predict_one_fold(pipeline, data)
         elif data.task.task_type == TaskTypesEnum.ts_forecasting:
             # For time series forecasting task in-sample forecasting is provided
-            test_target, preds = in_sample_ts_validation(pipeline, data)
+            test_target, preds = ts_cross_validation(pipeline, data)
         else:
             test_target, preds = fit_predict_one_fold(pipeline, data)
             # Convert predictions into one dimensional array

fedot/core/validation/composer_validation.py
@@ -9,8 +9,8 @@ from fedot.core.repository.quality_metrics_repository import MetricsRepository
 from fedot.core.repository.tasks import TaskTypesEnum
 
 
-def cross_validation(reference_data: InputData, cv_folds: int,
-                     metrics: [str, Callable], pipeline: Optional[Pipeline]) -> Tuple[float, ...]:
+def table_cross_validation(reference_data: InputData, cv_folds: int,
+                           metrics: [str, Callable], pipeline: Optional[Pipeline]) -> Tuple[float, ...]:
     """ TODO add description """
     if reference_data.task.task_type in [TaskTypesEnum.ts_forecasting, TaskTypesEnum.clustering]:
         raise NotImplementedError(f"Cross validation for {reference_data.task.task_type} is not implemented.")