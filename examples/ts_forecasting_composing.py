bahia14/Fedot_Times_Series_Forecast

composer ts validation added

examples/ts_forecasting_composing.py
@@ -45,16 +45,6 @@ def get_source_chain():
     return chain
 
 
-def display_chain_info(chain):
-    """ Function print info about chain """
-
-    print('\nObtained chain:')
-    for node in chain.nodes:
-        print(f'{node.operation.operation_type}, params: {node.custom_params}')
-    depth = int(chain.depth)
-    print(f'Chain depth {depth}\n')
-
-
 def get_available_operations():
     """ Function returns available operations for primary and secondary nodes """
     primary_operations = ['lagged', 'smoothing', 'gaussian_filter', 'ar']
@@ -164,11 +154,14 @@ def fit_predict_for_chain(chain, train_input, predict_input):
 
 
 def run_ts_forecasting_problem(forecast_length=50,
-                               with_visualisation=True) -> None:
+                               with_visualisation=True,
+                               cv_folds=None) -> None:
     """ Function launch time series task with composing
 
     :param forecast_length: length of the forecast
     :param with_visualisation: is it needed to show the plots
+    :param cv_folds: is it needed apply cross validation and what number
+    of folds to use
     """
     file_path = '../cases/data/metocean/metocean_data_test.csv'
 
@@ -205,7 +198,7 @@ def run_ts_forecasting_problem(forecast_length=50,
         max_depth=8, pop_size=10, num_of_generations=15,
         crossover_prob=0.8, mutation_prob=0.8,
         max_lead_time=datetime.timedelta(minutes=10),
-        allow_single_operations=False)
+        allow_single_operations=False, cv_folds=cv_folds)
 
     mutation_types = [MutationTypesEnum.parameter_change, MutationTypesEnum.simple,
                       MutationTypesEnum.reduce]
@@ -236,7 +229,7 @@ def run_ts_forecasting_problem(forecast_length=50,
                               actual_values=time_series,
                               is_visualise=with_visualisation)
 
-    display_chain_info(obtained_chain)
+    obtained_chain.print_structure()
 
 
 if __name__ == '__main__':

fedot/core/chains/tuning/tuner_interface.py
@@ -6,8 +6,8 @@ import numpy as np
 
 from fedot.core.log import Log, default_log
 from fedot.core.repository.tasks import TaskTypesEnum
-from fedot.core.validation.tuner_validation import in_sample_ts_validation, \
-    fit_predict_one_fold, ts_cross_validation
+from fedot.core.validation.tune.time_series import ts_cross_validation
+from fedot.core.validation.tune.simple import fit_predict_one_fold
 
 
 class HyperoptTuner(ABC):
@@ -49,8 +49,7 @@ class HyperoptTuner(ABC):
         """
         raise NotImplementedError()
 
-    @staticmethod
-    def get_metric_value(data, chain, loss_function, loss_params):
+    def get_metric_value(self, data, chain, loss_function, loss_params):
         """
         Method calculates metric for algorithm validation
 
@@ -64,12 +63,12 @@ class HyperoptTuner(ABC):
 
         # Make prediction
         if data.task.task_type == TaskTypesEnum.classification:
-            test_target, preds = fit_predict_one_fold(chain, data)
+            test_target, preds = fit_predict_one_fold(data, chain)
         elif data.task.task_type == TaskTypesEnum.ts_forecasting:
             # For time series forecasting task in-sample forecasting is provided
-            test_target, preds = ts_cross_validation(chain, data)
+            test_target, preds = ts_cross_validation(data, chain, log=self.log)
         else:
-            test_target, preds = fit_predict_one_fold(chain, data)
+            test_target, preds = fit_predict_one_fold(data, chain)
             # Convert predictions into one dimensional array
             preds = np.ravel(np.array(preds))
             test_target = np.ravel(test_target)
@@ -98,7 +97,6 @@ class HyperoptTuner(ABC):
                                                  chain=self.init_chain,
                                                  loss_function=loss_function,
                                                  loss_params=loss_params)
-        a = 0
 
     def final_check(self, data, tuned_chain, loss_function, loss_params):
         """
@@ -144,7 +142,6 @@ class HyperoptTuner(ABC):
             else:
                 self.log.info(f'{prefix_init_phrase} {obtained_metric:.3f} '
                               f'bigger than initial (+ 5% deviation) {init_metric:.3f}')
-                # TODO заменить обратно на self.init_chain
                 return self.init_chain
 
 

aimclub/FEDOT

examples/ts_forecasting_composing.py
@@ -45,16 +45,6 @@ def get_source_pipeline():
     return pipeline
 
 
-def display_pipeline_info(pipeline):
-    """ Function print info about pipeline """
-
-    print('\nObtained pipeline:')
-    for node in pipeline.nodes:
-        print(f'{node.operation.operation_type}, params: {node.custom_params}')
-    depth = int(pipeline.graph_depth)
-    print(f'Pipeline depth {depth}\n')
-
-
 def get_available_operations():
     """ Function returns available operations for primary and secondary nodes """
     primary_operations = ['lagged', 'smoothing', 'gaussian_filter', 'ar']
@@ -164,11 +154,14 @@ def fit_predict_for_pipeline(pipeline, train_input, predict_input):
 
 
 def run_ts_forecasting_problem(forecast_length=50,
-                               with_visualisation=True) -> None:
+                               with_visualisation=True,
+                               cv_folds=None) -> None:
     """ Function launch time series task with composing
 
     :param forecast_length: length of the forecast
     :param with_visualisation: is it needed to show the plots
+    :param cv_folds: is it needed apply cross validation and what number
+    of folds to use
     """
     file_path = '../cases/data/metocean/metocean_data_test.csv'
 
@@ -204,7 +197,8 @@ def run_ts_forecasting_problem(forecast_length=50,
         secondary=secondary_operations, max_arity=3,
         max_depth=8, pop_size=10, num_of_generations=15,
         crossover_prob=0.8, mutation_prob=0.8,
-        timeout=datetime.timedelta(minutes=10))
+        timeout=datetime.timedelta(minutes=10),
+        cv_folds=cv_folds)
 
     mutation_types = [parameter_change_mutation, MutationTypesEnum.simple,
                       MutationTypesEnum.reduce]
@@ -219,9 +213,9 @@ def run_ts_forecasting_problem(forecast_length=50,
 
     obtained_pipeline = composer.compose_pipeline(data=train_input, is_visualise=False)
 
-    ################################
+    ###################################
     # Obtained pipeline visualisation #
-    ################################
+    ###################################
     if with_visualisation:
         obtained_pipeline.show()
 
@@ -234,7 +228,7 @@ def run_ts_forecasting_problem(forecast_length=50,
                               actual_values=time_series,
                               is_visualise=with_visualisation)
 
-    display_pipeline_info(obtained_pipeline)
+    obtained_pipeline.print_structure()
 
 
 if __name__ == '__main__':

fedot/core/pipelines/tuning/tuner_interface.py
@@ -6,8 +6,8 @@ import numpy as np
 
 from fedot.core.log import Log, default_log
 from fedot.core.repository.tasks import TaskTypesEnum
-from fedot.core.validation.tuner_validation import in_sample_ts_validation, \
-    fit_predict_one_fold, ts_cross_validation
+from fedot.core.validation.tune.time_series import ts_cross_validation
+from fedot.core.validation.tune.simple import fit_predict_one_fold
 
 
 class HyperoptTuner(ABC):
@@ -49,8 +49,7 @@ class HyperoptTuner(ABC):
         """
         raise NotImplementedError()
 
-    @staticmethod
-    def get_metric_value(data, pipeline, loss_function, loss_params):
+    def get_metric_value(self, data, pipeline, loss_function, loss_params):
         """
         Method calculates metric for algorithm validation
 
@@ -64,12 +63,12 @@ class HyperoptTuner(ABC):
 
         # Make prediction
         if data.task.task_type == TaskTypesEnum.classification:
-            test_target, preds = fit_predict_one_fold(pipeline, data)
+            test_target, preds = fit_predict_one_fold(data, pipeline)
         elif data.task.task_type == TaskTypesEnum.ts_forecasting:
             # For time series forecasting task in-sample forecasting is provided
-            test_target, preds = ts_cross_validation(pipeline, data)
+            test_target, preds = ts_cross_validation(data, pipeline, log=self.log)
         else:
-            test_target, preds = fit_predict_one_fold(pipeline, data)
+            test_target, preds = fit_predict_one_fold(data, pipeline)
             # Convert predictions into one dimensional array
             preds = np.ravel(np.array(preds))
             test_target = np.ravel(test_target)