9c9c3ef050747f3854322c1113634abc2b4a125c
BarabanovaIrina <irlania@mail.ru>
2021-08-11 15:51:46 +0300

    add test_data support

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,9 +2,6 @@ import configparser
 import sys
 
 from fedot.core.data.data import InputData
-
-from fedot.core.data.data_split import train_test_data_setup
-
 from fedot.core.pipelines.pipeline import Pipeline
 
 
@@ -15,19 +12,26 @@ def extract_data_from_config_file(file):
     input_data = config['DEFAULT']['data']
     output_path = config['DEFAULT']['output_path']
 
-    return pipeline_file, input_data, output_path
+    if 'OPTIONAL' in config.sections():
+        if config['OPTIONAL']['test_data']:
+            test_data_path = config['OPTIONAL']['test_data']
+            return pipeline_file, input_data, test_data_path, output_path
+
+    return pipeline_file, input_data, None, output_path
 
 
 def run_fedot(config_file):
-    pipeline_file, input_data_file, output_path = extract_data_from_config_file(config_file)
+    pipeline_file, train_data_path, test_data_path, output_path = extract_data_from_config_file(config_file)
 
     pipeline = Pipeline().load(pipeline_file)
 
-    input_data = InputData.from_csv(input_data_file)
-
-    train_data, test_data = train_test_data_setup(input_data)
+    train_data = InputData.from_csv(train_data_path)
 
     pipeline.fit_from_scratch(train_data)
+
+    if test_data_path:
+        test_data = InputData.from_csv(test_data_path)
+        pipeline.predict(test_data)
 
     pipeline.save(output_path)
 
