ee9a00290283c408c78b075508b9227b127c24d6
Dreamlone <mik_sar@mail.ru>
2021-08-13 13:37:47 +0300

    add multi example

---
+++ cases/demo/multivariate_forecasting.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+from fedot.core.data.data_split import train_test_data_setup
+
+# Additional custom functions
+from cases.demo.processing import prepare_multimodal_data, plot_results, multi_automl_fit_forecast
+
+from pylab import rcParams
+rcParams['figure.figsize'] = 15, 7
+
+
+if __name__ == '__main__':
+    """ 
+    Below is an example of multivariate time series forecasting.
+    An example of how forecasts can be made is presented and a simple 
+    validation is given on a single block which length is equal to the 
+    length of the forecast horizon. 
+    """
+
+    # Define forecast horizon and read dataframe
+    forecast_length = 20
+    df = pd.read_csv('pw_dataset.csv', parse_dates=['datetime'])
+
+    # Wrap time series data into InputData class
+    features_to_use = ['wind_power_kWh', 'diesel_time_h', 'wind_time_h',
+                       'velocity_max_msec', 'velocity_mean_msec', 'tmp_grad',
+                       'diesel_fuel_kWh']
+    ts = np.array(df['diesel_fuel_kWh'])
+    mm_train, mm_test, = prepare_multimodal_data(dataframe=df,
+                                                 features=features_to_use,
+                                                 target='diesel_fuel_kWh',
+                                                 forecast_length=forecast_length)
+
+    # Prepare parameters for algorithm launch
+    # timeout 2 - means that AutoML algorithm will work for 2 minutes
+    composer_params = {'max_depth': 4,
+                       'max_arity': 3,
+                       'pop_size': 20,
+                       'num_of_generations': 100,
+                       'timeout': 0.2,
+                       'preset': 'light_tun',
+                       'metric': 'rmse',
+                       'cv_folds': None,
+                       'validation_blocks': None}
+    forecast = multi_automl_fit_forecast(mm_train, mm_test, composer_params,
+                                         vis=True)
+
+    mse_metric = mean_squared_error(mm_test.target, forecast, squared=False)
+    mae_metric = mean_absolute_error(mm_test.target, forecast)
+    print(f'MAE - {mae_metric:.2f}')
+    print(f'RMSE - {mse_metric:.2f}')
+
+    # Visualise predictions
+    plot_results(actual_time_series=ts,
+                 predicted_values=forecast,
+                 len_train_data=len(ts) - forecast_length)
---
+++ cases/demo/univariate_forecasting_advanced.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+
+# Additional custom functions
+from cases.demo.processing import prepare_unimodal_for_validation, plot_diesel_and_wind, \
+    advanced_validation, automl_fit_forecast
+
+from pylab import rcParams
+rcParams['figure.figsize'] = 15, 7
+
+
+if __name__ == '__main__':
+    """ 
+    Below is an example of univariate time series forecasting.
+    An example of how forecasts can be made is presented and an advanced 
+    validation is given on a several validation blocks which length is equal 
+    to the length of the forecast horizon * number of validation blocks. 
+    """
+
+    # Define forecast horizon and read dataframe
+    forecast_length = 20
+    validation_blocks = 2
+    df = pd.read_csv('pw_dataset.csv', parse_dates=['datetime'])
+
+    # Make visualisation
+    plot_diesel_and_wind(df)
+
+    # Wrap time series data into InputData class
+    ts = np.array(df['diesel_fuel_kWh'])
+    # Get data for time series implementation
+    train_input, validation_input = prepare_unimodal_for_validation(ts, forecast_length,
+                                                                    validation_blocks)
+
+    # Prepare parameters for algorithm launch
+    # timeout 2 - means that AutoML algorithm will work for 2 minutes
+    composer_params = {'max_depth': 4,
+                       'max_arity': 3,
+                       'pop_size': 20,
+                       'num_of_generations': 100,
+                       'timeout': 2,
+                       'preset': 'light_tun',
+                       'metric': 'rmse',
+                       'cv_folds': 2,
+                       'validation_blocks': 2}
+    forecast = automl_fit_forecast(train_input, validation_input, composer_params,
+                                   vis=True, in_sample_forecasting=True,
+                                   horizon=forecast_length*validation_blocks)
+
+    # Perform in-sample validation and display metrics
+    advanced_validation(forecast, forecast_length, validation_blocks, ts)
