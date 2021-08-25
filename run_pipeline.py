b3f6268f7356560dd2633c9d956a6f8e926b0191
Nikolay Nikitin <nicl.nno@gmail.com>
2021-08-25 19:45:16 +0300

    Timestamp added

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,6 +2,7 @@ import configparser
 import json
 import os
 import sys
+from datetime import datetime
 
 from fedot.core.data.data import InputData
 from fedot.core.pipelines.pipeline import Pipeline
@@ -27,21 +28,25 @@ def extract_data_from_config_file(file):
 
 
 def run_fedot(config_file):
+    print('start', datetime.now())
     pipeline_description, train_data_path, task, test_data_path, \
     output_path = extract_data_from_config_file(config_file)
 
     pipeline = pipeline_from_json(pipeline_description)
-
+    print('int1', datetime.now())
     train_data = InputData.from_csv(file_path=train_data_path,
                                     task=task)
-
+    print('fit', datetime.now())
     pipeline.fit_from_scratch(train_data)
+    print('predict', datetime.now())
 
     if test_data_path:
         test_data = InputData.from_csv(test_data_path)
         pipeline.predict(test_data)
 
+    print('save', datetime.now())
     pipeline.save(path=output_path)
+    print('fin', datetime.now())
 
 
 def pipeline_from_json(json_str: str):
