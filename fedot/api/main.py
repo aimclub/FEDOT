75c9aae0a06abe6ca2f5c6b6be7467dc065cc792
Nikolay Nikitin <nicl.nno@gmail.com>
2021-09-01 16:26:42 +0300

    Custom history path added (#411)
    * Custom history path added

--- fedot/api/main.py
+++ fedot/api/main.py
@@ -29,7 +29,8 @@ def default_evo_params(problem):
               'pop_size': 20,
               'num_of_generations': 20,
               'timeout': 2,
-              'preset': 'light_tun'}
+              'preset': 'light_tun',
+              'history_folder': None}
 
     if problem in ['classification', 'regression']:
         params['cv_folds'] = 3
@@ -64,9 +65,12 @@ class Fedot:
             'num_of_generations' - number of generations for composer
             'timeout':- composing time (minutes)
             'available_operations' - list of model names to use
-            'with_tuning' - allow huperparameters tuning for the model
+            'with_tuning' - allow hyperparameters tuning for the model
             'cv_folds' - number of folds for cross-validation
             'validation_blocks' - number of validation blocks for time series forecasting
+            'initial_pipeline' - initial assumption for composing
+            'genetic_scheme' - name of the genetic scheme
+            'history_folder' - name of the folder for composing history
     :param task_params:  additional parameters of the task
     :param seed: value for fixed random seed
     :param verbose_level: level of the output detailing
