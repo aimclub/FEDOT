10e891fd5813002401c46a35c6580c606a86dc27
v1docq <ir@raxeltelematics.com>
2021-09-06 13:35:58 +0300

    API update to 0.4.0 ver 1.2

--- test/unit/api/test_api_functions.py
+++ test/unit/api/test_api_functions.py
@@ -4,14 +4,16 @@ from fedot.core.repository.tasks import TaskTypesEnum, Task
 from fedot.core.repository.operation_types_repository import get_operations_for_task
 from fedot.core.repository.tasks import Task, TaskTypesEnum
 
+preset_checker = API_preset_helper()
+
 
 def test_presets_classification():
     task = Task(TaskTypesEnum.classification)
 
     class_operations = get_operations_for_task(task, mode='all')
 
-    operations_for_light_preset = filter_operations_by_preset(task, 'light')
-    operations_for_ultra_light_preset = filter_operations_by_preset(task, 'ultra_light')
+    operations_for_light_preset = preset_checker.filter_operations_by_preset(task, 'light')
+    operations_for_ultra_light_preset = preset_checker.filter_operations_by_preset(task, 'ultra_light')
 
     assert len(operations_for_ultra_light_preset) < len(operations_for_light_preset) < len(class_operations)
     assert {'dt', 'logit', 'knn'} <= set(operations_for_ultra_light_preset)
@@ -22,8 +24,8 @@ def test_presets_regression():
 
     regr_operations = get_operations_for_task(task, mode='all')
 
-    operations_for_light_preset = filter_operations_by_preset(task, 'light')
-    operations_for_ultra_light_preset = filter_operations_by_preset(task, 'ultra_light')
+    operations_for_light_preset = preset_checker.filter_operations_by_preset(task, 'light')
+    operations_for_ultra_light_preset = preset_checker.filter_operations_by_preset(task, 'ultra_light')
 
     assert len(operations_for_ultra_light_preset) < len(operations_for_light_preset) <= len(regr_operations)
     assert {'dtreg', 'lasso', 'ridge', 'linear'} <= set(operations_for_ultra_light_preset)
