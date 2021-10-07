05f2da7ba3adc915e29029c56935eff0854332c9
valer1435 <valp998@gmail.com>
2021-10-07 17:01:00 +0300

    pipeline_from_automl works

--- test/integration/test_automl.py
+++ test/integration/test_automl.py
@@ -11,11 +11,11 @@ from fedot.core.utils import fedot_project_root
 
 def test_pipeline_from_automl_example():
     project_root_path = str(fedot_project_root())
-    with OperationTypesRepository.init_automl_repository() as _:
-        file_path_train = os.path.join(project_root_path, 'test/data/simple_classification.csv')
-        file_path_test = file_path_train
 
-        auc = run_pipeline_from_automl(file_path_train, file_path_test, max_run_time=timedelta(seconds=1))
+    file_path_train = os.path.join(project_root_path, 'test/data/simple_classification.csv')
+    file_path_test = file_path_train
+
+    auc = run_pipeline_from_automl(file_path_train, file_path_test, max_run_time=timedelta(seconds=1))
 
     assert auc > 0.5
 
