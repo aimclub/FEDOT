ce4630d3e153eadfa329122e5ef9db21870a9ccd
ChrisLisbon <yulashka.htm@yandex.ru>
2021-10-12 15:01:17 +0300

    output_type added to custom_model output, default->custom refactoring

--- examples/time_series/ts_custom_model_tuning.py
+++ examples/time_series/ts_custom_model_tuning.py
@@ -5,12 +5,10 @@ from hyperopt import hp
 from sklearn.metrics import mean_squared_error
 from fedot.core.pipelines.tuning.search_space import SearchSpace
 
-from fedot.core.data.data import InputData
 from fedot.core.pipelines.node import PrimaryNode, SecondaryNode
 from fedot.core.pipelines.pipeline import Pipeline
 from fedot.core.pipelines.tuning.unified import PipelineTuner
-from fedot.core.repository.dataset_types import DataTypesEnum
-from fedot.core.repository.tasks import Task, TaskTypesEnum, TsForecastingParams
+from examples.time_series.ts_forecasting_tuning import prepare_input_data
 
 
 def custom_model_imitation(train_data, test_data, params):
@@ -18,8 +16,11 @@ def custom_model_imitation(train_data, test_data, params):
     a = params.get('a')
     b = params.get('b')
     shape = train_data.shape
+    out_type = 'ts'
+    if len(shape) > 1:
+        out_type = 'table'
     result = np.random.rand(*shape) * a + b
-    return result
+    return result, out_type
 
 
 def get_simple_pipeline():
@@ -31,44 +32,13 @@ def get_simple_pipeline():
     lagged_node.custom_params = {'window_size': 50}
 
     # For custom model params as initial approximation and wrappers with custom model as function is necessary
-    custom_node = SecondaryNode('default', nodes_from=[lagged_node])
-    custom_node.custom_params = {"a": -50, "b": 500, 'model': custom_model_imitation, 'output_type': 'table'}
+    custom_node = SecondaryNode('custom', nodes_from=[lagged_node])
+    custom_node.custom_params = {"a": -50, "b": 500, 'model': custom_model_imitation}
 
     node_final = SecondaryNode('ridge', nodes_from=[custom_node])
     pipeline = Pipeline(node_final)
 
     return pipeline
-
-
-def prepare_input_data(len_forecast, train_data_features, train_data_target,
-                       test_data_features):
-    """ Return prepared data for fit and predict
-    :param len_forecast: forecast length
-    :param train_data_features: time series which can be used as predictors for train
-    :param train_data_target: time series which can be used as target for train
-    :param test_data_features: time series which can be used as predictors for prediction
-    :return train_input: Input Data for fit
-    :return predict_input: Input Data for predict
-    :return task: Time series forecasting task with parameters
-    """
-    task = Task(TaskTypesEnum.ts_forecasting,
-                TsForecastingParams(forecast_length=len_forecast))
-
-    train_input = InputData(idx=np.arange(0, len(train_data_features)),
-                            features=train_data_features,
-                            target=train_data_target,
-                            task=task,
-                            data_type=DataTypesEnum.ts)
-
-    start_forecast = len(train_data_features)
-    end_forecast = start_forecast + len_forecast
-    predict_input = InputData(idx=np.arange(0, end_forecast),
-                              features=np.concatenate([train_data_features, test_data_features]),
-                              target=None,
-                              task=task,
-                              data_type=DataTypesEnum.ts)
-
-    return train_input, predict_input, task
 
 
 def run_pipeline_tuning(time_series, len_forecast):
@@ -89,10 +59,9 @@ def run_pipeline_tuning(time_series, len_forecast):
 
     # Setting custom search space for tuner (necessary)
     # model and output_type should be wrapped into hyperopt
-    custom_search_space = {'default': {'a': (hp.uniform, [-100, 100]),
-                                       'b': (hp.uniform, [0, 1000]),
-                                       'model': (hp.choice, [[custom_model_imitation]]),
-                                       'output_type': (hp.choice, [['table']])}}
+    custom_search_space = {'custom': {'a': (hp.uniform, [-100, 100]),
+                                      'b': (hp.uniform, [0, 1000]),
+                                      'model': (hp.choice, [[custom_model_imitation]])}}
     replace_default_search_space = True
     pipeline_tuner = PipelineTuner(pipeline=pipeline,
                                    task=task,
--- fedot/core/operations/evaluation/operation_implementations/models/custom_model.py
+++ fedot/core/operations/evaluation/operation_implementations/models/custom_model.py
@@ -7,12 +7,12 @@ from fedot.core.operations.evaluation. \
 from fedot.core.repository.dataset_types import DataTypesEnum
 
 
-class DefaultModelImplementation(ModelImplementation):
+class CustomModelImplementation(ModelImplementation):
     """
     Implementation of container for custom model, which is presented as function with
     input train_data(np.array), test_data(np.array), parameters(dict)
-    output type specification DataTypesEnum
-    into parameters dictionary {'model': function, 'output_type': 'table'}
+    output type specification DataTypesEnum (string)
+    into parameters dictionary {'model': function}
     """
     def __init__(self, params: dict = None, log: Log = None):
         super().__init__(log)
@@ -20,12 +20,6 @@ class DefaultModelImplementation(ModelImplementation):
         if not self.params:
             warnings.warn('There is no specified parameters for custom model! Skip node.')
         else:
-            # init output_type
-            if 'output_type' not in self.params.keys():
-                self.output_type = DataTypesEnum.table
-            else:
-                self.output_type = DataTypesEnum[self.params.get('output_type')]
-
             # init model
             if 'model' not in self.params.keys():
                 warnings.warn('There is no key word "model" for model definition in input dictionary. '
@@ -38,21 +32,23 @@ class DefaultModelImplementation(ModelImplementation):
     def fit(self, input_data):
         """
         Default implementation does not support fitting,
-        it should be presented inside wrappers.model parameter
+        it should be presented inside model parameter
         """
         pass
 
     def predict(self, input_data, is_fit_pipeline_stage: Optional[bool]):
         train_data = input_data.features
         target_data = input_data.target
+        self.output_type = input_data.data_type
         predict = train_data
         # If custom model has exceptions inviolate train data goes to Output
         if not is_fit_pipeline_stage:
             try:
-                predict = self.model(train_data, target_data, self.params)
+                predict, output_type = self.model(train_data, target_data, self.params)
+                self.output_type = DataTypesEnum[output_type]
             except Exception as e:
                 warnings.warn(f'{e}\nInput model has incorrect behaviour. Check type hints for model: \
-                                      Callable[[np.array, np.array, dict], np.array]')
+                                      Callable[[np.array, np.array, dict], np.array, str]')
 
         output_data = self._convert_to_output(input_data,
                                               predict=predict,
