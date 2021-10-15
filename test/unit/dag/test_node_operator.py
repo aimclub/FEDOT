921b248c44f8127913123ac13db9e2883dbbadeb
valer1435 <valp998@gmail.com>
2021-10-15 11:23:47 +0300

    add test to issue

--- test/unit/dag/test_node_operator.py
+++ test/unit/dag/test_node_operator.py
@@ -1,4 +1,11 @@
+from random import choice
+
+from fedot.core.composer.constraint import constraint_function
+from fedot.core.optimisers.adapters import PipelineAdapter
+from fedot.core.optimisers.gp_comp.gp_optimiser import GraphGenerationParams
+from fedot.core.optimisers.graph import OptNode, OptGraph
 from fedot.core.pipelines.node import PrimaryNode, SecondaryNode
+from fedot.core.pipelines.pipeline import Pipeline
 
 
 def get_nodes():
@@ -8,6 +15,15 @@ def get_nodes():
     root = SecondaryNode('logit', nodes_from=[third_node])
 
     return [root, third_node, first_node, second_node]
+
+
+def test_constraint_validation_with_opt_node():
+    first_node = PrimaryNode("ridge")
+    second_node = OptNode({'name': "opt"})
+    root = SecondaryNode('ridge', nodes_from=[first_node, second_node])
+    graph = PipelineAdapter().adapt(Pipeline(root))
+    graph_gener_params = GraphGenerationParams()
+    assert constraint_function(graph, graph_gener_params)
 
 
 def test_node_operator_ordered_subnodes_hierarchy():
