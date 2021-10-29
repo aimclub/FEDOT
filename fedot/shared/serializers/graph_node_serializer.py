ac3832e74aa327f4b8014bb5080af3f0d80c7c79
Sergei Pakulin <pakylin3@mail.ru>
2021-10-29 16:43:41 +0500

    + immutability to serialized fields

--- fedot/shared/serializers/graph_node_serializer.py
+++ fedot/shared/serializers/graph_node_serializer.py
@@ -6,9 +6,11 @@ from ..interfaces.serializable import Serializable
 class GraphNodeSerializer(Serializable):
 
     def to_json(self) -> Dict[str, Any]:
-        obj = super().to_json()
-        del obj['_operator']  # to prevent circular references
-        return obj
+        return {
+            k: v
+            for k, v in super().to_json().items()
+            if k != '_operator'  # to prevent circular references
+        }
 
     @classmethod
     def from_json(cls, json_obj: Dict[str, Any]):
--- fedot/shared/serializers/graph_serializer.py
+++ fedot/shared/serializers/graph_serializer.py
@@ -6,9 +6,11 @@ from ..interfaces.serializable import Serializable
 class GraphSerializer(Serializable):
 
     def to_json(self) -> Dict[str, Any]:
-        obj = super().to_json()
-        del obj['operator']  # to prevent circular references
-        return obj
+        return {
+            k: v
+            for k, v in super().to_json().items()
+            if k != 'operator'  # to prevent circular references
+        }
 
     @classmethod
     def from_json(cls, json_obj: Dict[str, Any]):
