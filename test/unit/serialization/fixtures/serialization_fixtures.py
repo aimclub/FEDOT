00dc4735fe51bdedea02fd751e78fd443d64a726
Sergei Pakulin <pakylin3@mail.ru>
2021-11-16 10:47:04 +0500

    + fixture for deserialization

---
+++ test/unit/serialization/fixtures/serialization_fixtures.py
@@ -0,0 +1,13 @@
+import pytest
+from fedot.shared import json_helpers
+
+
+@pytest.fixture
+def _get_class_fixture(monkeypatch):
+    def mock_get_class(obj_type: type):
+        return obj_type
+
+    monkeypatch.setattr(
+        f'{json_helpers._get_class.__module__}.{json_helpers._get_class.__qualname__}',
+        mock_get_class
+    )
