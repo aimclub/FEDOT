a51ef31f5f1a9c72d00df0b2146f78c306d49807
Sergei Pakulin <pakylin3@mail.ru>
2021-11-16 09:22:56 +0500

    improved checks in serialization encoder

--- test/unit/serialization/test_encoder.py
+++ test/unit/serialization/test_encoder.py
@@ -89,8 +89,16 @@ def test_encoder(case: EncoderTestCase):
         with pytest.raises(type(case.result)):
             encoder(case.input_data)
     else:
-        encoded = {k: v for k, v in encoder(case.input_data).items() if k != CLASS_PATH_KEY}
+        if getattr(case.input_data, '__dict__', None) is not None:
+            keys_before = vars(case.input_data).keys()
+            encoded = {k: v for k, v in encoder(case.input_data).items() if k != CLASS_PATH_KEY}
+            keys_after = vars(case.input_data).keys()
+        else:
+            keys_before = keys_after = {}
+            encoded = {k: v for k, v in encoder(case.input_data).items() if k != CLASS_PATH_KEY}
         assert encoded == case.result, 'Encoded json objects are not the same'
+        assert keys_before == keys_after, 'Object instance was changed'
         if isinstance(case.input_data, MockGraph):
+            assert vars(MOCK_NODE_1).keys() != vars(MOCK_NODE_1_COPY).keys()
             for node in case.input_data.nodes:
                 assert getattr(node, '_serialization_id', None) is not None
