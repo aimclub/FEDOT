3a79967ad2c6a314f1916bc5ea5627bf1a376f99
Sergei Pakulin <pakylin3@mail.ru>
2021-11-18 15:17:47 +0500

    renamed shared serialization module_path var

--- test/unit/serialization/test_object_path_dumping.py
+++ test/unit/serialization/test_object_path_dumping.py
@@ -4,7 +4,7 @@ import pytest
 from fedot.core.serializers.json_helpers import CLASS_PATH_KEY, DELIMITER, dump_path_to_obj
 
 from .dataclasses.serialization_dataclasses import DumpObjectTestCase
-from .shared_data import TEST_INPUT_MODULE_PATH, TEST_UUID, TestClass, TestEnum, TestSerializableClass, test_func
+from .shared_data import TEST_MODULE_PATH, TEST_UUID, TestClass, TestEnum, TestSerializableClass, test_func
 
 DUMPING_CASES = [
     DumpObjectTestCase(
@@ -16,25 +16,25 @@ DUMPING_CASES = [
     DumpObjectTestCase(
         test_input=TestEnum.test_val,
         test_answer={
-            CLASS_PATH_KEY: f'{TEST_INPUT_MODULE_PATH}{DELIMITER}TestEnum'
+            CLASS_PATH_KEY: f'{TEST_MODULE_PATH}{DELIMITER}TestEnum'
         }
     ),
     DumpObjectTestCase(
         test_input=test_func,
         test_answer={
-            CLASS_PATH_KEY: f'{TEST_INPUT_MODULE_PATH}{DELIMITER}test_func'
+            CLASS_PATH_KEY: f'{TEST_MODULE_PATH}{DELIMITER}test_func'
         }
     ),
     DumpObjectTestCase(
         test_input=TestClass().test_func,
         test_answer={
-            CLASS_PATH_KEY: f'{TEST_INPUT_MODULE_PATH}{DELIMITER}TestClass.test_func'
+            CLASS_PATH_KEY: f'{TEST_MODULE_PATH}{DELIMITER}TestClass.test_func'
         }
     ),
     DumpObjectTestCase(
         test_input=TestSerializableClass(),
         test_answer={
-            CLASS_PATH_KEY: f'{TEST_INPUT_MODULE_PATH}{DELIMITER}TestSerializableClass'
+            CLASS_PATH_KEY: f'{TEST_MODULE_PATH}{DELIMITER}TestSerializableClass'
         }
     )
 ]
