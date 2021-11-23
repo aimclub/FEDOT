ff2cdde1b83b7c0d36e99041aae1764c678ba3a0
Nikita Gorokhov <nikita.gorokhov@gmail.com>
2021-11-23 23:50:03 +0300

    Created script with memory and time measurement and comparison table creation

--- cases/hyperparameters_comparison/hyperparameters_comparison.py
+++ cases/hyperparameters_comparison/hyperparameters_comparison.py
@@ -1,9 +1,9 @@
 import os
+import random
 from time import time
-from datetime import timedelta
 import tracemalloc
 
-from sklearn.metrics import mean_squared_error, mean_absolute_error, roc_auc_score, f1_score, accuracy_score
+from sklearn.metrics import mean_squared_error
 from functools import partial
 import numpy as np
 import pandas as pd
@@ -17,21 +17,12 @@ from fedot.core.pipelines.tuning.unified import PipelineTuner
 from fedot.core.utils import fedot_project_root
 from fedot.core.repository.tasks import TaskTypesEnum
 from fedot.core.pipelines.tuning.search_space import SearchSpace
-
-from multiprocessing import Pool, freeze_support
-
-
-def pool_map(func,
-             iterable,
-             n_threads=1):
-    if n_threads <= 1:
-        res = [func(i) for i in iterable]
-    else:
-        p = Pool(n_threads)
-        res = p.map(func=func,
-                    iterable=iterable)
-        p.close()
-    return res
+from fedot.core.repository.operation_types_repository import OperationTypesRepository
+from fedot.core.repository.quality_metrics_repository import RegressionMetricsEnum
+from fedot.core.composer.gp_composer.gp_composer import GPComposer, GPComposerBuilder, GPComposerRequirements, \
+    GPGraphOptimiser, GPGraphOptimiserParameters
+from fedot.api.api_utils.api_composer import ApiComposer
+from fedot.core.composer.gp_composer.specific_operators import boosting_mutation, parameter_change_mutation
 
 
 def get_data(file_path,
@@ -57,11 +48,8 @@ def make_measurement(func,
     else:
         test = train
 
-    tracemalloc.start()
-    tracemalloc.stop()
-    tracemalloc.start()
-
     start_time = time()
+    tracemalloc.start()
 
     pipeline = func(train, **params)
 
@@ -69,38 +57,31 @@ def make_measurement(func,
     time_spent = time() - start_time
     tracemalloc.stop()
 
-    output_train = pipeline.predict(train)
-    output_test = pipeline.predict(test)
+    output = pipeline.predict(test)
 
-    try:
-        score_train = metrics(output_train.target, output_train.predict)
-        score_test = metrics(output_test.target, output_test.predict)
-    except Exception:
-        if output_test.predict.shape[1] == 1:
-            score_train = metrics(output_train.target, output_train.predict.astype(int))
-            score_test = metrics(output_test.target, output_test.predict.astype(int))
-        else:
-            score_train = metrics(output_train.target, output_train.predict.argmax(axis=1))
-            score_test = metrics(output_test.target, output_test.predict.argmax(axis=1))
+    score = metrics(output.target, output.predict)
 
-    return score_train, score_test, time_spent, memory_spent, pipeline.root_node.descriptive_id + ''
+    return score, time_spent, memory_spent, pipeline.root_node.descriptive_id
 
 
-def get_fixed_pipeline(fitted_operation='xgbreg'):
+def get_fixed_pipeline(task_type):
     imputation_node = PrimaryNode(operation_type='simple_imputation')
     onehot_node = SecondaryNode(operation_type='one_hot_encoding',
                                 nodes_from=[imputation_node])
     scaling_node = SecondaryNode(operation_type='scaling',
                                  nodes_from=[onehot_node])
-    final_node = SecondaryNode(operation_type=fitted_operation,
-                               nodes_from=[scaling_node])
+    if task_type == TaskTypesEnum.regression:
+        xgboost_node = SecondaryNode(operation_type='xgbreg',
+                                     nodes_from=[scaling_node])
+    else:
+        xgboost_node = SecondaryNode(operation_type='xgboost',
+                                     nodes_from=[scaling_node])
 
-    return final_node
+    return xgboost_node
 
 
-def fixed_structure_with_default_params(train,
-                                        fitted_operation='xgbreg'):
-    fixed_pipeline = get_fixed_pipeline(fitted_operation)
+def fixed_structure_with_default_params(train):
+    fixed_pipeline = get_fixed_pipeline(train.task.task_type)
 
     pipeline_ = Pipeline(fixed_pipeline)
     pipeline_.fit(train)
@@ -108,186 +89,143 @@ def fixed_structure_with_default_params(train,
     return pipeline_
 
 
-def fixed_structure_with_params_optimization(train,
-                                             fitted_operation='xgbreg',
-                                             iterations=100,
-                                             timeout=timedelta(minutes=5),
-                                             algo=rand.suggest,
-                                             search_space=SearchSpace(),
-                                             metrics=mean_squared_error,
-                                             cv_folds=3):
-    fixed_pipeline = get_fixed_pipeline(fitted_operation)
+def fixed_structure_with_random_search(train, search_space=SearchSpace()):
+    fixed_pipeline = get_fixed_pipeline(train.task.task_type)
 
     pipeline_ = Pipeline(fixed_pipeline)
 
-    pipeline_tuner = PipelineTuner(pipeline_,
-                                   iterations=iterations,
-                                   timeout=timeout,
-                                   task=train.task.task_type,
-                                   search_space=search_space,
-                                   algo=algo)
-
-    tuned_pipeline = pipeline_tuner.tune_pipeline(train,
-                                                  loss_function=metrics,
-                                                  cv_folds=cv_folds)
+    pipeline_tuner = PipelineTuner(pipeline_, task=train.task.task_type, search_space=search_space, algo=rand.suggest)
+    tuned_pipeline = pipeline_tuner.tune_pipeline(train, loss_function=mean_squared_error)
 
     return tuned_pipeline
 
 
-def save_comparison_results(key):
-    d, m, fo, algo, i, experiment = key
-    filename = d + '_' + m + '_' + fo + '_' + algo + '_' + str(i) + '_' + str(experiment)
-    if filename not in os.listdir('comparison_results'):
-        if algo == 'default':
-            score_train, score_test, time_spent, memory_spent, pipeline = make_measurement(
-                func=fixed_structure_with_default_params,
-                train_file_path=datasets[d]['train_file'],
-                test_file_path=datasets[d]['test_file'],
-                task=datasets[d]['task'],
-                metrics=datasets[d]['metrics'][m],
-                params={
-                    'fitted_operation': fo
-                }
-            )
-        else:
-            score_train, score_test, time_spent, memory_spent, pipeline = make_measurement(
-                func=fixed_structure_with_params_optimization,
-                train_file_path=datasets[d]['train_file'],
-                test_file_path=datasets[d]['test_file'],
-                task=datasets[d]['task'],
-                metrics=datasets[d]['metrics'][m],
-                params={
-                    'fitted_operation': fo,
-                    'algo': rand.suggest if algo == 'random' else tpe.suggest,
-                    'iterations': i,
-                    'timeout': timeout,
-                    'search_space': ss,
-                    'metrics': datasets[d]['metrics'][m]
-                }
-            )
-        txt = ','.join([str(score_train), str(score_test), str(time_spent), str(memory_spent), pipeline])
-        open('comparison_results/' + filename, 'w').write(txt)
+def fixed_structure_with_bayesian_optimization(train, search_space=SearchSpace()):
+    fixed_pipeline = get_fixed_pipeline(train.task.task_type)
+
+    pipeline_ = Pipeline(fixed_pipeline)
+
+    pipeline_tuner = PipelineTuner(pipeline_, task=train.task.task_type, search_space=search_space, algo=tpe.suggest)
+    tuned_pipeline = pipeline_tuner.tune_pipeline(train, loss_function=mean_squared_error)
+
+    return tuned_pipeline
 
 
-fitted_operations_for_classification = ['logit', 'lgbm']
-fitted_operations_for_regression = ['ridge', 'lgbmreg']
-metrics_for_binary_classification = {'f1': f1_score, 'auc': roc_auc_score}
-metrics_for_multi_classification = {'f1': f1_score, 'accuracy': accuracy_score}
-metrics_for_regression = {'mse': mean_squared_error, 'mae': mean_absolute_error}
+# not ready
 
-overview = pd.read_csv('data_for_comparison/overview.csv')
+"""
+def variable_structure_with_mutual_bayesian_optimization(train,
+                                                         max_arity=None,
+                                                         max_depth=None,
+                                                         pop_size=None,
+                                                         num_of_generations=None,
+                                                         crossover_prob=None,
+                                                         mutation_prob=None):
+    fixed_pipeline = get_fixed_pipeline(train.task.task_type)
 
-datasets = {}
-for idx, row in overview.iterrows():
-    datasets[row['name']] = {
-        'train_file': 'cases/hyperparameters_comparison/data_for_comparison/train_datasets/' + row['name'] + '.csv',
-        'test_file': 'cases/hyperparameters_comparison/data_for_comparison/test_datasets/' + row['name'] + '.csv',
-        'task': TaskTypesEnum.regression if row['task'] == 'regression'
-        else TaskTypesEnum.classification,
-        'fitted_operations': fitted_operations_for_regression if row['task'] == 'regression'
-        else fitted_operations_for_classification,
-        'metrics': metrics_for_regression if row['task'] == 'regression'
-        else metrics_for_binary_classification if row['num_classes'] == 2
-        else metrics_for_multi_classification
-    }
+    pipeline_ = Pipeline(fixed_pipeline)
 
-params = {
-    'ridge': {
-        'alpha': (hp.loguniform, [np.log(1e-9), np.log(1e4)])
-    },
-    'logit': {
-        'C': (hp.loguniform, [np.log(1e-9), np.log(1e4)])
-    },
-    'knn': {
-        'n_neighbors': (hp.randint, [1, 100]),
-        'metric': (hp.choice, [['euclidean', 'manhattan', 'chebyshev', 'cosine']]),
-        'weights': (hp.choice, [['uniform', 'distance']])
-    },
-    'knnreg': {
-        'n_neighbors': (hp.randint, [1, 100]),
-        'metric': (hp.choice, [['euclidean', 'manhattan', 'chebyshev', 'cosine']]),
-        'weights': (hp.choice, [['uniform', 'distance']])
-    },
-    'svr': {
-        'C': (hp.loguniform, [np.log(1e-9), np.log(1e4)]),
-        'kernel': (hp.choice, [['linear', 'poly', 'rbf', 'sigmoid']]),
-        'degree': (hp.randint, [2, 4])
-    },
-    'dt': {
-        'max_depth': (hp.randint, [2, 10]),
-        'min_samples_leaf': (hp.randint, [1, 10000])
-    },
-    'dtreg': {
-        'max_depth': (hp.randint, [2, 10]),
-        'min_samples_leaf': (hp.randint, [1, 10000])
-    },
-    'rf': {
-        'n_estimators': (hp.randint, [10, 1000]),
-        'max_features': (hp.uniform, [0.05, 1]),
-        'bootstrap': (hp.choice, [[True, False]]),
-        'max_depth': (hp.randint, [2, 10]),
-        'min_samples_leaf': (hp.randint, [1, 10000])
-    },
-    'rfr': {
-        'n_estimators': (hp.randint, [10, 1000]),
-        'max_features': (hp.uniform, [0.05, 1]),
-        'bootstrap': (hp.choice, [[True, False]]),
-        'max_depth': (hp.randint, [2, 10]),
-        'min_samples_leaf': (hp.randint, [1, 10000])
-    },
-    'xgbreg': {
-        'n_estimators': (hp.randint, [10, 1000]),
-        'learning_rate': (hp.loguniform, [np.log(1e-6), np.log(1e1)]),
-        'max_depth': (hp.randint, [2, 10]),
-        'min_child_weight': (hp.randint, [1, 10000]),
-        'cosample_bytree': (hp.uniform, [0.05, 1]),
-        'subsample': (hp.uniform, [0.05, 1]),
-        'alpha': (hp.loguniform, [np.log(1e-5), np.log(1e2)]),
-        'lambda': (hp.loguniform, [np.log(1e-5), np.log(1e2)])
-    },
-    'lgbm': {
-        'n_estimators': (hp.randint, [10, 1000]),
-        'early_stopping_rounds': (hp.randint, [10, 100]),
-        'learning_rate': (hp.loguniform, [np.log(1e-6), np.log(1e1)]),
-        'max_depth': (hp.randint, [2, 10]),
-        'min_data_in_leaf': (hp.randint, [1, 10000]),
-        'feature_fraction': (hp.uniform, [0.05, 1]),
-        'bagging_fraction': (hp.uniform, [0.05, 1]),
-        'lambda_l1': (hp.loguniform, [np.log(1e-5), np.log(1e2)]),
-        'lambda_l2': (hp.loguniform, [np.log(1e-5), np.log(1e2)])
-    },
-    'lgbmreg': {
-        'n_estimators': (hp.randint, [10, 1000]),
-        'early_stopping_rounds': (hp.randint, [10, 100]),
-        'learning_rate': (hp.loguniform, [np.log(1e-6), np.log(1e1)]),
-        'max_depth': (hp.randint, [2, 10]),
-        'min_data_in_leaf': (hp.randint, [1, 10000]),
-        'feature_fraction': (hp.uniform, [0.05, 1]),
-        'bagging_fraction': (hp.uniform, [0.05, 1]),
-        'lambda_l1': (hp.loguniform, [np.log(1e-5), np.log(1e2)]),
-        'lambda_l2': (hp.loguniform, [np.log(1e-5), np.log(1e2)])
+    available_model_types, _ = OperationTypesRepository().suitable_operation(task_type=train.task.task_type)
+
+    optimizer_parameters = GPGraphOptimiserParameters(genetic_scheme_type=genetic_scheme_type,
+                                                      mutation_types=[boosting_mutation, parameter_change_mutation,
+                                                                      MutationTypesEnum.single_edge,
+                                                                      MutationTypesEnum.single_change,
+                                                                      MutationTypesEnum.single_drop,
+                                                                      MutationTypesEnum.single_add],
+                                                      crossover_types=[CrossoverTypesEnum.one_point,
+                                                                       CrossoverTypesEnum.subtree],
+                                                      history_folder=composer_params.get('history_folder'))
+
+    req = GPComposerRequirements(primary=available_model_types, secondary=available_model_types,
+                                 max_arity=5 if max_arity is None else max_arity,
+                                 max_depth=5 if max_depth is None else max_depth,
+                                 pop_size=10 if pop_size is None else pop_size,
+                                 num_of_generations=5 if num_of_generations is None else num_of_generations,
+                                 crossover_prob=0.4 if crossover_prob is None else crossover_prob,
+                                 mutation_prob=0.5 if mutation_prob is None else mutation_prob,)
+
+    builder = ApiComposer.get_gp_composer_builder(task=api_params['task'],
+                                           metric_function=metric_function,
+                                           composer_requirements=composer_requirements,
+                                           optimizer_parameters=optimizer_parameters,
+                                           data=api_params['train_data'],
+                                           initial_pipeline=api_params['initial_pipeline'],
+                                           logger=api_params['logger'])
+
+    builder = GPComposerBuilder(train.task).with_requirements(req).with_metrics(RegressionMetricsEnum.RMSE)
+    print(1)
+
+    gp_composer = builder.build()
+    print(2)
+    pipeline_gp_composed = gp_composer.compose_pipeline(data=train)
+    print(3)
+
+    pipeline_gp_composed.fit_from_scratch(input_data=train)
+    print(4)
+
+    pipeline_tuner = PipelineTuner(pipeline_gp_composed, task=train.task.task_type, algo=tpe.suggest)
+    print(5)
+    tuned_pipeline = pipeline_tuner.tune_pipeline(train, loss_function=mean_squared_error)
+    print(6)
+
+    return tuned_pipeline
+"""
+
+score, time_spent, memory_spent, pipeline = {}, {}, {}, {}
+
+datasets = {
+    'elo': {'train_file': 'cases/data/elo/train_elo_split.csv',
+            'test_file': 'cases/data/elo/test_elo_split.csv',
+            'task': TaskTypesEnum.regression,
+            'metrics': partial(mean_squared_error, squared=False)}
+}
+
+approaches = {
+    'default': {
+        'function': fixed_structure_with_default_params,
+        'params': {}
     }
 }
 
-ss = SearchSpace(params, True)
-n_threads = 1
-n_experiments = 3
-n_iter = [50, 200]
-timeout = timedelta(minutes=30)
+params = [
+    {
+        'xgbreg': {
+            'n_estimators': (hp.choice, [[10 ** n for n in range(1, 10)]]),
+            'max_depth': (hp.randint, [1, 10]),
+            'learning_rate': (hp.loguniform, [np.log(0.01), np.log(1)]),
+            'lambda': (hp.choice, [[0] + [1.21 ** i for i in range(0, 7)]]),
+            'alpha': (hp.choice, [[0] + [1.2 ** i for i in range(0, 7)]]),
+            'max_bin': (hp.choice, [[2 ** i - 1 for i in range(1, 8)]]),
+            'cosample_bytree': (hp.uniform, [0.01, 1]),
+            'subsample': (hp.uniform, [0.01, 1]),
+            'min_child_weight': (hp.choice, [[2 ** i - 1 for i in range(3, 8)]])
+        }
+    }
+]
 
-keys = []
+for p in params:
+    approaches['rand___' + str(p)] = {'function': fixed_structure_with_random_search,
+                                      'params': {'search_space': SearchSpace(p, True)}}
+    approaches['tpe___' + str(p)] = {'function': fixed_structure_with_bayesian_optimization,
+                                     'params': {'search_space': SearchSpace(p, True)}}
 
-for experiment in range(n_experiments):
-    for d in datasets:
-        for m in datasets[d]['metrics']:
-            for fo in datasets[d]['fitted_operations']:
-                keys += [(d, m, fo, 'default', 0, experiment)]
-                for i in n_iter:
-                    keys += [(d, m, fo, 'random', i, experiment)]
-                    keys += [(d, m, fo, 'tpe', i, experiment)]
+for d in datasets:
+    for a in approaches:
+        score[(d, a)], time_spent[(d, a)], memory_spent[(d, a)], pipeline[(d, a)] = make_measurement(
+            approaches[a]['function'],
+            datasets[d]['train_file'],
+            datasets[d]['test_file'],
+            datasets[d]['task'],
+            datasets[d]['metrics'],
+            approaches[a]['params']
+        )
 
-if __name__ == '__main__':
-    freeze_support()
-    pool_map(func=save_comparison_results,
-             iterable=keys,
-             n_threads=n_threads)
+pd.set_option('max_column', 6)
+long_comparison_table = pd.concat([pd.Series(score),
+                                   pd.Series(time_spent),
+                                   pd.Series(memory_spent),
+                                   pd.Series(pipeline)],
+                                  axis=1).reset_index()
+long_comparison_table.columns = ['dataset', 'approach', 'score', 'time_spent', 'memory_spent', 'pipeline']
+print(long_comparison_table)
