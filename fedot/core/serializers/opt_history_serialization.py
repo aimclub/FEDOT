84904a0591f4d772c22b31000d626783db4c428a
Sergei Pakulin <pakylin3@mail.ru>
2021-12-06 13:51:36 +0500

    changed parent_objects serialization process

--- fedot/core/serializers/opt_history_serialization.py
+++ fedot/core/serializers/opt_history_serialization.py
@@ -1,28 +1,28 @@
 import itertools
 from typing import TYPE_CHECKING, Any, Dict, List, Type
 
+from fedot.core.optimisers.adapters import PipelineAdapter
 from fedot.core.optimisers.opt_history import OptHistory
 
 if TYPE_CHECKING:
     from fedot.core.optimisers.gp_comp.individual import Individual
 
-from . import any_serialization
+from .any_serialization import any_from_json
 
 
-def _convert_individuals_pipelines_to_templates(individuals: List[List['Individual']]):
-    from fedot.core.pipelines.template import PipelineTemplate
-
+def _convert_individuals_opt_graphs_to_templates(individuals: List[List['Individual']]):
+    adapter = PipelineAdapter()
     for ind in list(itertools.chain(*individuals)):
         for parent_op in ind.parent_operators:
             parent_op.parent_objects = [
-                PipelineTemplate(parent_obj)
+                adapter.restore_as_template(parent_obj)
                 for parent_obj in parent_op.parent_objects
             ]
     return individuals
 
 
 def opt_history_from_json(cls: Type[OptHistory], json_obj: Dict[str, Any]) -> OptHistory:
-    deserialized_hist = any_serialization.any_from_json(cls, json_obj)
-    deserialized_hist.individuals = _convert_individuals_pipelines_to_templates(deserialized_hist.individuals)
-    deserialized_hist.archive_history = _convert_individuals_pipelines_to_templates(deserialized_hist.archive_history)
+    deserialized_hist = any_from_json(cls, json_obj)
+    deserialized_hist.individuals = _convert_individuals_opt_graphs_to_templates(deserialized_hist.individuals)
+    deserialized_hist.archive_history = _convert_individuals_opt_graphs_to_templates(deserialized_hist.archive_history)
     return deserialized_hist
--- fedot/core/serializers/parent_operator_serialization.py
+++ fedot/core/serializers/parent_operator_serialization.py
@@ -1,14 +1,16 @@
 from typing import Any, Dict
 
+from fedot.core.optimisers.adapters import PipelineAdapter
 from fedot.core.optimisers.opt_history import ParentOperator
 
-from . import any_serialization
+from .any_serialization import any_to_json
 
 
 def parent_operator_to_json(obj: ParentOperator) -> Dict[str, Any]:
-    serialized_op = any_serialization.any_to_json(obj)
+    serialized_op = any_to_json(obj)
+    adapter = PipelineAdapter()
     serialized_op['parent_objects'] = [
-        parent_obj.link_to_empty_pipeline
+        adapter.adapt(parent_obj.link_to_empty_pipeline)
         for parent_obj in serialized_op['parent_objects']
     ]
     return serialized_op
