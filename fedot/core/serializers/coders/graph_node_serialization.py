7b0d2be1937f793dcd940c978fa7201e225d0a5e
Sergei Pakulin <pakylin3@mail.ru>
2021-12-08 21:45:03 +0500

    fixed OptNode content serialization

--- fedot/core/serializers/coders/graph_node_serialization.py
+++ fedot/core/serializers/coders/graph_node_serialization.py
@@ -9,8 +9,10 @@ def graph_node_to_json(obj: GraphNode) -> Dict[str, Any]:
     """
     Uses regular serialization but excludes "_operator" field to rid of circular references
     """
-    return {
+    encoded = {
         k: v
         for k, v in any_to_json(obj).items()
         if k not in ['_operator', '_fitted_operation', '_node_data']
     }
+    encoded['content']['name'] = str(encoded['content']['name'])
+    return encoded
