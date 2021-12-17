692b4bfba1beea53812d509498dffe3708dd4a19
Sergei Pakulin <pakylin3@mail.ru>
2021-12-17 19:42:27 +0500

    + graph serialization upd on '_serialization_id'

--- fedot/core/serializers/coders/parent_operator_serialization.py
+++ fedot/core/serializers/coders/parent_operator_serialization.py
@@ -9,7 +9,10 @@ from . import any_to_json
 def parent_operator_to_json(obj: ParentOperator) -> Dict[str, Any]:
     serialized_op = any_to_json(obj)
     serialized_op['parent_objects'] = [
-        parent_obj.graph.uid
+        {
+            'uid': parent_obj.graph.uid,
+            'serial': parent_obj.graph._serialization_id
+        }
         for parent_obj in serialized_op['parent_objects']
     ]
     return serialized_op
