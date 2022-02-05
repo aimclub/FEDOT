2d55dc66dea62b310b533e5e0e79ff9fc13dc96a
Nikita Gorokhov <nikita.gorokhov@gmail.com>
2022-02-05 23:36:27 +0300

    Added test_greater_is_better docstring

--- test/unit/pipelines/test_pipeline_tuning.py
+++ test/unit/pipelines/test_pipeline_tuning.py
@@ -366,6 +366,7 @@ def test_complex_search_space_tuning_correct():
 
 
 def test_greater_is_better():
+    """ Tests _greater_is_better function correctness"""
     target = np.array([1, 0, 1, 0, 1])
     multi_target = np.array([2, 0, 1, 0, 1, 2])
     assert _greater_is_better(target, acc, None)
