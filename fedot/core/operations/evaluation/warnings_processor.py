dc73f9f37293bc484862af3745323a38e40931c9
Dreamlone <mik_sar@mail.ru>
2022-03-22 12:29:33 +0300

    add wrapper

---
+++ fedot/core/operations/evaluation/warnings_processor.py
@@ -0,0 +1,16 @@
+from contextlib import contextmanager
+import sys, os
+
+
+@contextmanager
+def suppress_stdout():
+    """ According to solution
+    http://thesmithfam.org/blog/2012/10/25/temporarily-suppress-console-output-in-python/
+    """
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
