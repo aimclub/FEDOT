bff53c8a45927dbcc9444cc8535c4997a20d76ef
Dreamlone <mik_sar@mail.ru>
2022-03-23 13:31:46 +0300

    pep8 fix

--- fedot/core/operations/warnings_processor.py
+++ fedot/core/operations/warnings_processor.py
@@ -5,9 +5,7 @@ from contextlib import contextmanager
 
 @contextmanager
 def suppress_stdout():
-    """ 
-    Doesn't display messages in the terminal
-    According to solution
+    """ Doesn't display messages in the terminal. According to solution
     http://thesmithfam.org/blog/2012/10/25/temporarily-suppress-console-output-in-python/
     """
     with open(os.devnull, "w") as devnull:
