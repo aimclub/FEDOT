1cbc7d0b3bd1222e91309c75d330746d71e22dad
Mikhail Sarafanov <52418437+Dreamlone@users.noreply.github.com>
2022-03-28 19:02:48 +0300

    Update for release 0.5.2 (#616)
    Update for release 0.5.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 
 # The text of the README file
 NAME = "fedot"
-VERSION = "0.5.1"
+VERSION = "0.5.2"
 AUTHOR = "NSS Lab"
 SHORT_DESCRIPTION = "Automated machine learning framework for composite pipelines"
 README = (Path(os.path.join(HERE, "README.rst"))).read_text()
