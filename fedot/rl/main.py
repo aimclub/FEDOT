418a016312afa175332205cba8a22c041dcf9e41
Andrey Stebenkov <a.stebenkov75@yandex.ru>
2022-04-07 17:56:39 +0300

    Checkpoint and valid test edit, comments added

--- fedot/rl/main.py
+++ fedot/rl/main.py
@@ -10,6 +10,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from ptan.agent import default_states_preprocessor
 from torch.utils.tensorboard import SummaryWriter
 
 from fedot.core.utils import fedot_project_root, default_fedot_data_dir
@@ -17,12 +18,12 @@ from fedot.rl.pipeline_env import PipelineEnv
 
 GAMMA = 0.99
 
-LEARNING_RATE = 0.002
+LEARNING_RATE = 0.001
 ENTROPY_BETA = 0.35
 BATCH_SIZE = 128
 NUM_ENVS = 50
 
-REWARD_STEPS = 4
+REWARD_STEPS = 5
 CLIP_GRAD = 0.1
 
 
@@ -36,12 +37,14 @@ class A2CRnn(nn.Module):
             nn.Linear(hidden_dim, hidden_dim)
         )
 
+        """ Возвращает политику (стратегию) с распределением вероятности по действиям """
         self.policy = nn.Sequential(
             nn.Linear(hidden_dim, 512),
             nn.ReLU(),
             nn.Linear(512, action_dim)
         )
 
+        """ Число, которое приблизительно соответствует ценности состояния """
         self.value = nn.Sequential(
             nn.Linear(hidden_dim, 512),
             nn.ReLU(),
@@ -105,23 +108,28 @@ def unpack_batch(batch, net, device):
     not_done_idx = []
     last_states = []
 
+    # Проходим по обучающему набору переходов и копируем их поля в списки
     for idx, exp in enumerate(batch):
         states.append(np.array(exp.state, copy=False))
         actions.append(int(exp.action))
-        rewards.append(exp.reward)
+        rewards.append(exp.reward)  # Дисконтированная награда
         if exp.last_state is not None:
             not_done_idx.append(idx)
             last_states.append(np.array(exp.last_state, copy=False))
 
+    # Заводим переменные для вычисления на Torch
     states_v = torch.FloatTensor(np.array(states, copy=False)).to(device)
     actions_t = torch.LongTensor(actions).to(device)
 
     # handle rewards
     rewards_np = np.array(rewards, dtype=np.float32)
     if not_done_idx:
+        # Подготавливаем переменную с последним состоянием в цепочке переходов
         last_states_v = torch.FloatTensor(np.array(last_states, copy=False)).to(device)
+        # Запрашиваем аппроксимацию V(s)
         last_vals_v = net(last_states_v)[1]
         last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
+        # Добавляем значение к дисконтированному вознаграждению
         rewards_np[not_done_idx] += GAMMA ** REWARD_STEPS * last_vals_np
 
     ref_vals_v = torch.FloatTensor(rewards_np).to(device)
@@ -132,13 +140,16 @@ if __name__ == '__main__':
     file_path_train = 'cases/data/scoring/scoring_train.csv'
     full_path_train = os.path.join(str(fedot_project_root()), file_path_train)
 
+    file_path_valid = 'cases/data/scoring/scoring_test.csv'
+    full_path_valid = os.path.join(str(fedot_project_root()), file_path_valid)
+
     # make_env = lambda: ptan.common.wrappers.wrap_dqn(
     #     PipelineEnv(full_path_train))
     # envs = [make_env() for _ in range(NUM_ENVS)]
     # in_dim = envs[0].observation_space.shape[0]
     # out_dim = envs[0].action_space.n
 
-    env = PipelineEnv(path_to_data=full_path_train, env_name='train', mode='train')
+    env = PipelineEnv(path_to_data=full_path_train, path_to_valid=full_path_valid, env_name='train')
     in_dim = env.observation_space.shape[0]
     out_dim = env.action_space.n
 
@@ -175,19 +186,43 @@ if __name__ == '__main__':
     batch = []
 
     with RewardTracker(tb_writer, stop_reward=75) as tracker:
-        with ptan.common.utils.TBMeanTracker(tb_writer, batch_size=10) as tb_tracker:
+        with ptan.common.utils.TBMeanTracker(tb_writer, batch_size=5) as tb_tracker:
             for step_idx, exp in enumerate(exp_source):
                 batch.append(exp)
 
-                # handle new rewards
                 new_rewards = exp_source.pop_total_rewards()
 
                 if new_rewards:
                     if tracker.reward(new_rewards[0], step_idx):
                         break
 
+                if (step_idx % 500) == 0 and step_idx != 0:
+                    path_to_save = join(path_to_checkpoint, f'agent_{step_idx}')
+                    torch.save(pnet.state_dict(), path_to_save)
+
+                    with torch.no_grad():
+                        total_rewards = []
+
+                        for episode in range(25):
+                            state = env.reset()
+                            done = False
+
+                            while not done:
+                                state = default_states_preprocessor(state).to(device)
+                                logits_v, _ = pnet(state)
+
+                                prob_v = F.softmax(logits_v)
+                                prob = prob_v.data.cpu().numpy()
+                                action = np.random.choice(len(prob), p=prob)
+                                state, reward, done, info = env.step(action, mode='test')
+                                total_rewards.append(reward)
+
+                    tb_writer.add_scalar("valid_reward", np.mean(total_rewards), step_idx)
+
                 if len(batch) < BATCH_SIZE:
                     continue
+
+                # Процесс подсчета loss
 
                 states_v, actions_t, vals_ref_v = unpack_batch(batch, pnet, device=device)
                 batch.clear()
@@ -195,23 +230,32 @@ if __name__ == '__main__':
                 optimizer.zero_grad()
 
                 logits_v, value_v = pnet(states_v)
+                # Рассчитываем MSE между значением, возвращенным сетью, и аппроксимацией, выполненной
+                # с помощью уравнения Беллмана, развернутного на REWARD_STEPS вперед
                 loss_value_v = F.mse_loss(value_v.squeeze(-1), vals_ref_v)
 
+                # Рассчитываем потери, связанные со стратегией, чтобы выполнить градиенты по стратегиям.
+                # Два первых шага заключаются в вычислении логарифма вероятности действий по формуле:
+                # A(s, a) = Q(s, a) - V(s)
                 log_prob_v = F.log_softmax(logits_v, dim=1)
                 adv_v = vals_ref_v - value_v.squeeze(-1).detach()
+                # Вычисляем логарифм вероятностей для выбранных действий
+                # и масштабируем их с помощью adv_v.
                 log_prob_actions_v = adv_v * log_prob_v[range(BATCH_SIZE), actions_t]
+                # Значение потерь при PG будет равно взятому с обратным знаком среднему для данных
+                # масштабированных логарифмов вероятностей
                 loss_policy_v = -log_prob_actions_v.mean()
 
+                # Рассчитываем потери на энтропию
                 prob_v = F.softmax(logits_v, dim=1)
                 entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
 
-                # Calculate policy gradients only
+                # Просчитываем градиенты
                 loss_policy_v.backward(retain_graph=True)
                 grads = np.concatenate([p.grad.data.cpu().numpy().flatten()
                                         for p in pnet.parameters()
                                         if p.grad is not None])
 
-                # Apply entropy and value gradients
                 loss_v = entropy_loss_v + loss_value_v
                 loss_v.backward()
                 nn.utils.clip_grad_norm_(pnet.parameters(), CLIP_GRAD)
@@ -229,7 +273,3 @@ if __name__ == '__main__':
                 tb_tracker.track("grad_l2", np.sqrt(np.mean(np.square(grads))), step_idx)
                 tb_tracker.track("grad_max", np.max(np.abs(grads)), step_idx)
                 tb_tracker.track("grad_var", np.var(grads), step_idx)
-
-                if step_idx % 1000:
-                    path_to_checkpoint = join(path_to_checkpoint, f'agent_{step_idx}')
-                    torch.save(pnet.state_dict(), path_to_checkpoint)
--- fedot/rl/pipeline_env.py
+++ fedot/rl/pipeline_env.py
@@ -23,10 +23,9 @@ class PipelineEnv(gym.Env):
     reward_range = (-float(100), float(100))
     spec = None
 
-    def __init__(self, path_to_data=None, path_to_valid=None, env_name='default', mode='train', pipeline_depth=4,
+    def __init__(self, path_to_data=None, path_to_valid=None, env_name='default', pipeline_depth=4,
                  graph_render=True):
         self.env_name = 'pl_env_' + env_name
-        self.mode = mode
         self.full_train_data = InputData.from_csv(path_to_data)
         self.train_data, self.test_data = train_test_data_setup(InputData.from_csv(path_to_data), split_ratio=0.7)
         self.testing_data = self.test_data.target
@@ -85,90 +84,68 @@ class PipelineEnv(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def step(self, action: int):
-        if self.mode == 'train':
-            assert self.action_space.contains(action)
+    def step(self, action: int, mode='train'):
+        assert self.action_space.contains(action)
 
-            # Если агент выбирал плейхолдеры, то штрафуем
-            if action == self.pop:
-                self._update_observation(action)
-                reward, done, info = self._env_response()
-
-                return self.observation, reward, done, info
-
-            # Если агент первым действием выбрал заокнчить построение пайплайна,
-            # то штрафуем
-            if action == self.eop and self.nodes == []:
-                self._update_observation(action)
-                reward, done, info = self._env_response()
-
-                return self.observation, reward, done, info
-
-            self._construct_pipeline(action)
-
+        # Если агент выбирал плейхолдеры, то штрафуем
+        if action == self.pop:
             self._update_observation(action)
+            reward, done, info = self._env_response()
 
-            # Если агент решил закончить построение пайплайна или достиг лимита,
-            # то обороачиваем и проверяем
-            if action == self.eop or self.cur_pos == len(self.observation):
-                self.pipeline = Pipeline(self.nodes[-1])
-                pipeline = self.pipeline
-                try:
-                    # Проверка пайплайна на ошибки построения
-                    validate(pipeline, task=Task(self.task_type))
+            return self.observation, reward, done, info
 
-                    # Если прошел, то обучаем
+        # Если агент первым действием выбрал заокнчить построение пайплайна,
+        # то штрафуем
+        if action == self.eop and self.nodes == []:
+            self._update_observation(action)
+            reward, done, info = self._env_response()
+
+            return self.observation, reward, done, info
+
+        self._construct_pipeline(action)
+
+        self._update_observation(action)
+
+        # Если агент решил закончить построение пайплайна или достиг лимита,
+        # то обороачиваем и проверяем
+        if action == self.eop or self.cur_pos == len(self.observation):
+            self.pipeline = Pipeline(self.nodes[-1])
+            pipeline = self.pipeline
+            try:
+                # Проверка пайплайна на ошибки построения
+                validate(pipeline, task=Task(self.task_type))
+
+                # Если прошел, то обучаем
+                if mode == 'train':
                     self.pipeline.fit(self.train_data)
 
                     # Проверка полученого пайплайна на тестовых данных
                     results = self.pipeline.predict(self.test_data)
 
                     # Подсчитываем метрику
-                    self.metric_value = roc_auc(y_true=self.testing_data, y_score=results.predict, multi_class='ovo',
+                    self.metric_value = roc_auc(y_true=self.testing_data,
+                                                y_score=results.predict,
+                                                multi_class='ovo',
                                                 average='macro')
 
-                    rwd = 100 * self.metric_value
-                    self.render(mode='graph')
-                    reward, done, info = self._env_response(reward=rwd, length=len(self.nodes))
-                    return self.observation, reward, done, info
-                except ValueError:
-                    reward, done, info = self._env_response(reward=-85)
-                    return self.observation, reward, done, info
-            else:
-                reward, done, info = self._env_response(reward=0, done=False)
-                return self.observation, reward, done, info
-
-        if self.mode == 'test':
-            assert self.action_space.contains(action)
-
-            self._construct_pipeline(action)
-            self._update_observation(action)
-
-            if action == self.eop or self.cur_pos == len(self.observation):
-                self.pipeline = Pipeline(self.nodes[-1])
-                pipeline = self.pipeline
-                try:
-                    validate(pipeline, task=Task(self.task_type))
-
+                elif mode == 'test':
                     self.pipeline.fit(self.full_train_data)
 
                     results = self.pipeline.predict(self.valid_data)
 
-                    self.metric_value = roc_auc(y_true=self.validing_data, y_score=results.predict, multi_class='ovo',
+                    self.metric_value = roc_auc(y_true=self.validing_data,
+                                                y_score=results.predict,
+                                                multi_class='ovo',
                                                 average='macro')
 
-                    rwd = 100 * self.metric_value
-                    self.render(mode='text')
-                    self.render(mode='graph')
-
-                    reward, done, info = self._env_response(reward=rwd, length=len(self.nodes))
-                    return self.observation, reward, done, info
-
-                except ValueError:
-                    self.render(mode='text')
-                    reward, done, info = self._env_response(reward=-85)
-                    return self.observation, reward, done, info
-
+                reward = 100 * self.metric_value
+                self.render(mode='graph')
+                _, done, info = self._env_response(length=len(self.nodes))
+                return self.observation, reward, done, info
+            except ValueError:
+                reward, done, info = self._env_response(reward=-85)
+                return self.observation, reward, done, info
+        else:
             reward, done, info = self._env_response(reward=0, done=False)
             return self.observation, reward, done, info
 
@@ -218,8 +195,8 @@ if __name__ == '__main__':
     file_path_train = 'cases/data/scoring/scoring_train.csv'
     full_path_train = os.path.join(str(fedot_project_root()), file_path_train)
 
-    file_path_test = 'cases/data/scoring/scoring_test.csv'
-    full_path_test = os.path.join(str(fedot_project_root()), file_path_test)
+    # file_path_test = 'cases/data/scoring/scoring_test.csv'
+    # full_path_test = os.path.join(str(fedot_project_root()), file_path_test)
 
     env = PipelineEnv(path_to_data=full_path_train)
 
