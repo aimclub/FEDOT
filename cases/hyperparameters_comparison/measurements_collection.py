7ba2652ca5428d477876e6f4279fb85839d77cbb
Nikita Gorokhov <nikita.gorokhov@gmail.com>
2022-04-12 15:18:04 +0300

    Changed separator to tab in measurements_collection.py

--- cases/hyperparameters_comparison/measurements_collection.py
+++ cases/hyperparameters_comparison/measurements_collection.py
@@ -2,6 +2,6 @@ import os
 
 txt = 'dataset,metrics,fitted_operation,approach,iterations,experiment_number,score_train,score_test,time_spent,memory_spent,pipeline'
 for f in os.listdir('comparison_results'):
-    txt += '\n' + ';'.join(f.split('_')) + ';' + open('comparison_results/' + f, 'r').read()
+    txt += '\n' + '\t'.join(f.split('_')) + '\t' + open('comparison_results/' + f, 'r').read()
 
 open('long_comparison_.csv', 'w').write(txt)
