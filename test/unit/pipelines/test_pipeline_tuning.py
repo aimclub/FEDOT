af07824a3b96c9709b515f5bbd8a2c3dd2bd267f
Nikita Gorokhov <nikita.gorokhov@gmail.com>
2022-04-18 14:46:31 +0300

    Added _calculate_loss_function testing

--- test/unit/pipelines/test_pipeline_tuning.py
+++ test/unit/pipelines/test_pipeline_tuning.py
@@ -15,7 +15,7 @@ from fedot.core.pipelines.node import PrimaryNode, SecondaryNode
 from fedot.core.pipelines.pipeline import Pipeline
 from fedot.core.pipelines.tuning.search_space import SearchSpace
 from fedot.core.pipelines.tuning.sequential import SequentialTuner
-from fedot.core.pipelines.tuning.tuner_interface import _greater_is_better
+from fedot.core.pipelines.tuning.tuner_interface import _greater_is_better, _calculate_loss_function
 from fedot.core.pipelines.tuning.unified import PipelineTuner
 from fedot.core.repository.dataset_types import DataTypesEnum
 from fedot.core.repository.tasks import Task, TaskTypesEnum
@@ -376,3 +376,37 @@ def test_greater_is_better():
     assert _greater_is_better(target, custom_maximized_metrics, None, data_type)
     assert not _greater_is_better(target, mse, None, data_type)
     assert not _greater_is_better(target, custom_minimized_metrics, None, data_type)
+
+
+def test_calculate_loss_function():
+    """ Tests _calculate_loss_function correctness on quality metrics"""
+    target = np.array([1, 0, 1, 0, 1])
+    multi_target = np.array([2, 0, 1, 0, 1])
+    regr_target = np.array([0.2, 0.1, 1, 0.3, 1.7])
+    pred_clear = np.array([1, 0, 1, 0, 0])
+    pred_prob = np.array([[0.2, 0.8],
+                          [0.7, 0.3],
+                          [0.4, 0.6],
+                          [0.51, 0.49],
+                          [0.51, 0.49]])
+    multi_pred_clear = np.array([2, 0, 1, 0, 2])
+    multi_pred_prob = np.array([[0.2, 0.3, 0.5],
+                                [0.6, 0.3, 0.1],
+                                [0.3, 0.4, 0.3],
+                                [0.5, 0.4, 0.1],
+                                [0.1, 0.4, 0.5]])
+    regr_pred = np.array([0.23, 0.15, 1.2, 0.4, 1.16])
+
+    assert _calculate_loss_function(acc, None, target, pred_prob) == 0.8
+    assert _calculate_loss_function(acc, None, target, pred_clear) == 0.8
+    assert np.allclose(a=_calculate_loss_function(roc, None, target, pred_prob),
+                       b=7 / 12,
+                       rtol=1e-9)
+
+    assert _calculate_loss_function(acc, None, multi_target, multi_pred_prob) == 0.8
+    assert _calculate_loss_function(acc, None, multi_target, multi_pred_clear) == 0.8
+    assert np.allclose(a=_calculate_loss_function(roc, {'multi_class': 'ovo'}, multi_target, multi_pred_prob),
+                       b=11 / 12,
+                       rtol=1e-9)
+
+    assert _calculate_loss_function(mse, None, regr_target, regr_pred) == 0.069
