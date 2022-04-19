Fixed PipelineTuner.get_metric_value calculation for binary+rf+accuracy-like case

fedot/core/pipelines/tuning/tuner_interface.py
@@ -219,7 +219,7 @@ class HyperoptTuner(ABC):
 
 def _create_multi_target_prediction(target):
     """ Function creates an array of shape (target len, num classes)
-    with classes probabilities from target values, used in _greater_is_better
+    with classes probabilities from target values
 
     :param target: target for define what problem is solving (max or min)
 
@@ -228,8 +228,7 @@ def _create_multi_target_prediction(target):
 
     len_target = target.shape[0]
 
-    multi_target = csr_matrix((np.ones(len_target), (np.arange(len_target),
-                                                     target.reshape((len_target,))))).A
+    multi_target = csr_matrix((np.ones(len_target), (np.arange(len_target), target.reshape((len_target,))))).A
 
     return multi_target
 
@@ -286,8 +285,11 @@ def _calculate_loss_function(loss_function, loss_params, target, preds):
         try:
             # transform class probabilities to 1st class probability, actual for binary auc_roc-like metrics
             metric_value = loss_function(target, preds[:, 1], **loss_params)
+        except IndexError:
+            # transform 1st class probability to assigned class, actual for accuracy-like metrics with binary
+            metric_value = loss_function(target, preds.round(), **loss_params)
         except ValueError:
-            # transform class probabilities to assigned class, actual for accuracy-like metrics
+            # transform class probabilities to assigned class, actual for accuracy-like metrics with multiclass
             metric_value = loss_function(target, np.argmax(preds, axis=1), **loss_params)
 
     return metric_value