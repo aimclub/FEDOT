79e942a5c58327d5f4275bd022173c506b58b39c
valer1435 <valp998@gmail.com>
2022-05-30 16:27:31 +0300

    add max_pipeline_fit_time

--- examples/simple/classification/api_classification.py
+++ examples/simple/classification/api_classification.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from fedot.api.main import Fedot
 from fedot.core.utils import fedot_project_root
 
@@ -8,13 +10,14 @@ def run_classification_example(timeout: float = None):
 
     problem = 'classification'
 
-    baseline_model = Fedot(problem=problem, timeout=timeout)
-    baseline_model.fit(features=train_data_path, target='target', predefined_model='rf')
+    # baseline_model = Fedot(problem=problem, timeout=timeout)
+    # baseline_model.fit(features=train_data_path, target='target', predefined_model='rf')
+    #
+    # baseline_model.predict(features=test_data_path)
+    # print(baseline_model.get_metrics())
 
-    baseline_model.predict(features=test_data_path)
-    print(baseline_model.get_metrics())
-
-    auto_model = Fedot(problem=problem, seed=42, timeout=timeout, n_jobs=1)
+    auto_model = Fedot(problem=problem, seed=42, timeout=timeout, n_jobs=1,
+                       composer_params={'max_pipeline_fit_time': timedelta(minutes=1)})
     auto_model.fit(features=train_data_path, target='target')
     prediction = auto_model.predict_proba(features=test_data_path)
     print(auto_model.get_metrics())
@@ -23,4 +26,4 @@ def run_classification_example(timeout: float = None):
 
 
 if __name__ == '__main__':
-    run_classification_example(timeout=4)
+    run_classification_example(timeout=1)
