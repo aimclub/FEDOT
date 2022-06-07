5073c19550fc96ac2b3e83040df466bdd513f4ef
Dreamlone <mik_sar@mail.ru>
2022-06-07 14:53:45 +0300

    fix ts pipelines generator

--- examples/simple/time_series_forecasting/ts_pipelines.py
+++ examples/simple/time_series_forecasting/ts_pipelines.py
@@ -1,5 +1,3 @@
-from fedot.core.pipelines.node import PrimaryNode, SecondaryNode
-from fedot.core.pipelines.pipeline import Pipeline
 from fedot.core.pipelines.pipeline_builder import PipelineBuilder
 
 
@@ -9,14 +7,14 @@ def ts_ets_pipeline():
     cut -> ets -> final forecast
     Where cut - cut part of dataset and ets - exponential smoothing
     """
-    node_cut = PrimaryNode("cut")
-    node_ets = SecondaryNode("ets", nodes_from=[node_cut])
-    node_ets.custom_params = {"error": "add",
-                              "trend": "add",
-                              "seasonal": "add",
-                              "damped_trend": False,
-                              "seasonal_periods": 20}
-    return Pipeline(node_ets)
+    pip_builder = PipelineBuilder().add_node('cut').add_node('ets',
+                                                             params={'error': 'add',
+                                                                     'trend': 'add',
+                                                                     'seasonal': 'add',
+                                                                     'damped_trend': False,
+                                                                     'seasonal_periods': 20})
+    pipeline = pip_builder.to_pipeline()
+    return pipeline
 
 
 def ts_ets_ridge_pipeline():
@@ -27,21 +25,15 @@ def ts_ets_ridge_pipeline():
     lagged - ridge /
     Where cut - cut part of dataset, ets - exponential smoothing
    """
-    node_cut = PrimaryNode("cut")
-    node_cut.custom_params = {"cut_part": 0.5}
-    node_ets = SecondaryNode("ets", nodes_from=[node_cut])
-    node_ets.custom_params = {"error": "add",
-                              "trend": "add",
-                              "seasonal": "add",
-                              "damped_trend": False,
-                              "seasonal_periods": 20}
+    pip_builder = PipelineBuilder()\
+        .add_sequence(('cut', {'cut_part': 0.5}),
+                      ('ets', {'error': 'add', 'trend': 'add', 'seasonal': 'add',
+                               'damped_trend': False, 'seasonal_periods': 20}),
+                      branch_idx=0)\
+        .add_sequence('lagged', 'ridge', branch_idx=1).join_branches('ridge')
 
-    node_lagged = PrimaryNode("lagged")
-    node_ridge_1 = SecondaryNode("ridge", nodes_from=[node_lagged])
-
-    node_ridge_2 = SecondaryNode("ridge", nodes_from=[node_ridge_1, node_ets])
-
-    return Pipeline(node_ridge_2)
+    pipeline = pip_builder.to_pipeline()
+    return pipeline
 
 
 def ts_glm_pipeline():
@@ -51,9 +43,8 @@ def ts_glm_pipeline():
 
     Where glm - Generalized linear model
     """
-    node_glm = PrimaryNode("glm")
-    node_glm.custom_params = {"family": "gaussian"}
-    return Pipeline(node_glm)
+    pipeline = PipelineBuilder().add_node('glm', params={'family': 'gaussian'}).to_pipeline()
+    return pipeline
 
 
 def ts_glm_ridge_pipeline():
@@ -65,14 +56,12 @@ def ts_glm_ridge_pipeline():
 
     Where glm - Generalized linear model
     """
-    node_glm = PrimaryNode("glm")
+    pip_builder = PipelineBuilder() \
+        .add_sequence('glm', branch_idx=0) \
+        .add_sequence('lagged', 'ridge', branch_idx=1).join_branches('ridge')
 
-    node_lagged = PrimaryNode("lagged")
-    node_ridge_1 = SecondaryNode("ridge", nodes_from=[node_lagged])
-
-    node_ridge_2 = SecondaryNode("ridge", nodes_from=[node_ridge_1, node_glm])
-
-    return Pipeline(node_ridge_2)
+    pipeline = pip_builder.to_pipeline()
+    return pipeline
 
 
 def ts_polyfit_pipeline(degree):
@@ -82,9 +71,8 @@ def ts_polyfit_pipeline(degree):
 
     Where polyfit - Polynomial interpolation
     """
-    node_polyfit = PrimaryNode("polyfit")
-    node_polyfit.custom_params = {"degree": degree}
-    return Pipeline(node_polyfit)
+    pipeline = PipelineBuilder().add_node('polyfit', params={'degree': degree}).to_pipeline()
+    return pipeline
 
 
 def ts_polyfit_ridge_pipeline(degree):
@@ -96,15 +84,12 @@ def ts_polyfit_ridge_pipeline(degree):
 
     Where polyfit - Polynomial interpolation
     """
-    node_polyfit = PrimaryNode("polyfit")
-    node_polyfit.custom_params = {"degree": degree}
+    pip_builder = PipelineBuilder() \
+        .add_sequence(('polyfit', {'degree': degree}), branch_idx=0) \
+        .add_sequence('lagged', 'ridge', branch_idx=1).join_branches('ridge')
 
-    node_lagged = PrimaryNode("lagged")
-    node_ridge_1 = SecondaryNode("ridge", nodes_from=[node_lagged])
-
-    node_ridge_2 = SecondaryNode("ridge", nodes_from=[node_ridge_1, node_polyfit])
-
-    return Pipeline(node_ridge_2)
+    pipeline = pip_builder.to_pipeline()
+    return pipeline
 
 
 def ts_complex_ridge_pipeline():
@@ -114,15 +99,11 @@ def ts_complex_ridge_pipeline():
                     -> ridge -> final forecast
     lagged - ridge /
     """
-    node_lagged_1 = PrimaryNode("lagged")
-    node_lagged_2 = PrimaryNode("lagged")
+    pip_builder = PipelineBuilder() \
+        .add_sequence('lagged', 'rigde', branch_idx=0) \
+        .add_sequence('lagged', 'ridge', branch_idx=1).join_branches('ridge')
 
-    node_ridge_1 = SecondaryNode("ridge", nodes_from=[node_lagged_1])
-    node_ridge_2 = SecondaryNode("ridge", nodes_from=[node_lagged_2])
-
-    node_final = SecondaryNode("ridge", nodes_from=[node_ridge_1, node_ridge_2])
-    pipeline = Pipeline(node_final)
-
+    pipeline = pip_builder.to_pipeline()
     return pipeline
 
 
@@ -137,20 +118,15 @@ def ts_complex_ridge_smoothing_pipeline():
 
     Where smoothing - rolling mean
     """
-    node_smoothing = PrimaryNode("smoothing")
-    node_lagged_1 = SecondaryNode("lagged", nodes_from=[node_smoothing])
-    node_lagged_2 = PrimaryNode("lagged")
+    pip_builder = PipelineBuilder() \
+        .add_sequence('smoothing', 'lagged', 'rigde', branch_idx=0) \
+        .add_sequence('lagged', 'ridge', branch_idx=1).join_branches('ridge')
 
-    node_ridge_1 = SecondaryNode("ridge", nodes_from=[node_lagged_1])
-    node_ridge_2 = SecondaryNode("ridge", nodes_from=[node_lagged_2])
-
-    node_final = SecondaryNode("ridge", nodes_from=[node_ridge_1, node_ridge_2])
-    pipeline = Pipeline(node_final)
-
+    pipeline = pip_builder.to_pipeline()
     return pipeline
 
 
-def ts_complex_dtreg_pipeline(first_node="lagged"):
+def ts_complex_dtreg_pipeline(first_node='lagged'):
     """
         Return pipeline with the following structure:
 
@@ -160,12 +136,11 @@ def ts_complex_dtreg_pipeline(first_node="lagged"):
 
         Where dtreg = tree regressor, rfr - random forest regressor
     """
-    node_lagged_1 = PrimaryNode(first_node)
-    node_lagged_2 = PrimaryNode(first_node)
-    node_dtreg_1 = SecondaryNode("dtreg", nodes_from=[node_lagged_1])
-    node_dtreg_2 = SecondaryNode("dtreg", nodes_from=[node_lagged_2])
-    node_final = SecondaryNode("rfr", nodes_from=[node_dtreg_1, node_dtreg_2])
-    pipeline = Pipeline(node_final)
+    pip_builder = PipelineBuilder() \
+        .add_sequence(first_node, 'dtreg', branch_idx=0) \
+        .add_sequence(first_node, 'dtreg', branch_idx=1).join_branches('rfr')
+
+    pipeline = pip_builder.to_pipeline()
     return pipeline
 
 
@@ -180,11 +155,13 @@ def ts_multiple_ets_pipeline():
 
     Where ets - exponential_smoothing
     """
-    node_ets2 = PrimaryNode("ets")
-    node_ets = PrimaryNode("ets")
-    node_ets3 = PrimaryNode("ets")
-    final_lasso = SecondaryNode('lasso', nodes_from=[node_ets, node_ets2, node_ets3])
-    pipeline = Pipeline(final_lasso)
+    pip_builder = PipelineBuilder() \
+        .add_sequence('ets', branch_idx=0) \
+        .add_sequence('ets', branch_idx=1) \
+        .add_sequence('ets', branch_idx=2) \
+        .join_branches('lasso')
+
+    pipeline = pip_builder.to_pipeline()
     return pipeline
 
 
@@ -195,8 +172,7 @@ def ts_ar_pipeline():
 
     Where ar - auto regression
     """
-    node_ar = PrimaryNode("ar")
-    pipeline = Pipeline(node_ar)
+    pipeline = PipelineBuilder().add_node('ar').to_pipeline()
     return pipeline
 
 
@@ -220,17 +196,37 @@ def ts_stl_arima_pipeline():
     return pipeline
 
 
-def ts_locf_forecast_pipeline():
-    locf_node = PrimaryNode("locf")
-    lagged_node = PrimaryNode("lagged")
-    pipeline = Pipeline(SecondaryNode("ridge", nodes_from=[locf_node, lagged_node]))
+def ts_locf_ridge_pipeline():
+    """
+    Pipeline with naive LOCF (last observation carried forward) model
+    and lagged features
+     locf  \
+            ridge -> final forecast
+    lagged /
+    """
+    pip_builder = PipelineBuilder() \
+        .add_sequence('locf', branch_idx=0) \
+        .add_sequence('lagged', branch_idx=1) \
+        .join_branches('ridge')
+
+    pipeline = pip_builder.to_pipeline()
     return pipeline
 
 
-def ts_naive_average_pipeline():
-    naive_average_node = PrimaryNode("ts_naive_average")
-    lagged_node = PrimaryNode("lagged")
-    pipeline = Pipeline(SecondaryNode("ridge", nodes_from=[naive_average_node, lagged_node]))
+def ts_naive_average_ridge_pipeline():
+    """
+    Pipeline with simple forecasting model (the forecast is mean value for known
+    part)
+    ts_naive_average \
+                      ridge -> final forecast
+         lagged      /
+    """
+    pip_builder = PipelineBuilder() \
+        .add_sequence('ts_naive_average', branch_idx=0) \
+        .add_sequence('lagged', branch_idx=1) \
+        .join_branches('ridge')
+
+    pipeline = pip_builder.to_pipeline()
     return pipeline
 
 
@@ -243,21 +239,15 @@ def clstm_pipeline():
 
     Where clstm - convolutional long short-term memory model
     """
-    node_lagged_1 = PrimaryNode("lagged")
+    clstm_params = {'window_size': 29, 'hidden_size': 50, 'learning_rate': 0.004,
+                    'cnn1_kernel_size': 5, 'cnn1_output_size': 32,
+                    'cnn2_kernel_size': 4, 'cnn2_output_size': 32,
+                    'batch_size': 64, 'num_epochs': 3}
 
-    node_ridge_1 = SecondaryNode("ridge", nodes_from=[node_lagged_1])
-    node_clstm = PrimaryNode("clstm")
-    node_clstm.custom_params = {"window_size": 29,
-                                "hidden_size": 50,
-                                "learning_rate": 0.004,
-                                "cnn1_kernel_size": 5,
-                                "cnn1_output_size": 32,
-                                "cnn2_kernel_size": 4,
-                                "cnn2_output_size": 32,
-                                "batch_size": 64,
-                                "num_epochs": 3}
+    pip_builder = PipelineBuilder() \
+        .add_sequence('lagged', 'ridge', branch_idx=0) \
+        .add_sequence(('clstm', clstm_params), branch_idx=1) \
+        .join_branches('ridge')
 
-    node_final = SecondaryNode("ridge", nodes_from=[node_ridge_1, node_clstm])
-    pipeline = Pipeline(node_final)
-
+    pipeline = pip_builder.to_pipeline()
     return pipeline
