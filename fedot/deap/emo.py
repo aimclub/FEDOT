7c7d0d0747e6933a0000357e2c9e11ac2dcb0c06
Mattias Aabmets <mattias.aabmets@gmail.com>
2022-06-10 14:53:38 +0300

    license fix

--- fedot/deap/emo.py
+++ fedot/deap/emo.py
@@ -2,7 +2,7 @@
 #                                                                                        #
 #   MIT License                                                                          #
 #                                                                                        #
-#   Copyright (c) 2022 The Original DEAP Team, Mattias Aabmets and Contributors          #
+#   Copyright (c) 2022 The Original DEAP Team and The FEDOT Team                         #
 #                                                                                        #
 #   Permission is hereby granted, free of charge, to any person obtaining a copy         #
 #   of this software and associated documentation files (the "Software"), to deal        #
