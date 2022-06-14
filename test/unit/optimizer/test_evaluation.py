012e1bb7823f92cf50b63a6651e0fa1f0e99d7bb
YamLyubov <yamlyubov@gmail.com>
2022-06-14 17:12:08 +0300

    Tests for MultipricessingDispatcher

--- test/unit/optimizer/test_evaluation.py
+++ test/unit/optimizer/test_evaluation.py
@@ -1,114 +1,98 @@
-import os
-from functools import partial
+import datetime
 
 import pytest
 
-from fedot.core.data.data import InputData
-from fedot.core.optimisers.gp_comp.evaluation import MultiprocessingDispatcher, SimpleDispatcher
 from fedot.core.optimisers.adapters import PipelineAdapter
+from fedot.core.optimisers.fitness import Fitness, null_fitness, SingleObjFitness
+from fedot.core.optimisers.gp_comp.evaluation import MultiprocessingDispatcher
+from fedot.core.optimisers.gp_comp.individual import Individual
 from fedot.core.optimisers.objective import Objective
-from fedot.core.pipelines.node import PrimaryNode, SecondaryNode
+from fedot.core.optimisers.timer import OptimisationTimer
 from fedot.core.pipelines.pipeline import Pipeline
 from fedot.core.repository.quality_metrics_repository import ClassificationMetricsEnum
-from fedot.core.pipelines.pipeline_builder import PipelineBuilder
-from fedot.core.optimisers.gp_comp.individual import Individual
-from fedot.core.repository.tasks import Task, TaskTypesEnum
-from test.unit.api.test_api_cli_params import project_root_path
-from test.unit.models.test_model import classification_dataset
+from test.unit.pipelines.test_node_cache import pipeline_first, pipeline_second, pipeline_third, pipeline_fourth
+from test.unit.validation.test_table_cv import get_classification_data
 
 
+def prepared_objective(pipeline: Pipeline) -> Fitness:
+    train_data = get_classification_data()
+    pipeline.fit(train_data)
 
-def first_pipeline():
-    """
-    Returns pipeline with structure
-    knn -> prediction
-    """
-    pipeline_builder = PipelineBuilder().add_node('knn')
-    pipeline = pipeline_builder.to_pipeline()
-    return pipeline
-
-
-def second_pipeline():
-    """
-    Returns pipeline with structure
-      knn
-         \
-           logit -> prediction
-         /
-      svc
-    """
-    pipeline_builder = PipelineBuilder()\
-        .add_sequence('knn', branch_idx=0)\
-        .add_sequence('svc', branch_idx=1)\
-        .join_branches('logit')
-    pipeline = pipeline_builder.to_pipeline()
-    return pipeline
-
-
-def third_pipeline():
-    """
-    Returns pipeline with structure
-    rf -> logit -> prediction
-    """
-    pipeline_builder = PipelineBuilder()\
-        .add_node('rf')\
-        .add_node('logit')
-    pipeline = pipeline_builder.to_pipeline()
-    return pipeline
-
-
-def fourth_pipeline():
-    """
-    Returns pipeline with structure
-    rf -> logit
-               \
-                svc -> rf -> prediction
-               |
-            scv
-    """
-    pipeline_builder = PipelineBuilder()\
-        .add_sequence('rf', 'logit', branch_idx=0)\
-        .add_sequence('svc', branch_idx=1)\
-        .join_branches('svc')
-    pipeline = pipeline_builder.to_pipeline()
-    return pipeline
-
-
-def classification_random_forest_pipeline():
-    """
-    Returns pipeline with the following structure:
-
-    scaling -> rf -> final prediction
-    """
-    node_scaling = PrimaryNode('scaling')
-    node_final = SecondaryNode('rf', nodes_from=[node_scaling])
-    return Pipeline(node_final)
-
-
-def sample_pipeline():
-    return Pipeline(SecondaryNode(operation_type='logit',
-                                  nodes_from=[PrimaryNode(operation_type='rf'),
-                                              PrimaryNode(operation_type='scaling')]))
-
-
-def get_classification_data():
-    file_path = os.path.join(project_root_path, 'test/data/simple_classification.csv')
-    input_data = InputData.from_csv(file_path, task=Task(TaskTypesEnum.classification))
-    return input_data
-
-
-def test_multiprocessingdispatcher_without_timelimit_without_multiprocessing(classification_dataset):
     metric = ClassificationMetricsEnum.accuracy
     objective = Objective(metric)
-    # input_data = get_classification_data()
-    input_data = classification_dataset
+    return objective(pipeline, reference_data=train_data)
 
-    objective_function_with_data = partial(objective, reference_data=input_data)
+
+def invalid_objective(pipeline: Pipeline) -> Fitness:
+    return null_fitness()
+
+
+def throwing_exception_objective(pipeline: Pipeline) -> Fitness:
+    x = 1/0
+    return SingleObjFitness(x)
+
+
+def test_multiprocessing_dispatcher_without_timelimit_without_multiprocessing():
     adapter = PipelineAdapter()
-    evaluator = SimpleDispatcher(adapter).dispatch(objective_function_with_data)
-
-    # pipelines = [first_pipeline(), second_pipeline(), third_pipeline(), fourth_pipeline()]
-    pipelines = [sample_pipeline()]
-    list(map(lambda x: x.fit(input_data=input_data), pipelines))
+    pipelines = [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth()]
     population = [Individual(adapter.adapt(pipeline)) for pipeline in pipelines]
-    evaluator(population)
+
+    evaluator = MultiprocessingDispatcher(adapter).dispatch(prepared_objective)
+    evaluated_population = evaluator(population)
+    fitness = list(map(lambda x: x.fitness, evaluated_population))
+    assert all(x.valid for x in fitness), "At least one fitness value is invalid"
+    assert len(pipelines) == len(evaluated_population), "Not all pipelines was evaluated"
+
+
+def test_multiprocessing_with_multiprocessing():
+    adapter = PipelineAdapter()
+    pipelines = [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth()]
+    population = [Individual(adapter.adapt(pipeline)) for pipeline in pipelines]
+
+    evaluator = MultiprocessingDispatcher(adapter, n_jobs=-1).dispatch(prepared_objective)
+    evaluated_population = evaluator(population)
+    fitness = list(map(lambda x: x.fitness, evaluated_population))
+    assert all(x.valid for x in fitness), "At least one fitness value is invalid"
+    assert len(pipelines) == len(evaluated_population), "Not all pipelines was evaluated"
+
+
+def test_multiprocessing_dispatcher_with_timelimit():
+    adapter = PipelineAdapter()
+    pipelines = [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth()]
+    population = [Individual(adapter.adapt(pipeline)) for pipeline in pipelines]
+
+    timeout = datetime.timedelta(minutes=0.001)
+    with OptimisationTimer(timeout=timeout) as t:
+        evaluator = MultiprocessingDispatcher(adapter, timer=t).dispatch(prepared_objective)
+        evaluated_population = evaluator(population)
+    fitness = list(map(lambda x: x.fitness, evaluated_population))
+    assert all(x.valid for x in fitness), "At least one fitness value is invalid"
+    assert len(evaluated_population) >= 1, "At least one pipeline is evaluated"
+
+    timeout = datetime.timedelta(minutes=5)
+    with OptimisationTimer(timeout=timeout) as t:
+        evaluator = MultiprocessingDispatcher(adapter, timer=t).dispatch(prepared_objective)
+        evaluated_population = evaluator(population)
+    fitness = list(map(lambda x: x.fitness, evaluated_population))
+    assert all(x.valid for x in fitness), "At least one fitness value is invalid"
+    assert len(pipelines) == len(evaluated_population), "Not all pipelines was evaluated"
+
+
+def test_multiprocessing_with_invalid_objective():
+    adapter = PipelineAdapter()
+    pipelines = [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth()]
+    population = [Individual(adapter.adapt(pipeline)) for pipeline in pipelines]
+
+    evaluator = MultiprocessingDispatcher(adapter).dispatch(invalid_objective)
+    with pytest.raises(AttributeError):
+        evaluator(population)
+
+
+def test_multiprocessing_with_objective_throwing_exception():
+    adapter = PipelineAdapter()
+    pipelines = [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth()]
+    population = [Individual(adapter.adapt(pipeline)) for pipeline in pipelines]
+
+    evaluator = MultiprocessingDispatcher(adapter).dispatch(throwing_exception_objective)
+    with pytest.raises(ZeroDivisionError):
+        evaluator(population)
