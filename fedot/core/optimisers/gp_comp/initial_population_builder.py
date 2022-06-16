7c616977781da3a92d49fc7d80c74e6db4babb1f
Grigorii Kirgizov <gkirgizov@gmail.com>
2022-06-16 16:59:39 +0300

    Fix one usage of GraphVerifier

--- fedot/core/optimisers/gp_comp/initial_population_builder.py
+++ fedot/core/optimisers/gp_comp/initial_population_builder.py
@@ -2,13 +2,11 @@ from typing import Callable, Optional, Sequence
 
 import numpy as np
 
-from fedot.core.composer.constraint import constraint_function
 from fedot.core.log import Log
 from fedot.core.optimisers.gp_comp.individual import Individual
 from fedot.core.optimisers.gp_comp.operators.operator import Operator, PopulationT
 from fedot.core.optimisers.graph import OptGraph
-from fedot.core.optimisers.optimizer import GraphGenerationParams
-
+from fedot.core.pipelines.verification import GraphVerifier
 GraphSampler = Callable[[], OptGraph]
 IndividualSampler = Callable[[], Individual]
 
@@ -20,8 +18,8 @@ class InitialPopulationBuilder:
 
     _max_generation_attempts = 1000
 
-    def __init__(self, graph_generation_params: GraphGenerationParams, log: Log):
-        self.graph_gen_params = graph_generation_params
+    def __init__(self, verifier: GraphVerifier, log: Log):
+        self.verifier = verifier
         self.mutation_operator: Callable[[Individual], Individual] = lambda ind: ind
         self.individual_sampler: Optional[IndividualSampler] = None
         self.initial_individuals: Sequence[Individual] = ()
@@ -54,7 +52,7 @@ class InitialPopulationBuilder:
         while len(population) < pop_size:
             sampled_ind = self.individual_sampler()
             new_ind = self.mutation_operator(sampled_ind)
-            if constraint_function(new_ind.graph, self.graph_gen_params) and new_ind not in population:
+            if new_ind not in population and self.verifier(new_ind.graph):
                 population.append(new_ind)
             n_iter += 1
             if n_iter >= self._max_generation_attempts:
