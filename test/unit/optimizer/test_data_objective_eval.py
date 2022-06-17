6b80bb58c442f10be2d70bc27a7a3c2b6bcaabf2
YamLyubov <yamlyubov@gmail.com>
2022-06-17 12:54:33 +0300

    Added fixtures and parametrisation for tests. PipelineObjectiveEvaluate is tested to return actual value of a metric.

--- test/unit/optimizer/test_data_objective_eval.py
+++ test/unit/optimizer/test_data_objective_eval.py
@@ -1,21 +1,21 @@
 from functools import partial
-from numbers import Real
 
 import numpy as np
 import pytest
 
-from fedot.core.dag.graph import Graph
 from fedot.core.data.data import InputData
 from fedot.core.data.supplementary_data import SupplementaryData
 from fedot.core.log import default_log
 from fedot.core.optimisers.objective import Objective, PipelineObjectiveEvaluate
 from fedot.core.pipelines.pipeline import Pipeline
 from fedot.core.repository.dataset_types import DataTypesEnum
-from fedot.core.repository.quality_metrics_repository import ClassificationMetricsEnum
+from fedot.core.repository.quality_metrics_repository import ClassificationMetricsEnum, MetricsRepository
 from fedot.core.repository.tasks import Task, TaskTypesEnum
 from fedot.core.validation.split import tabular_cv_generator, OneFoldInputDataSplit
 from test.unit.models.test_model import classification_dataset
-from test.unit.validation.test_table_cv import sample_pipeline
+from test.unit.pipelines.test_node_cache import pipeline_first, pipeline_second, pipeline_third, pipeline_fourth, \
+    pipeline_fifth
+from test.unit.validation.test_table_cv import sample_pipeline, get_classification_data
 
 _ = classification_dataset
 
@@ -28,6 +28,16 @@ def throwing_exception_metric(*args, **kwargs):
     raise Exception
 
 
+def actual_fitness(data_split, pipeline, metric):
+    for (train_input, test_input) in data_split():
+        pipeline.fit(train_input)
+
+        # predicted = pipeline.predict(test_input)
+        metric_function = MetricsRepository().metric_by_id(metric, default_callable=metric)
+        metric_value = metric_function(pipeline=pipeline, reference_data=test_input)
+    return metric_value
+
+
 def empty_datasource():
     task = Task(TaskTypesEnum.classification)
     features = np.array([])
@@ -38,21 +48,22 @@ def empty_datasource():
     yield input_data, input_data
 
 
-def test_pipeline_objective_evaluate_with_different_metrics(classification_dataset):
-    pipeline = sample_pipeline()
+@pytest.mark.parametrize(
+    'pipeline',
+    [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth(), pipeline_fifth()]
+)
+def test_pipeline_objective_evaluate_with_different_metrics(classification_dataset, pipeline):
     log = default_log(__name__)
 
     data_split = partial(OneFoldInputDataSplit().input_split, input_data=classification_dataset)
-    metrics = [ClassificationMetricsEnum.ROCAUC_penalty,
-               ClassificationMetricsEnum.accuracy,
-               ClassificationMetricsEnum.logloss,
-               ClassificationMetricsEnum.ROCAUC,
-               ClassificationMetricsEnum.precision,
-               ClassificationMetricsEnum.f1]
-    for metric in metrics:
+    for metric in ClassificationMetricsEnum:
         objective_eval = PipelineObjectiveEvaluate(Objective(metric), data_split, log=log)
         fitness = objective_eval(pipeline)
+        act_fitness = actual_fitness(data_split, sample_pipeline(), metric)
+        print(fitness.value, act_fitness)
         assert fitness.valid
+        assert fitness.value is not None
+        assert abs(fitness.value - act_fitness) < 0.016
 
 
 def test_pipeline_objective_evaluate_with_empty_pipeline(classification_dataset):
@@ -79,48 +90,6 @@ def test_pipeline_objective_evaluate_with_cv_fold(classification_dataset):
     assert fitness.valid
 
 
-def test_pipeline_objective_evaluate_with_time_constraint(classification_dataset):
-    pipeline = sample_pipeline()
-    log = default_log(__name__)
-
-    data_split = partial(OneFoldInputDataSplit().input_split, input_data=classification_dataset)
-    metric = ClassificationMetricsEnum.ROCAUC_penalty
-
-    timelimit = 0.0001
-    objective_eval = PipelineObjectiveEvaluate(Objective(metric), data_split, time_constraint=timelimit, log=log)
-    fitness = objective_eval(pipeline)
-    assert not fitness.valid
-
-    timelimit = 300
-    objective_eval = PipelineObjectiveEvaluate(Objective(metric), data_split, time_constraint=timelimit, log=log)
-    fitness = objective_eval(pipeline)
-    assert fitness.valid
-
-
-def test_pipeline_objective_evaluate_with_empty_metrics(classification_dataset):
-    pipeline = sample_pipeline()
-    log = default_log(__name__)
-
-    data_split = partial(OneFoldInputDataSplit().input_split, input_data=classification_dataset)
-    metrics = []
-
-    objective_eval = PipelineObjectiveEvaluate(Objective(metrics), data_split, log=log)
-    fitness = objective_eval(pipeline)
-    assert not fitness.valid
-
-
-def test_pipeline_objective_evaluate_with_throwing_exception_metrics(classification_dataset):
-    pipeline = sample_pipeline()
-    log = default_log(__name__)
-
-    data_split = partial(OneFoldInputDataSplit().input_split, input_data=classification_dataset)
-    metric = throwing_exception_metric
-
-    objective_eval = PipelineObjectiveEvaluate(Objective(metric), data_split, log=log)
-    fitness = objective_eval(pipeline)
-    assert not fitness.valid
-
-
 def test_pipeline_objective_evaluate_with_empty_datasource(classification_dataset):
     pipeline = sample_pipeline()
     log = default_log(__name__)
@@ -129,5 +98,39 @@ def test_pipeline_objective_evaluate_with_empty_datasource(classification_datase
     metric = ClassificationMetricsEnum.ROCAUC_penalty
 
     objective_eval = PipelineObjectiveEvaluate(Objective(metric), data_split, log=log)
+    fitness = objective_eval(pipeline)
+    assert not fitness.valid
+
+
+def test_pipeline_objective_evaluate_with_time_constraint(classification_dataset):
+    pipeline = sample_pipeline()
+    log = default_log(__name__)
+
+    data_split = partial(OneFoldInputDataSplit().input_split, input_data=classification_dataset)
+    metric = ClassificationMetricsEnum.ROCAUC_penalty
+
+    time_constraint = 0.0001
+    objective_eval = PipelineObjectiveEvaluate(Objective(metric), data_split, time_constraint=time_constraint, log=log)
+    fitness = objective_eval(pipeline)
+    assert not fitness.valid
+
+    time_constraint = 300
+    objective_eval = PipelineObjectiveEvaluate(Objective(metric), data_split, time_constraint=time_constraint, log=log)
+    fitness = objective_eval(pipeline)
+    assert fitness.valid
+
+
+@pytest.mark.parametrize(
+    'metrics',
+    [[],
+     throwing_exception_metric]
+)
+def test_pipeline_objective_evaluate_with_invalid_metrics(classification_dataset, metrics):
+    pipeline = sample_pipeline()
+    log = default_log(__name__)
+
+    data_split = partial(OneFoldInputDataSplit().input_split, input_data=classification_dataset)
+
+    objective_eval = PipelineObjectiveEvaluate(Objective(metrics), data_split, log=log)
     fitness = objective_eval(pipeline)
     assert not fitness.valid
--- test/unit/optimizer/test_evaluation.py
+++ test/unit/optimizer/test_evaluation.py
@@ -3,8 +3,8 @@ import datetime
 import pytest
 
 from fedot.core.optimisers.adapters import PipelineAdapter
-from fedot.core.optimisers.fitness import Fitness, null_fitness, SingleObjFitness
-from fedot.core.optimisers.gp_comp.evaluation import MultiprocessingDispatcher
+from fedot.core.optimisers.fitness import Fitness, null_fitness
+from fedot.core.optimisers.gp_comp.evaluation import MultiprocessingDispatcher, SimpleDispatcher
 from fedot.core.optimisers.gp_comp.individual import Individual
 from fedot.core.optimisers.objective import Objective
 from fedot.core.optimisers.timer import OptimisationTimer
@@ -12,6 +12,14 @@ from fedot.core.pipelines.pipeline import Pipeline
 from fedot.core.repository.quality_metrics_repository import ClassificationMetricsEnum
 from test.unit.pipelines.test_node_cache import pipeline_first, pipeline_second, pipeline_third, pipeline_fourth
 from test.unit.validation.test_table_cv import get_classification_data
+
+
+@pytest.fixture(scope="module")
+def set_up_tests():
+    adapter = PipelineAdapter()
+    pipelines = [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth()]
+    population = [Individual(adapter.adapt(pipeline)) for pipeline in pipelines]
+    return adapter, population
 
 
 def prepared_objective(pipeline: Pipeline) -> Fitness:
@@ -27,51 +35,34 @@ def invalid_objective(pipeline: Pipeline) -> Fitness:
     return null_fitness()
 
 
-def throwing_exception_objective(pipeline: Pipeline) -> Fitness:
-    x = 1/0
-    return SingleObjFitness(x)
+def throwing_exception_objective(*args, **kwargs):
+    raise Exception
 
 
-def test_multiprocessing_dispatcher_without_timeout_without_multiprocessing():
-    adapter = PipelineAdapter()
-    pipelines = [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth()]
-    population = [Individual(adapter.adapt(pipeline)) for pipeline in pipelines]
+@pytest.mark.parametrize(
+    'dispatcher',
+    [MultiprocessingDispatcher(PipelineAdapter()),
+     MultiprocessingDispatcher(PipelineAdapter(), n_jobs=-1),
+     MultiprocessingDispatcher(PipelineAdapter(), n_jobs=1)]
+)
+def test_multiprocessing_dispatcher_with_and_without_multiprocessing(set_up_tests, dispatcher):
+    _, population = set_up_tests
 
-    evaluator = MultiprocessingDispatcher(adapter).dispatch(prepared_objective)
+    evaluator = dispatcher.dispatch(prepared_objective)
     evaluated_population = evaluator(population)
-    fitness = list(map(lambda x: x.fitness, evaluated_population))
+    fitness = [x.fitness for x in evaluated_population]
     assert all(x.valid for x in fitness), "At least one fitness value is invalid"
-    assert len(pipelines) == len(evaluated_population), "Not all pipelines was evaluated"
+    assert len(population) == len(evaluated_population), "Not all pipelines was evaluated"
 
 
-def test_multiprocessing_dispatcher_with_multiprocessing():
-    adapter = PipelineAdapter()
-    pipelines = [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth()]
-    population = [Individual(adapter.adapt(pipeline)) for pipeline in pipelines]
-
-    evaluator = MultiprocessingDispatcher(adapter, n_jobs=-1).dispatch(prepared_objective)
-    evaluated_population = evaluator(population)
-    fitness = list(map(lambda x: x.fitness, evaluated_population))
-    assert all(x.valid for x in fitness), "At least one fitness value is invalid"
-    assert len(pipelines) == len(evaluated_population), "Not all pipelines was evaluated"
-
-    evaluator = MultiprocessingDispatcher(adapter, n_jobs=1).dispatch(prepared_objective)
-    evaluated_population = evaluator(population)
-    fitness = list(map(lambda x: x.fitness, evaluated_population))
-    assert all(x.valid for x in fitness), "At least one fitness value is invalid"
-    assert len(pipelines) == len(evaluated_population), "Not all pipelines was evaluated"
-
-
-def test_multiprocessing_dispatcher_with_timeout():
-    adapter = PipelineAdapter()
-    pipelines = [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth()]
-    population = [Individual(adapter.adapt(pipeline)) for pipeline in pipelines]
+def test_multiprocessing_dispatcher_with_timeout(set_up_tests):
+    adapter, population = set_up_tests
 
     timeout = datetime.timedelta(minutes=0.001)
     with OptimisationTimer(timeout=timeout) as t:
         evaluator = MultiprocessingDispatcher(adapter, timer=t).dispatch(prepared_objective)
         evaluated_population = evaluator(population)
-    fitness = list(map(lambda x: x.fitness, evaluated_population))
+    fitness = [x.fitness for x in evaluated_population]
     assert all(x.valid for x in fitness), "At least one fitness value is invalid"
     assert len(evaluated_population) >= 1, "At least one pipeline is evaluated"
 
@@ -79,26 +70,68 @@ def test_multiprocessing_dispatcher_with_timeout():
     with OptimisationTimer(timeout=timeout) as t:
         evaluator = MultiprocessingDispatcher(adapter, timer=t).dispatch(prepared_objective)
         evaluated_population = evaluator(population)
-    fitness = list(map(lambda x: x.fitness, evaluated_population))
+    fitness = [x.fitness for x in evaluated_population]
     assert all(x.valid for x in fitness), "At least one fitness value is invalid"
-    assert len(pipelines) == len(evaluated_population), "Not all pipelines was evaluated"
+    assert len(population) == len(evaluated_population), "Not all pipelines was evaluated"
 
 
-def test_multiprocessing_dispatcher_with_invalid_objective():
-    adapter = PipelineAdapter()
-    pipelines = [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth()]
-    population = [Individual(adapter.adapt(pipeline)) for pipeline in pipelines]
+def test_multiprocessing_dispatcher_with_invalid_objective(set_up_tests):
+    adapter, population = set_up_tests
 
     evaluator = MultiprocessingDispatcher(adapter).dispatch(invalid_objective)
     with pytest.raises(AttributeError):
         evaluator(population)
 
 
-def test_multiprocessing_dispatcher_with_objective_throwing_exception():
-    adapter = PipelineAdapter()
-    pipelines = [pipeline_first(), pipeline_second(), pipeline_third(), pipeline_fourth()]
-    population = [Individual(adapter.adapt(pipeline)) for pipeline in pipelines]
+def test_multiprocessing_dispatcher_with_objective_throwing_exception(set_up_tests):
+    adapter, population = set_up_tests
 
     evaluator = MultiprocessingDispatcher(adapter).dispatch(throwing_exception_objective)
-    with pytest.raises(ZeroDivisionError):
+    with pytest.raises(Exception):
+        evaluator(population)
+
+
+def test_simple_dispatcher_without_timeout(set_up_tests):
+    adapter, population = set_up_tests
+
+    evaluator = SimpleDispatcher(adapter).dispatch(prepared_objective)
+    evaluated_population = evaluator(population)
+    fitness = [x.fitness for x in evaluated_population]
+    assert all(x.valid for x in fitness), "At least one fitness value is invalid"
+    assert len(population) == len(evaluated_population), "Not all pipelines was evaluated"
+
+
+def test_simple_dispatcher_with_timeout(set_up_tests):
+    adapter, population = set_up_tests
+
+    timeout = datetime.timedelta(minutes=0.001)
+    with OptimisationTimer(timeout=timeout) as t:
+        evaluator = SimpleDispatcher(adapter, timer=t).dispatch(prepared_objective)
+        evaluated_population = evaluator(population)
+    fitness = [x.fitness for x in evaluated_population]
+    assert all(x.valid for x in fitness), "At least one fitness value is invalid"
+    assert len(evaluated_population) < len(population), "Not all pipelines should be evaluated (not enough time)"
+
+    timeout = datetime.timedelta(minutes=5)
+    with OptimisationTimer(timeout=timeout) as t:
+        evaluator = SimpleDispatcher(adapter, timer=t).dispatch(prepared_objective)
+        evaluated_population = evaluator(population)
+    fitness = [x.fitness for x in evaluated_population]
+    assert all(x.valid for x in fitness), "At least one fitness value is invalid"
+    assert len(population) == len(evaluated_population), "Not all pipelines was evaluated"
+
+
+def test_simple_dispatcher_with_invalid_objective(set_up_tests):
+    adapter, population = set_up_tests
+
+    evaluator = SimpleDispatcher(adapter).dispatch(invalid_objective)
+    with pytest.raises(AttributeError):
+        evaluator(population)
+
+
+def test_simple_dispatcher_with_objective_throwing_exception(set_up_tests):
+    adapter, population = set_up_tests
+
+    evaluator = SimpleDispatcher(adapter).dispatch(throwing_exception_objective)
+    with pytest.raises(Exception):
         evaluator(population)
