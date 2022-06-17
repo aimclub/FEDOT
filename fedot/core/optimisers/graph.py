14d766fe4c9764a824777aa65312e9d90378b843
gkirgizov <gkirgizov@gmail.com>
2022-06-17 15:32:10 +0300

    WIP Graph hierarchy refactoring

--- fedot/core/optimisers/graph.py
+++ fedot/core/optimisers/graph.py
@@ -1,8 +1,9 @@
 from os import PathLike
 from copy import deepcopy
-from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
+from typing import Any, Callable, Iterable, List, Optional, Union, Sequence, Tuple
 from uuid import uuid4
 
+from fedot.core.dag.graph import Graph
 from fedot.core.dag.graph_node import GraphNode
 from fedot.core.dag.graph_operator import GraphOperator
 from fedot.core.dag.node_operator import NodeOperator
@@ -20,7 +21,7 @@ def node_ops_adaptation(func: Callable) -> Callable:
 
     def _decorator(self, *args, **kwargs) -> Any:
         func_result = func(self, *args, **kwargs)
-        self._nodes = [_adapt(self._node_adapter, node) for node in self.nodes]
+        self.nodes = [_adapt(self._node_adapter, node) for node in self.nodes]
         return func_result
 
     return _decorator
@@ -113,7 +114,7 @@ class OptNode:
         return self._operator.distance_to_primary_level()
 
 
-class OptGraph:
+class OptGraph(Graph):
     """Base class used for optimized structure
 
     :param nodes: optimization graph nodes object(s)
@@ -121,11 +122,15 @@ class OptGraph:
 
     def __init__(self, nodes: Union[OptNode, List[OptNode]] = ()):
         self.log = default_log(self)
-        self._operator = GraphOperator(ensure_wrapped_in_sequence(nodes))
+        self.operator = GraphOperator(nodes)
 
     @property
-    def nodes(self):
-        return self._operator.nodes
+    def nodes(self) -> List[OptNode]:
+        return self.operator.nodes
+
+    @nodes.setter
+    def nodes(self, new_nodes: List[OptNode]):
+        self.operator.nodes = new_nodes
 
     @property
     def _node_adapter(self):
@@ -142,7 +147,7 @@ class OptGraph:
 
         :param new_node: new OptNode object
         """
-        self._operator.add_node(self._node_adapter.restore(new_node))
+        self.operator.add_node(self._node_adapter.restore(new_node))
 
     @node_ops_adaptation
     def update_node(self, old_node: OptNode, new_node: OptNode):
@@ -153,8 +158,8 @@ class OptGraph:
         :param new_node: OptNode object to replace
         """
 
-        self._operator.update_node(self._node_adapter.restore(old_node),
-                                   self._node_adapter.restore(new_node))
+        self.operator.update_node(self._node_adapter.restore(old_node),
+                                  self._node_adapter.restore(new_node))
 
     @node_ops_adaptation
     def update_subtree(self, old_subroot: OptNode, new_subroot: OptNode):
@@ -164,8 +169,8 @@ class OptGraph:
         :param old_subroot: OptNode object to replace
         :param new_subroot: OptNode object to replace
         """
-        self._operator.update_subtree(self._node_adapter.restore(old_subroot),
-                                      self._node_adapter.restore(new_subroot))
+        self.operator.update_subtree(self._node_adapter.restore(old_subroot),
+                                     self._node_adapter.restore(new_subroot))
 
     @node_ops_adaptation
     def delete_node(self, node: OptNode):
@@ -175,7 +180,7 @@ class OptGraph:
         :param node: OptNode object to delete
         """
 
-        self._operator.delete_node(self._node_adapter.restore(node))
+        self.operator.delete_node(self._node_adapter.restore(node))
 
     @node_ops_adaptation
     def delete_subtree(self, subroot: OptNode):
@@ -184,43 +189,43 @@ class OptGraph:
 
         :param subroot:
         """
-        self._operator.delete_subtree(self._node_adapter.restore(subroot))
+        self.operator.delete_subtree(self._node_adapter.restore(subroot))
 
     def distance_to_root_level(self, node: OptNode) -> int:
         """ Returns distance to root level """
-        return self._operator.distance_to_root_level(node=self._node_adapter.restore(node))
+        return self.operator.distance_to_root_level(node=self._node_adapter.restore(node))
 
     def nodes_from_layer(self, layer_number: int) -> List[Any]:
         """ Returns all nodes from specified layer """
-        return self._operator.nodes_from_layer(layer_number=layer_number)
+        return self.operator.nodes_from_layer(layer_number=layer_number)
 
     @node_ops_adaptation
     def node_children(self, node: OptNode) -> List[Optional[OptNode]]:
         """ Returns all node's children """
-        return self._operator.node_children(node=self._node_adapter.restore(node))
+        return self.operator.node_children(node=self._node_adapter.restore(node))
 
     @node_ops_adaptation
     def connect_nodes(self, node_parent: OptNode, node_child: OptNode):
         """ Add an edge from node_parent to node_child """
-        self._operator.connect_nodes(parent=self._node_adapter.restore(node_parent),
-                                     child=self._node_adapter.restore(node_child))
+        self.operator.connect_nodes(parent=self._node_adapter.restore(node_parent),
+                                    child=self._node_adapter.restore(node_child))
 
     @node_ops_adaptation
     def disconnect_nodes(self, node_parent: OptNode, node_child: OptNode,
                          clean_up_leftovers: bool = True):
         """ Delete an edge from node_parent to node_child """
-        self._operator.disconnect_nodes(node_parent=self._node_adapter.restore(node_parent),
-                                        node_child=self._node_adapter.restore(node_child),
-                                        clean_up_leftovers=clean_up_leftovers)
+        self.operator.disconnect_nodes(node_parent=self._node_adapter.restore(node_parent),
+                                       node_child=self._node_adapter.restore(node_child),
+                                       clean_up_leftovers=clean_up_leftovers)
 
     def get_nodes_degrees(self):
         """ Nodes degree as the number of edges the node has:
          k = k(in) + k(out) """
-        return self._operator.get_nodes_degrees()
+        return self.operator.get_nodes_degrees()
 
     def get_edges(self):
         """ Returns all available edges in a given graph """
-        return self._operator.get_edges()
+        return self.operator.get_edges()
 
     @copy_doc(Graph.show)
     def show(self, save_path: Optional[Union[PathLike, str]] = None, engine: str = 'matplotlib',
@@ -230,10 +235,10 @@ class OptGraph:
                                     font_size_scale, edge_curvature_scale)
 
     def __eq__(self, other) -> bool:
-        return self._operator.__eq__(other)
+        return self.operator.__eq__(other)
 
     def __str__(self):
-        return str(self._operator.graph_description)
+        return str(self.operator.graph_description)
 
     @copy_doc(Graph.__repr__)
     def __repr__(self):
@@ -241,13 +246,13 @@ class OptGraph:
 
     @property
     def root_node(self):
-        roots = self._operator.root_node
+        roots = self.operator.root_node
         return roots
 
     @property
     @copy_doc(GraphOperator.descriptive_id)
     def descriptive_id(self) -> str:
-        return self._operator.descriptive_id
+        return self.operator.descriptive_id
 
     @property
     @copy_doc(Graph.length)
