a227d016b68d4f93ba58565e141097be87094c9a
morrisnein <petroochcho@gmail.com>
2022-06-17 22:44:14 +0300

    OptHistory serialization refactoring

--- fedot/core/serializers/coders/opt_history_serialization.py
+++ fedot/core/serializers/coders/opt_history_serialization.py
@@ -1,21 +1,20 @@
-import operator
-from copy import deepcopy
-from functools import reduce
-from typing import Any, Collection, Dict, List, Type, Union
+from itertools import chain
+from typing import Any, Dict, List, Type, Union
 
 from fedot.core.optimisers.gp_comp.individual import Individual
 from fedot.core.optimisers.graph import OptGraph
 from fedot.core.optimisers.opt_history import OptHistory
-
 from . import any_from_json, any_to_json
 
 
 def _get_individuals_pool_from_generations_list(generations_list: List[List[Individual]]) -> List[Individual]:
-    uid_to_individual_map = {ind.uid: ind for gen in generations_list for ind in gen}
+    uid_to_individual_map = {ind.uid: ind for ind in chain.from_iterable(generations_list)}
     parents_map = {}
     for individual in uid_to_individual_map.values():
         for parent_operator in individual.parent_operators:
             for parent_ind in parent_operator.parent_individuals:
+                if parent_ind.uid in uid_to_individual_map:
+                    continue
                 parents_map[parent_ind.uid] = parent_ind
     uid_to_individual_map.update(parents_map)
     individuals_pool = list(uid_to_individual_map.values())
@@ -23,10 +22,7 @@ def _get_individuals_pool_from_generations_list(generations_list: List[List[Indi
 
 
 def _serialize_generations_list(generations_list: List[List[Individual]]):
-    new_generations_list = []
-    for generation in generations_list:
-        new_generations_list.append([individual.uid for individual in generation])
-    return new_generations_list
+    return [[individual.uid for individual in generation] for generation in generations_list]
 
 
 def opt_history_to_json(obj: OptHistory) -> Dict[str, Any]:
@@ -50,32 +46,33 @@ def _deserialize_generations_list(generations_list: List[List[Union[str, Individ
                                                                    uid_to_individual_map=uid_to_individual_map)
 
 
-def _deserialize_parent_individuals(generations_list: List[List[Individual]],
+def _deserialize_parent_individuals(individuals: List[Individual],
                                     uid_to_individual_map: Dict[str, Individual]):
     """The operation is executed in-place"""
-    for gen in generations_list:
-        for individual in gen:
-            for parent_op in individual.parent_operators:
-                parent_op.parent_individuals = \
-                    _get_individuals_from_uid_list(uid_list=parent_op.parent_individuals,
-                                                   uid_to_individual_map=uid_to_individual_map)
+    for individual in individuals:
+        for parent_op in individual.parent_operators:
+            parent_op.parent_individuals = \
+                _get_individuals_from_uid_list(uid_list=parent_op.parent_individuals,
+                                               uid_to_individual_map=uid_to_individual_map)
 
 
 def opt_history_from_json(cls: Type[OptHistory], json_obj: Dict[str, Any]) -> OptHistory:
     deserialized = any_from_json(cls, json_obj)
+    # Define all history individuals pool
     if hasattr(deserialized, 'individuals_pool'):
-        # if the history has `individuals_pool`, then other attributes contain uid strings that should be converted
-        # to `Individual` instances.
-        uid_to_individual_map = {ind.uid: ind for ind in deserialized.individuals_pool}
-        del deserialized.individuals_pool
-        _deserialize_generations_list(deserialized.individuals, uid_to_individual_map)
-        _deserialize_generations_list(deserialized.archive_history, uid_to_individual_map)
+        individuals_pool = deserialized.individuals_pool
     else:
         # Older histories has no `individuals_pool`, and all individuals are represented in `individuals` attribute.
         # Let's gather them through all the generations.
-        uid_to_individual_map = {ind.uid: ind for gen in deserialized.individuals for ind in gen}
-    # Now the history attributes are guaranteed to contain individuals.
-    # But parent_individuals still contain uid strings.
-    _deserialize_parent_individuals(deserialized.individuals, uid_to_individual_map)
-    _deserialize_parent_individuals(deserialized.archive_history, uid_to_individual_map)
+        individuals_pool = list(chain.from_iterable(deserialized.individuals))
+    # Deserialize parents for all pipelines
+    uid_to_individual_map = {ind.uid: ind for ind in individuals_pool}
+    _deserialize_parent_individuals(individuals_pool, uid_to_individual_map)
+    # if the history has `individuals_pool`, then other attributes contain uid strings that should be converted
+    # to `Individual` instances.
+    if hasattr(deserialized, 'individuals_pool'):
+        _deserialize_generations_list(deserialized.individuals, uid_to_individual_map)
+        _deserialize_generations_list(deserialized.archive_history, uid_to_individual_map)
+        del deserialized.individuals_pool
+
     return deserialized
