355774dfc3de8156c45b81ddffb6183d47ac4bd0
valer1435 <valp998@gmail.com>
2022-06-24 16:34:25 +0300

    increase timeout

--- test/unit/validation/test_time_series_cv.py
+++ test/unit/validation/test_time_series_cv.py
@@ -142,7 +142,7 @@ def test_api_cv_correct():
     time series through api """
     folds = 2
     _, forecast_len, validation_blocks, time_series = configure_experiment()
-    timeout = 0.05
+    timeout = 0.1
     composer_params = {'max_depth': 1,
                        'max_arity': 2,
                        'preset': 'fast_train',
