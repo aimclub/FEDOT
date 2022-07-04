f08b6783d6ff4eaa7fa3aa0d18aed9074f946023
valer1435 <valp998@gmail.com>
2022-07-04 14:14:42 +0300

    fix exogenous

--- examples/advanced/time_series_forecasting/exogenous.py
+++ examples/advanced/time_series_forecasting/exogenous.py
@@ -89,7 +89,7 @@ def run_exogenous_experiment(path_to_file, len_forecast=250, with_exog=True,
                                    features=exog_variable, target=time_series,
                                    task=task, data_type=DataTypesEnum.ts)
 
-    train_input_exog, _ = train_test_data_setup(predict_input_exog)
+    train_input_exog, predict_input_exog = train_test_data_setup(predict_input_exog)
 
     if with_exog is True:
         # Example with exogenous time series
