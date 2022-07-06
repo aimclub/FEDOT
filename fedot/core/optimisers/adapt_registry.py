31be97626495d14c077256539d7b985c44c8100c
Grigorii Kirgizov <gkirgizov@gmail.com>
2022-07-06 16:25:43 +0300

    WIP adapt registry: add registration mechanics for `restore`

--- fedot/core/optimisers/adapt_registry.py
+++ fedot/core/optimisers/adapt_registry.py
@@ -1,5 +1,5 @@
 from inspect import signature, Parameter
-from typing import Optional, Callable, Any
+from typing import Optional, Callable, Any, Tuple
 
 from fedot.core.optimisers.adapters import BaseOptimizationAdapter, DirectAdapter
 from fedot.core.optimisers.graph import OptGraph
@@ -15,6 +15,7 @@ from fedot.core.utilities.singleton import SingletonMeta
 #  - decorator before init; with normal call
 #  - decorator before init; with too-early call
 
+# TODO: registration of all native functions
 
 class AdaptRegistry(metaclass=SingletonMeta):
 
@@ -22,11 +23,24 @@ class AdaptRegistry(metaclass=SingletonMeta):
         self.adapter = adapter or DirectAdapter()
         self._adaptee_cls = self.adapter._base_graph_class
         self._graph_cls = OptGraph
+        self._native_opt_functions = set()
+
+    def is_native(self, fun: Callable):
+        return fun in self._native_opt_functions
+
+    def register_native(self, fun: Callable):
+        """Registers callable object as an internal function that doesn't
+        require adapt/restore mechanics when called inside the optimiser.
+        Can be used as a decorator."""
+        self._native_opt_functions.add(fun)
+        return fun
 
     def adapt(self, fun: Callable):
         return _transform(fun, self._maybe_adapt, self._maybe_restore)
 
     def restore(self, fun: Callable):
+        if fun in self._native_opt_functions:
+            return fun
         return _transform(fun, self._maybe_restore, self._maybe_adapt)
 
     def _maybe_adapt(self, item):
@@ -55,7 +69,14 @@ def _transform(fun, f_args, f_ret):
     def adapted_fun(*args, **kwargs):
         adapted_args = (f_args(arg) for arg in args)
         adapted_kwargs = dict((kw, f_args(arg)) for kw, arg in kwargs)
+
         result = fun(*adapted_args, **adapted_kwargs)
-        return f_ret(result)
+
+        if isinstance(result, Tuple):
+            # In case when function returns not only Graph
+            adapted_result = (f_ret(result_item) for result_item in result)
+        else:
+            adapted_result = f_ret(result)
+        return adapted_result
 
     return adapted_fun
