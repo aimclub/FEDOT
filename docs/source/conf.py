da1497e232ab4342576dcfefdfa953070fdd2300
Denis Sidorenko <sidorenko.den95@gmail.com>
2022-07-25 13:46:25 +0300

    'year' in conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'FEDOT'
-copyright = '2021-{}, NSS Lab'.format(datetime.datetime.now().year)
+copyright = '2020-{}, NSS Lab'.format(datetime.datetime.now().year)
 author = 'NSS Lab'
 
 # The full version, including alpha/beta/rc tags
