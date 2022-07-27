6cc6d5dedb22c8a14e887be148d2abe62cbe9af0
Grigorii Kirgizov <gkirgizov@gmail.com>
2022-07-27 11:01:02 +0300

    Drop unused parameter of initial_population from Composer

--- fedot/core/composer/gp_composer/gp_composer.py
+++ fedot/core/composer/gp_composer/gp_composer.py
@@ -19,24 +19,22 @@ class GPComposer(Composer):
     Genetic programming based composer
     :param optimiser: optimiser generated in ComposerBuilder.
     :param composer_requirements: requirements for composition process.
+    :param history: optimization history
     :param pipelines_cache: Cache manager for fitted models, optional.
     :param preprocessing_cache: Cache manager for optional preprocessing encoders and imputers, optional.
-    :param initial_pipelines: defines the initial state of the population.
     """
 
     def __init__(self, optimiser: GraphOptimizer,
                  composer_requirements: PipelineComposerRequirements,
-                 initial_pipelines: Optional[Sequence[Pipeline]] = None,
                  history: Optional[OptHistory] = None,
                  pipelines_cache: Optional[OperationsCache] = None,
                  preprocessing_cache: Optional[PreprocessingCache] = None):
 
-        super().__init__(optimiser, composer_requirements, initial_pipelines)
+        super().__init__(optimiser, composer_requirements)
         self.composer_requirements = composer_requirements
         self.pipelines_cache: Optional[OperationsCache] = pipelines_cache
         self.preprocessing_cache: Optional[PreprocessingCache] = preprocessing_cache
 
-        self.optimiser: GraphOptimiser = optimiser
         self.history: Optional[OptHistory] = history
         self.best_models: Collection[Pipeline] = ()
 
