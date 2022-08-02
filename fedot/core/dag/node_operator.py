6dd67d7a6419dcc43bbff246df1164ab3ab084aa
gkirgizov <gkirgizov@gmail.com>
2022-08-02 18:58:38 +0300

    Add backward compatibility in one test

---
+++ fedot/core/dag/node_operator.py
@@ -0,0 +1,6 @@
+
+# Preserved for backward compatibility for unpickling
+#  Pipelines with older Nodes with NodeOperator inside.
+#  see https://github.com/nccr-itmo/FEDOT/pull/802
+class NodeOperator:
+    pass
