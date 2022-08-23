2771269f957bb751217abbb637d549be54e8a060
Sergei Pakulin <pakylin3@mail.ru>
2022-08-23 12:38:23 +0500

    +checks on existing preproc cache

--- fedot/core/optimisers/objective/data_objective_eval.py
+++ fedot/core/optimisers/objective/data_objective_eval.py
@@ -9,10 +9,9 @@ from fedot.core.data.data import InputData
 from fedot.core.log import default_log
 from fedot.core.operations.model import Model
 from fedot.core.optimisers.fitness import Fitness
+from fedot.core.optimisers.objective.objective import Objective, to_fitness
+from fedot.core.optimisers.objective.objective_eval import ObjectiveEvaluate
 from fedot.core.pipelines.pipeline import Pipeline
-from fedot.preprocessing.preprocessing import DataPreprocessor
-from .objective import Objective, to_fitness
-from .objective_eval import ObjectiveEvaluate
 
 DataSource = Callable[[], Iterable[Tuple[InputData, InputData]]]
 
@@ -88,12 +87,12 @@ class PipelineObjectiveEvaluate(ObjectiveEvaluate[Pipeline]):
         graph.try_load_from_cache(self._pipelines_cache, self._preprocessing_cache, fold_id)
         graph.fit(
             train_data,
-
             time_constraint=self._time_constraint
         )
 
         if self._pipelines_cache is not None:
             self._pipelines_cache.save_pipeline(graph, fold_id)
+        if self._preprocessing_cache is not None:
             self._preprocessing_cache.add_preprocessor(graph, fold_id)
 
         return graph
