8c77391f8cfc189329780d141f397bafcc8ab4cf
Grigorii Kirgizov <gkirgizov@gmail.com>
2022-08-29 11:09:40 +0300

    WIP impl state transition in policy graph

--- rl_opt/policy_graph.py
+++ rl_opt/policy_graph.py
@@ -1,5 +1,5 @@
 from enum import Enum, auto
-from typing import TypeVar, Generic, Dict, Sequence, Optional
+from typing import TypeVar, Generic, Dict, Sequence, Optional, Hashable, Tuple
 
 import gym
 import networkx as nx
@@ -12,6 +12,7 @@ from torch.distributions import Categorical
 
 from fedot.core.dag.graph import Graph
 from fedot.core.dag.graph_node import GraphNode
+from fedot.core.log import default_log
 
 ObsType = TypeVar('ObsType', bound=Space)
 ActionType = TypeVar('ActionType')
@@ -55,7 +56,7 @@ class PolicyNode:
 
     def predict(self, observation) -> ActionType:
         # TODO: computation of action probs by the model
-        action_probs: Sequence[float] = [1.0 / float(len(self._actions))]  # uniform
+        action_probs: Sequence[float] = [1.0 / float(len(self._actions))]  # uniform distrib
 
         action_distrib = Categorical(action_probs)
         action_index = action_distrib.sample(1)
@@ -65,24 +66,58 @@ class PolicyNode:
     def transition(self, observation):
         pass
 
+    # TODO: add hash for networkx OR come up with some persistent hash-like node keys
+    def __hash__(self):
+        pass
+
+
+StateKeyType = Hashable
+ACTION = 'action'
+ACT_PROB = 'probability'
+
 
 class PolicyGraph(Graph, Generic[ObsType]):
 
     def __init__(self, action_space: Space, observation_space: ObsType):
-        self._state: PolicyNode = None  # current node
+        self._state: StateKeyType = None
         self._graph = nx.DiGraph()
         pass
 
     # TODO: how will this look like with learning? Like, we fit on the rollout history?
-    def fit(self):
+    def fit(self, observation):
+        action = self._state_node.predict(observation)
+        self.transition(action)
+
+        # ...
+        # ...
+        # ...
+
         pass
 
-    def transition(self, observation) -> ActionType:
+    def transition(self, action: ActionType):
         """On each observation transitions to another state
         and outputs Action that's encoded in the edge between states"""
-        # self.action =
-        # self._state =
-        pass
+
+        # collect all candidate actions
+        successors = self._graph.out_edges(self._state, data=True)
+        transition_candidates = []
+        transition_probs = []
+        for _, succ_state, attrs in successors:
+            if attrs[ACTION] == action:
+                act_prob = attrs.get(ACT_PROB, None)
+                transition_candidates.append(succ_state)
+                transition_probs.append(act_prob)
+
+        # if any transition probability is not set, then assume uniform probability
+        if not all(transition_probs):
+            transition_probs = None
+        # choose next state given probabilities
+        next_state = np.random.choice(transition_candidates, p=transition_probs)
+        self._state = next_state
+
+    @property
+    def _state_node(self) -> PolicyNode:
+        return self._graph[self._state]
 
 
 
