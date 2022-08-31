1ca8685f76c2f0a1e85c1f2d7c259519e5ba0da7
Grigorii Kirgizov <gkirgizov@gmail.com>
2022-08-31 12:30:17 +0300

    WIP redesign outputs of the network
    Store actions only as edges
    Determine unique transition by destination state, not by action
    Actions can repeat

--- rl_opt/policy_graph.py
+++ rl_opt/policy_graph.py
@@ -13,11 +13,13 @@ from torch.distributions import Categorical
 from fedot.core.dag.graph import Graph
 from fedot.core.dag.graph_node import GraphNode
 
+
 ObsType = TypeVar('ObsType', bound=Space)
 ActionType = TypeVar('ActionType')
-
 StateKeyType = Hashable
+
 ACTION = 'action'
+NODE = 'node'
 ACT_PROB = 'probability'
 
 
@@ -48,81 +50,111 @@ class ModelFactory:
 class PolicyNode:
     def __init__(self,
                  obs_shape: th.Size,
-                 actions: Sequence[ActionType],
+                 transitions: Sequence[StateKeyType],
                  model_factory: Optional[ModelFactory] = None):
-        self._actions = list(actions)
+        self.transitions = list(transitions)
         model_factory = model_factory or ModelFactory()
-        self.model = model_factory.get_model(obs_shape, num_outputs=len(actions))
+        self.model = model_factory.get_model(obs_shape, num_outputs=len(transitions))
 
     def fit(self):
         pass
 
-    def predict(self, observation) -> ActionType:
+    def predict(self, observation) -> StateKeyType:
         # TODO: computation of action probs by the model
         #  and saving of intermediate info for local learning step
-        action_logits = self.model.forward(observation)
-        action_probs = nn.Softmax()(action_logits)
+        logits = self.model.forward(observation)
+        probs = nn.Softmax()(logits)
 
         # uniform distribution
-        action_probs: Sequence[float] = [1.0 / float(len(self._actions))]
+        probs: Sequence[float] = [1.0 / float(len(self.transitions))]
 
-        action_distrib = Categorical(action_probs)
-        action_index = action_distrib.sample(1)
-        action = self._actions[action_index]
-        return action
-
-    # TODO: add hash for networkx OR come up with some persistent hash-like node keys
-    def __hash__(self):
-        pass
+        distrib = Categorical(probs)
+        index = distrib.sample(1)
+        transition = self.transitions[index]
+        return transition
 
 
-class PolicyGraph(Graph, Generic[ObsType]):
+class PolicyGraph(Generic[ObsType]):
 
-    def __init__(self, action_space: Space, observation_space: ObsType):
-        self._state: StateKeyType = None
-        self._graph = nx.DiGraph()
-        pass
+    def __init__(self,
+                 observation_space: ObsType,
+                 action_space: Space,
+                 state_graph: nx.DiGraph,
+                 initial_state: Optional[StateKeyType] = None):
+
+        self._graph = PolicyGraph.to_model_graph(observation_space, action_space, state_graph)
+        if initial_state is not None:
+            if initial_state not in self._graph:
+                raise ValueError(f'Invalid initial state {initial_state} is not found in state graph.')
+            self._state = initial_state
+        else:
+            self._state = list(self._graph)[0]
+        self._state_key_counter = len(self._graph)
+
+    @staticmethod
+    def to_model_graph(observation_space: ObsType,
+                       action_space: Space,
+                       state_graph: nx.DiGraph) -> nx.DiGraph:
+        model_factory = ModelFactory()
+        state_attrs = {}
+        for state in state_graph:
+            successors = state_graph.successors(state)
+            # action_edges = state_graph.out_edges(state, data=True)
+            # actions = [attrs[ACTION] for _, dst, attrs in action_edges]
+            new_model = PolicyNode(observation_space, successors, model_factory)
+            state_attrs[state] = {NODE: new_model}
+        model_graph = nx.DiGraph(state_graph)
+        nx.set_node_attributes(model_graph, state_attrs)
+        return model_graph
+
+    def add_transition(self,
+                       source_state: StateKeyType,
+                       action: ActionType,
+                       dest_state: StateKeyType):
+        """Add new transition from source node to the destination node with an action.
+
+        If a transition with such action already exists -- then a duplicate is made.
+        If destination state is None -- a new state is created.
+        """
+        source_node = self._graph[source_state]
+        if dest_state not in self._graph:
+            pass
+
+    @property
+    def _next_state_key(self) -> StateKeyType:
+        self._state_key_counter += 1
+        state_key = 'S' + str(self._state_key_counter)
+        return state_key
 
     # TODO: how will this look like with learning? Like, we fit on the rollout history?
     def fit(self, observation):
-        action = self.predict(observation)
-        self.transition(action)
-
+        destination = self.predict(observation)
+        self.transition(destination)
         # ...
         # ...
         # ...
-
         pass
 
-    def predict(self, observation) -> ActionType:
+    def predict(self, observation) -> StateKeyType:
         return self._state_node.predict(observation)
 
-    def transition(self, action: ActionType):
+    def transition(self, destination: StateKeyType) -> ActionType:
         """On each observation transitions to another state
         and outputs Action that's encoded in the edge between states"""
 
-        # collect all candidate actions
         successors = self._graph.out_edges(self._state, data=True)
-        transition_candidates = []
-        transition_probs = []
         for _, succ_state, attrs in successors:
-            if attrs[ACTION] == action:
-                act_prob = attrs.get(ACT_PROB, None)
-                transition_candidates.append(succ_state)
-                transition_probs.append(act_prob)
-
-        # if any transition probability is not set, then assume uniform probability
-        if not all(transition_probs):
-            transition_probs = None
-        # choose next state given probabilities
-        next_state = np.random.choice(transition_candidates, p=transition_probs)
-        self._state = next_state
+            if succ_state == destination:
+                self._state = destination
+                action = attrs[ACTION]
+                return action
+        else:
+            raise ValueError(f'Destination state <{destination}> is not found'
+                             f' in successors of the state <{self._state}>')
 
     @property
     def _state_node(self) -> PolicyNode:
-        return self._graph[self._state]
-
-
+        return self._graph[self._state][NODE]
 
 
 def evaluate_policy_graph():
@@ -130,7 +162,7 @@ def evaluate_policy_graph():
 
     n_rollouts = 100
     max_steps = 1000
-    policy = PolicyGraph(env.action_space, env.observation_space)
+    policy = PolicyGraph(env.observation_space, env.action_space)
 
     for i_rollout in range(n_rollouts):
         observations = []
@@ -139,8 +171,8 @@ def evaluate_policy_graph():
         observation, info = env.reset(seed=42)
 
         for i in range(max_steps):
-            action = policy.predict(observation)  # User-defined policy function
-            policy.transition(action)
+            next_state = policy.predict(observation)  # User-defined policy function
+            action = policy.transition(next_state)
 
             observation, reward, done, info = env.step(action)
             # env.render()
