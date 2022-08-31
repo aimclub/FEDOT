7c5ca6607432dc43a31d35086a3506eed3306873
valer1435 <valp998@gmail.com>
2022-08-31 15:03:05 +0300

    fix multi ts

--- cases/multivariate_ts_forecasting.py
+++ cases/multivariate_ts_forecasting.py
@@ -52,7 +52,7 @@ def launch_fedot_forecasting(target_column: int = 1, forecast_horizon: int = 50,
 
     # Configure AutoML
     task_parameters = TsForecastingParams(forecast_length=forecast_horizon)
-    model = Fedot(problem='ts_forecasting', task_params=task_parameters, timeout=5,
+    model = Fedot(problem='ts_forecasting', task_params=task_parameters, timeout=30,
                   cv_folds=2, validation_blocks=2)
     target_series = train_df[train_df['label'] == target_column]
     obtained_pipeline = model.fit(features=train_data,
