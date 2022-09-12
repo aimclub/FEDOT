8972ee73fb26ac3f01755173448434801aa4b89f
Grigorii Kirgizov <gkirgizov@gmail.com>
2022-09-12 14:57:18 +0300

    pep8 import fixes

--- fedot/core/optimisers/gp_comp/operators/inheritance.py
+++ fedot/core/optimisers/gp_comp/operators/inheritance.py
@@ -1,7 +1,6 @@
 from functools import partial
 from typing import (Callable)
 
-from fedot.core.optimisers.composer_requirements import ComposerRequirements
 from fedot.core.optimisers.gp_comp.operators.operator import PopulationT, Operator
 from fedot.core.optimisers.gp_comp.operators.selection import Selection
 from fedot.core.utilities.data_structures import ComparableEnum as Enum
@@ -22,7 +21,8 @@ class Inheritance(Operator):
     def __call__(self, previous_population: PopulationT, new_population: PopulationT) -> PopulationT:
         return self._inheritance_type_by_genetic_scheme(previous_population, new_population)()
 
-    def _inheritance_type_by_genetic_scheme(self, previous_population: PopulationT, new_population: PopulationT) -> Callable:
+    def _inheritance_type_by_genetic_scheme(self, previous_population: PopulationT,
+                                            new_population: PopulationT) -> Callable:
         steady_state_scheme = partial(steady_state_inheritance, previous_population,
                                       new_population, self.selection)
         generational_scheme = partial(direct_inheritance, new_population, self.parameters.pop_size)
