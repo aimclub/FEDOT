35f9418548cbc92931569e89c458f01e05213308
Grigorii Kirgizov <gkirgizov@gmail.com>
2022-09-20 18:05:01 +0300

    WIP graph generation bench

---
+++ examples/advanced/abstract_graph_evolution/mmd.py
@@ -0,0 +1,89 @@
+import itertools
+import numpy as np
+from scipy.linalg import toeplitz
+import pyemd
+
+
+def emd(x, y, distance_scaling=1.0):
+    support_size = max(len(x), len(y))
+    d_mat = toeplitz(range(support_size)).astype(np.float)
+    distance_mat = d_mat / distance_scaling
+
+    # convert histogram values x and y to float, and make them equal len
+    x = x.astype(np.float)
+    y = y.astype(np.float)
+    if len(x) < len(y):
+        x = np.hstack((x, [0.0] * (support_size - len(x))))
+    elif len(y) < len(x):
+        y = np.hstack((y, [0.0] * (support_size - len(y))))
+
+    emd_value = pyemd.emd(x, y, distance_mat)
+    return emd_value
+
+
+def l2(x, y):
+    dist = np.linalg.norm(x - y, 2)
+    return dist
+
+
+def gaussian_emd(x, y, sigma=1.0, distance_scaling=1.0):
+    ''' Gaussian kernel with squared distance in exponential term replaced by EMD
+    Args:
+      x, y: 1D pmf of two distributions with the same support
+      sigma: standard deviation
+    '''
+    emd_val = emd(x, y, distance_scaling=distance_scaling)
+    return np.exp(-emd_val ** 2 / (2 * sigma ** 2))
+
+
+def gaussian(x, y, sigma=1.0):
+    dist = np.linalg.norm(x - y, 2)
+    return np.exp(-dist ** 2 / (2 * sigma ** 2))
+
+
+def discrepancy(samples1, samples2, kernel, *args, **kwargs):
+    d = sum(kernel(s1, s2, *args, **kwargs) for s1, s2 in itertools.product(samples1, samples2))
+    d /= len(samples1) * len(samples2)
+    return d
+
+
+def compute_mmd(samples1, samples2, kernel=gaussian_emd, normalize=True, *args, **kwargs):
+    ''' MMD between two samples
+    '''
+    # normalize histograms into pmf
+    if normalize:
+        samples1 = [s1 / np.sum(s1) for s1 in samples1]
+        samples2 = [s2 / np.sum(s2) for s2 in samples2]
+    return discrepancy(samples1, samples1, kernel, *args, **kwargs) + \
+           discrepancy(samples2, samples2, kernel, *args, **kwargs) - \
+           2 * discrepancy(samples1, samples2, kernel, *args, **kwargs)
+
+
+def compute_emd(samples1, samples2, kernel, normalize=True, *args, **kwargs):
+    ''' EMD between average of two samples
+    '''
+    if normalize:
+        samples1 = [np.mean(samples1)]
+        samples2 = [np.mean(samples2)]
+    return discrepancy(samples1, samples2, kernel, *args, **kwargs)
+
+
+def test_mmd_compute():
+    s1 = np.array([0.2, 0.8])
+    s2 = np.array([0.3, 0.7])
+    samples1 = [s1, s2]
+
+    s3 = np.array([0.25, 0.75])
+    s4 = np.array([0.35, 0.65])
+    samples2 = [s3, s4]
+
+    s5 = np.array([0.8, 0.2])
+    s6 = np.array([0.7, 0.3])
+    samples3 = [s5, s6]
+
+    print('between samples1 and samples2: ', compute_mmd(samples1, samples2, kernel=gaussian_emd, sigma=1.0))
+    print('between samples1 and samples3: ', compute_mmd(samples1, samples3, kernel=gaussian_emd, sigma=1.0))
+
+
+if __name__ == '__main__':
+    test_mmd_compute()
---
+++ examples/advanced/abstract_graph_evolution/random_graph_bench.py
@@ -0,0 +1,52 @@
+from collections.abc import Sequence
+
+import numpy as np
+from typing import Callable
+
+from examples.advanced.abstract_graph_evolution.mmd import compute_mmd
+from fedot.core.dag.graph import Graph
+from fedot.core.optimisers.objective import ObjectiveFunction
+
+import networkx as nx
+
+
+def generate_random_target():
+    pass
+
+
+def degree_stats(graph_prediction: Sequence[Graph], graph_target: Sequence[Graph]) -> ObjectiveFunction:
+    pass
+
+
+def degree_stats_impl(graph_prediction: Sequence[nx.Graph],
+                      graph_target: Sequence[nx.Graph]):
+    mmd_dist = mmd_stats_impl(nx.degree_histogram, graph_prediction, graph_target)
+    return mmd_dist
+
+
+def clustering_stats_impl(graph_prediction: Sequence[nx.Graph],
+                          graph_target: Sequence[nx.Graph]):
+    bins = 100
+    mmd_dist = mmd_stats_impl(clustering_stats_graph, graph_prediction, graph_target,
+                              sigma=0.1, distance_scaling=bins)
+    return mmd_dist
+
+
+def mmd_stats_impl(stat_function: Callable[[nx.Graph], np.ndarray],
+                   graph_prediction: Sequence[nx.Graph],
+                   graph_target: Sequence[nx.Graph],
+                   sigma: float = 1.0,
+                   distance_scaling: float = 1.0,
+                   normalize: bool = False):
+    sample_predict = list(map(stat_function, graph_prediction))
+    sample_target = list(map(stat_function, graph_target))
+    mmd_dist = compute_mmd(sample_target, sample_predict,
+                           normalize=normalize,
+                           sigma=sigma, distance_scaling=distance_scaling)
+    return mmd_dist
+
+
+def clustering_stats_graph(graph: nx.Graph, bins: int = 100) -> np.ndarray:
+    clustering_coeffs = list(nx.clustering(graph).values())
+    hist, _ = np.histogram(clustering_coeffs, bins=bins, range=(0.0, 1.0), density=True)
+    return hist
