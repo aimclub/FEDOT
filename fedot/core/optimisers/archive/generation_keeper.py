7a9283ec923df834c57be0563b19175819182165
gkirgizov <gkirgizov@gmail.com>
2022-10-31 17:28:21 +0300

    Modify generation_keeper.py for new objetive, better log

--- fedot/core/optimisers/archive/generation_keeper.py
+++ fedot/core/optimisers/archive/generation_keeper.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Iterable, Sequence, Type
+from typing import Dict, Iterable, Sequence, Type, Optional
 
 import numpy as np
 
@@ -26,7 +26,7 @@ class ImprovementWatcher(ABC):
         raise NotImplementedError()
 
     @abstractmethod
-    def is_metric_improved(self, metric: MetricsEnum) -> bool:
+    def is_metric_improved(self, metric_id) -> bool:
         """Check if specified metric has improved."""
         raise NotImplementedError()
 
@@ -46,20 +46,22 @@ class ImprovementWatcher(ABC):
 class GenerationKeeper(ImprovementWatcher):
     """Generation keeper that primarily tracks number of generations and stagnation duration.
 
-    :param objective: Objective that specifies metrics and if it's multi objective optimization.
-    :param keep_n_best: How many best individuals to keep from all generations.
-     NB: relevant only for single-objective optimization.
-    :param initial_generation: Optional first generation;
-     NB: if None then keeper is created in inconsistent state and requires an initial .append().
+    Args:
+        objective: Objective that specifies metrics and if it's multi objective optimization.
+        keep_n_best: How many best individuals to keep from all generations.
+         NB: relevant only for single-objective optimization.
+        initial_generation: Optional first generation;
+         NB: if None then keeper is created in inconsistent state and requires an initial .append().
     """
 
     def __init__(self,
-                 objective: Objective,
+                 objective: Optional[Objective] = None,
                  keep_n_best: int = 1,
                  initial_generation: PopulationT = None):
         self._generation_num = 0  # 0 means state before initial generation is added
         self._stagnation_counter = 0  # Initialized in non-stagnated state
-        self._metrics_improvement = {metric_id: False for metric_id in objective.metrics}
+
+        self._metrics_improvement = {metric_id: False for metric_id in objective.metric_names}
         self.archive = ParetoFront() if objective.is_multi_objective else HallOfFame(maxsize=keep_n_best)
 
         if initial_generation is not None:
@@ -81,8 +83,8 @@ class GenerationKeeper(ImprovementWatcher):
     def is_any_improved(self) -> bool:
         return any(self._metrics_improvement.values())
 
-    def is_metric_improved(self, metric: MetricsEnum) -> bool:
-        return self._metrics_improvement.get(metric, False)
+    def is_metric_improved(self, metric_id) -> bool:
+        return self._metrics_improvement.get(metric_id, False)
 
     @property
     def is_quality_improved(self) -> bool:
@@ -129,9 +131,11 @@ class GenerationKeeper(ImprovementWatcher):
     def _reset_metrics_improvement(self):
         self._metrics_improvement = {metric_id: False for metric_id in self._metric_ids}
 
-    def _get_fitness_info(self, individual: Individual):
-        return dict(zip(self._metric_ids, individual.fitness.values))
+    def _format_fitness(self, individual: Individual) -> str:
+        fitness_info = zip(self._metric_ids, individual.fitness.values)
+        fitness_info_str = [f'{name}={value:.3f}' for name, value in fitness_info]
+        return ' '.join(fitness_info_str)
 
     def __str__(self) -> str:
         return (f'{self.archive.__class__.__name__} archive fitness: '
-                f'{[self._get_fitness_info(ind) for ind in self.best_individuals]}')
+                f'{[self._format_fitness(ind) for ind in self.best_individuals]}')
