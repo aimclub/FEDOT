f8221b120453ef8694d545513b725c5c17facaf3
gkirgizov <gkirgizov@gmail.com>
2022-11-05 12:11:37 +0300

    Enhance and fix tests

--- test/unit/serialization/test_custom_serialize.py
+++ test/unit/serialization/test_custom_serialize.py
@@ -1,14 +1,25 @@
+import json
+from copy import deepcopy
+
 import pytest
 from typing import Dict
 
-from fedot.core.serializers.serializer import register_serializable
+from fedot.core.serializers.serializer import register_serializable, Serializer, CLASS_PATH_KEY
 
 
-@register_serializable(add_save_load=True)
-class DefaultSerializable:
+class DataEq:
     def __init__(self, data):
         self.data = data
         self._data = data
+
+    def __eq__(self, other):
+        return (self.data == other.data and
+                self._data == other._data)
+
+
+@register_serializable(add_save_load=True)
+class DefaultSerializable(DataEq):
+    pass
 
 
 custom_id = '0042'
@@ -23,22 +34,19 @@ def decode_custom(cls, _dict):
 
 
 @register_serializable(to_json=encode_custom, from_json=decode_custom, add_save_load=True)
-class CustomSerializable:
-    def __init__(self, data):
-        self.data = data
-        self._data = data
+class CustomSerializable(DataEq):
+    pass
 
 
 @register_serializable(add_save_load=True)
-class CustomSerializableWithMethods:
-    def __init__(self, data):
-        self.data = data
-        self._data = data
+class CustomSerializableWithMethods(DataEq):
+    pass
 
     def to_json(self) -> Dict:
         return {custom_id: self.data}
 
-    @classmethod
+    # @classmethod
+    @staticmethod
     def from_json(cls, _dict):
         return CustomSerializableWithMethods(_dict[custom_id])
 
@@ -47,13 +55,39 @@ class CustomSerializableWithMethods:
                                  CustomSerializable(666),
                                  CustomSerializableWithMethods(777)])
 def test_serializable(obj):
+    dumped = json.dumps(obj, cls=Serializer)
+    loaded = json.loads(dumped, cls=Serializer)
 
-    # check custom id
-
-    pass
+    assert loaded == deepcopy(obj)
 
 
+@pytest.mark.parametrize('obj', [DefaultSerializable(42),
+                                 CustomSerializable(666),
+                                 CustomSerializableWithMethods(777)])
+def test_default_save_load(obj):
+    # test that have 'save' and 'load' methods added by default
+    assert hasattr(obj, 'save')
+    assert hasattr(obj, 'load')
+    assert obj.__class__.load(obj.save()) == obj
+
+
+@pytest.mark.parametrize('obj', [CustomSerializableWithMethods(777)])
+def test_serializable_with_class_methods(obj):
+    dumped_srz = json.dumps(obj, cls=Serializer)
+    dumped_self = obj.to_json()
+
+    decoded_self = obj.__class__.from_json(obj.__class__, dumped_self)
+    decoded_srz = json.loads(dumped_srz, cls=Serializer)
+
+    assert decoded_self == decoded_srz == deepcopy(obj)
+
+
+@pytest.mark.parametrize('obj', [CustomSerializable(666)])
 def test_serializable_custom(obj):
-    # check custom id
+    dumped_srz = json.dumps(obj, cls=Serializer)
+    dumped_self = encode_custom(obj)
 
-    pass
+    decoded_self = decode_custom(obj.__class__, dumped_self)
+    decoded_srz = json.loads(dumped_srz, cls=Serializer)
+
+    assert decoded_self == decoded_srz == deepcopy(obj)
