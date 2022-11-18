a44cdfa096df2286fa554ee56fa6c77a02baf735
Grigorii Kirgizov <gkirgizov@gmail.com>
2022-11-18 20:17:06 +0300

    WIP drop files used in GOLEM

--- fedot/core/optimisers/graph.py
+++
@@ -1,5 +0,0 @@
-from fedot.core.dag.graph_delegate import GraphDelegate
-from fedot.core.dag.linked_graph_node import LinkedGraphNode
-
-OptNode = LinkedGraphNode
-OptGraph = GraphDelegate
--- fedot/core/serializers/coders/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-from .enum_serialization import enum_from_json, enum_to_json
-from .graph_node_serialization import graph_node_to_json
-from .graph_serialization import graph_from_json
-from .opt_history_serialization import opt_history_from_json, opt_history_to_json
-from .parent_operator_serialization import parent_operator_from_json, parent_operator_to_json
-from .uuid_serialization import uuid_from_json, uuid_to_json
-from .objective_serialization import objective_from_json
