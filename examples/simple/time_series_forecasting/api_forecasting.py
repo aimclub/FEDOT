af82a46faed493899122396a025815648667c1c0
Lyubov Yamshchikova <43475193+YamLyubov@users.noreply.github.com>
2022-12-27 10:42:23 +0300

    Fix plot_pareto

--- examples/simple/time_series_forecasting/api_forecasting.py
+++ examples/simple/time_series_forecasting/api_forecasting.py
@@ -5,8 +5,6 @@ import pandas as pd
 from fedot.api.main import Fedot
 from fedot.core.data.data import InputData
 from fedot.core.data.data_split import train_test_data_setup
-from fedot.core.pipelines.node import PrimaryNode, SecondaryNode
-from fedot.core.pipelines.pipeline import Pipeline
 from fedot.core.repository.dataset_types import DataTypesEnum
 from fedot.core.repository.tasks import TsForecastingParams, Task, TaskTypesEnum
 from fedot.core.utils import fedot_project_root
