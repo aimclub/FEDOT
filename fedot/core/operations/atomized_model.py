f4db84e54630caaa7183558c7c5206a5e76b4381
Lyubov Yamshchikova <43475193+YamLyubov@users.noreply.github.com>
2023-02-22 17:17:24 +0300

    Minor

--- fedot/core/operations/atomized_model.py
+++ fedot/core/operations/atomized_model.py
@@ -12,7 +12,6 @@ from fedot.core.pipelines.tuning.tuner_builder import TunerBuilder
 from fedot.core.repository.operation_types_repository import OperationMetaInfo, \
     atomized_model_type
 from fedot.core.utils import make_pipeline_generator
-from fedot.preprocessing.preprocessing import DataPreprocessor
 
 
 class AtomizedModel(Operation):
@@ -25,14 +24,10 @@ class AtomizedModel(Operation):
         super().__init__(operation_type=atomized_model_type())
         self.pipeline = pipeline
         self.unique_id = self.pipeline.root_node.descriptive_id
-        self.atomized_preprocessor = DataPreprocessor()
 
-    def fit(self, params: Optional[Union[OperationParameters, dict]], data: InputData,
-            use_cache: bool = True):
+    def fit(self, params: Optional[Union[OperationParameters, dict]], data: InputData):
 
         copied_input_data = deepcopy(data)
-        copied_input_data = self.atomized_preprocessor.obligatory_prepare_for_fit(copied_input_data)
-
         predicted_train = self.pipeline.fit(input_data=copied_input_data)
         fitted_atomized_operation = self.pipeline
 
@@ -43,8 +38,6 @@ class AtomizedModel(Operation):
 
         # Preprocessing applied
         copied_input_data = deepcopy(data)
-        copied_input_data = self.atomized_preprocessor.obligatory_prepare_for_predict(copied_input_data)
-
         prediction = fitted_operation.predict(input_data=copied_input_data, output_mode=output_mode)
         prediction = self.assign_tabular_column_types(prediction, output_mode)
         return prediction
