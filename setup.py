50bc79b2493b1bfef7c0b21a2a55dfc0a9fe8717
Grigorii Kirgizov <gkirgizov@gmail.com>
2023-02-23 16:35:55 +0800

    hotfix to setup.py (#1055)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setuptools.setup(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
