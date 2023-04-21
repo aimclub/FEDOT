2aa0282de43ea2f9a819db16ae7fc7b3a51b1369
Pakulin Sergei <pakylin3@mail.ru>
2023-04-21 16:15:50 +0300

    update tests

--- test/unit/data_operations/test_data_definition.py
+++ test/unit/data_operations/test_data_definition.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from fedot.api.api_utils.data_definition import FeaturesType
 from fedot.core.data.data import np_datetime_to_numeric
 
 _DATE = '2000-01-01T10:00:00.100'
@@ -12,34 +11,29 @@ _DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
 
 
 @pytest.mark.parametrize('features', [
-    pd.DataFrame([
-        [datetime.strptime(_DATE, _DATE_FORMAT), np.datetime64(_DATE), pd.Timestamp(_DATE), _DATE, 42., 42]
-    ]),
-    pd.DataFrame([
-        [datetime.strptime(_DATE, _DATE_FORMAT), np.datetime64(_DATE), pd.Timestamp(_DATE), _DATE, 42., 42]
-    ], dtype=object),
-    pd.DataFrame([
-        [datetime.strptime(_DATE, _DATE_FORMAT), np.datetime64(_DATE), pd.Timestamp(_DATE), 42]
-    ]),
-    pd.DataFrame([
-        [datetime.strptime(_DATE, _DATE_FORMAT), np.datetime64(_DATE), pd.Timestamp(_DATE)]
-    ]),
-    pd.DataFrame(pd.date_range(_DATE, periods=3, freq='D')),
-
     np.array([
         [_DATE, datetime.strptime(_DATE, _DATE_FORMAT), np.datetime64(_DATE), pd.Timestamp(_DATE), 54, 54.]
+    ]),
+    np.array([
+        [datetime.strptime(_DATE, _DATE_FORMAT), np.datetime64(_DATE), pd.Timestamp(_DATE), 42]
     ], dtype=object),
     np.array([
-        [datetime.strptime(_DATE, _DATE_FORMAT), np.datetime64(_DATE), pd.Timestamp(_DATE), 54]
+        [datetime.strptime(_DATE, _DATE_FORMAT), np.datetime64(_DATE), pd.Timestamp(_DATE), 54., 54]
     ], dtype=object),
     np.array([
         [*pd.date_range(_DATE, periods=3, freq='D').to_numpy(), 54, 54.]
     ], dtype=object),
     np.array([
-        [*pd.date_range(_DATE, periods=3, freq='D').to_numpy()]
+        [*pd.date_range(_DATE, periods=3, freq='D')]
     ], dtype=np.datetime64),
-    pd.date_range(_DATE, periods=3, freq='D').to_numpy()
+    pd.date_range(_DATE, periods=3, freq='D').to_numpy(),
+    np.array([
+        [datetime.strptime(_DATE, _DATE_FORMAT), np.datetime64(_DATE), pd.Timestamp(_DATE)]
+    ]),
+    np.array([
+        ['without_datetime', 54, 54.]
+    ], dtype=object)
 ])
-def test_datetime_erasure(features: FeaturesType):
+def test_datetime_erasure(features: np.ndarray):
     result = np_datetime_to_numeric(features)
     assert 'datetime' not in str(pd.DataFrame(result).infer_objects().dtypes)
