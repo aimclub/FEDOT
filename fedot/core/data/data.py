9d6a8bf50b10da035942c2983b31412d8206571b
Pakulin Sergei <pakylin3@mail.ru>
2023-04-21 17:37:55 +0300

    clarify output dtype

--- fedot/core/data/data.py
+++ fedot/core/data/data.py
@@ -593,12 +593,12 @@ def np_datetime_to_numeric(data: np.ndarray) -> np.ndarray:
         The same table data with datetimes (if existed) converted to integer
     """
     orig_shape = data.shape
-    orig_dtype = object if 'datetime' in str((dt := data.dtype)) else dt
+    out_dtype = np.int64 if 'datetime' in str((dt := data.dtype)) else dt
     features_df = pd.DataFrame(data, copy=False).infer_objects()
     date_cols = features_df.select_dtypes('datetime')
     converted_cols = date_cols.to_numpy(np.int64) // 10 ** 6  # to 'ms' unit from 'ns'
     features_df[date_cols.columns] = converted_cols
-    return features_df.to_numpy(orig_dtype).reshape(orig_shape)
+    return features_df.to_numpy(out_dtype).reshape(orig_shape)
 
 
 def array_to_input_data(features_array: np.array,
