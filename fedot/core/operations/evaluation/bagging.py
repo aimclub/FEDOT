f6f4df2e9e03e2714d0d61dc885ecbe9f1e6183e
Andrey Stebenkov <a.stebenkov75@yandex.ru>
2023-05-25 16:40:44 +0300

    sklearn bagging refactoring

--- fedot/core/operations/evaluation/bagging.py
+++ fedot/core/operations/evaluation/bagging.py
@@ -2,6 +2,7 @@ from abc import ABC
 from typing import Optional
 
 from catboost import CatBoostClassifier, CatBoostRegressor
+from golem.core.utilities.random import RandomStateHandler
 from lightgbm import LGBMClassifier, LGBMRegressor
 from sklearn.ensemble import BaggingClassifier, BaggingRegressor, AdaBoostRegressor
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
@@ -21,70 +22,46 @@ from fedot.core.operations.operation_parameters import OperationParameters, get_
 
 
 class SkLearnBaggingStrategy(EvaluationStrategy, ABC):
-    """ FEDOT's bagging base class. Set params to bagging class
+    """ This class defines the certain base bagging operation implementation for the sklearn operations
+    defined in operation repository
 
-        Args:
-            operation_type: 'str' selected operation as a base model in bagging
+    Args:
+        operation_type: 'str' selected operation as a base model in bagging
 
-            .. details:: possible bagging operations:
+        .. details:: possible bagging operations:
 
-                - ``bag_dt`` -> Bagging of the Decision Tree
-                - ``bag_catboost`` -> Bagging of the Catboost classifier
-                - ``bag_xgboost`` -> Bagging of the Xgboost classifier
-                - ``bag_lgbm`` -> Bagging of the LightGBM
-                - ``bag_lgbmxt`` -> Bagging of the LightGBM Extra Trees
+            - ``bag_dt`` -> Bagging for the Decision Tree
+            - ``bag_dtreg`` -> Bagging for the Decision Trees Regressors
+            - ``bag_adareg`` -> Bagging for AdaBoosting Regressor
 
-                - ``bag_dtreg`` -> Bagging of the Decision Trees Regressors
-                - ``bag_adareg`` -> AdaBoosting Regressor
-                - ``bag_xgboostreg`` -> Bagging of the Xgboost classifier
-                - ``bag_catboostreg`` -> Bagging of the Catboost classifier
-                - ``bag_lgbmreg`` -> Bagging of the LightGBM
-                - ``bag_lgbmxtreg`` -> Bagging of the LightGBM Extra Trees
+        params: operation's init and fitting hyperparameters
 
-            params: operation's init and fitting hyperparameters
+        .. details:: explanation of params
 
-            .. details:: explanation of params
-                - ``bagging_params`` - params belong to bagging operation:
-                    - ``n_estimators`` - numbers of base learner in bagging structure
-                    - ``bootstrap`` - whether samples are drawn with replacement. If False, sampling without replacement
-                                      is performed.
-                    - ``oob_score`` - whether to use out-of-bag samples to estimate the generalization error.
-                                      Only available if bootstrap=True.
-                    - ``n_jobs`` - the number of jobs to run in parallel
-
-                - ``model_params`` - model's fitting params, which is used in bagging
-
-
+            - ``n_estimators`` - the number of base estimators in bagging ensemble
+            - ``bootstrap`` - whether samples are drawn with replacement. If False,
+                              sampling without replacement is performed.
+            - ``oob_score`` - whether to use out-of-bag samples to estimate the generalization error.
+                              Only available if bootstrap=True.
+            - ``max_samples`` - the number of samples to draw from X to train each base estimator
+            - ``max_features`` - the number of features to draw from X to train each base estimator
+            - ``n_jobs`` - the number of jobs to run in parallel
+            - ``model_params`` - model's fitting params
     """
 
     _operations_by_types = {
+        # Classification
         'bag_dtreg': DecisionTreeRegressor,
         'bag_adareg': AdaBoostRegressor,
-        'bag_xgboostreg': XGBRegressor,
-        'bag_catboostreg': CatBoostRegressor,
-        'bag_lgbmreg': LGBMRegressor,
-        'bag_lgbmxtreg': LGBMRegressor,
 
+        # Regression
         'bag_dt': DecisionTreeClassifier,
-        'bag_catboost': CatBoostClassifier,
-        'bag_xgboost': XGBClassifier,
-        'bag_lgbm': LGBMClassifier,
-        'bag_lgbmxt': LGBMClassifier
-    }
-
-    _ds_techniques_by_types = {
-        'knora': KNORAU,
-        'kne': KNORAE,
-        'desp': DESP,
-        'ola': OLA,
-        'mcb': MCB,
     }
 
     def __init__(self, operation_type: str, params: Optional[OperationParameters] = None):
         super().__init__(operation_type, params)
         self.operation_impl = None
         self.bagging_operation = None
-        self._ds_technique = None
 
     def _convert_to_operation(self, operation_type: str):
         if operation_type in self._operations_by_types.keys():
@@ -101,9 +78,12 @@ class SkLearnBaggingStrategy(EvaluationStrategy, ABC):
     def _set_operation_params(self, operation_type, params):
         if params is None:
             params = get_default_params(operation_type)
+
         elif isinstance(params, dict):
             params = OperationParameters.from_operation_type(operation_type, **params)
+
         elif isinstance(params, OperationParameters):
+            # Getting models params after applying mutation
             if params.get('model_params'):
                 params = OperationParameters.from_operation_type(operation_type, **(params.to_dict()))
 
@@ -113,11 +93,8 @@ class SkLearnBaggingStrategy(EvaluationStrategy, ABC):
         self._bagging_params = {}
 
         for param in params.keys():
-            if param != 'model_params' and param != 'dynamic_selection_technique':
+            if param != 'model_params':
                 self._bagging_params.update({param: params.get(param)})
-
-        if params.get('dynamic_selection_technique'):
-            self._ds_technique = params.get('dynamic_selection_technique')
 
         return params
 
@@ -136,24 +113,13 @@ class SkLearnBaggingStrategy(EvaluationStrategy, ABC):
         """
         operation_implementation = self.operation_impl
 
-        if self._ds_technique:
-            train_data, dsel_data = train_test_data_setup(train_data, split_ratio=0.5)
-
-        # TODO: Fix RandomStateHandler()
-        # with RandomStateHandler():
-        #     operation_implementation.fit(train_data.features, train_data.target)
-        operation_implementation.fit(train_data.features, train_data.target)
-
-        if self._ds_technique:
-            dynamic_selection = self._ds_techniques_by_types[self._ds_technique](operation_implementation)
-            dynamic_selection.fit(dsel_data.features, dsel_data.target)
-
-            return dynamic_selection
+        with RandomStateHandler():
+            operation_implementation.fit(train_data.features, train_data.target)
 
         return operation_implementation
 
     def predict(self, trained_operation, predict_data: InputData):
-        """ Method to predict
+        """ This method used for prediction of the target data
 
         Args:
             trained_operation: operation object
@@ -166,11 +132,11 @@ class SkLearnBaggingStrategy(EvaluationStrategy, ABC):
 
 
 class SkLearnBaggingClassificationStrategy(SkLearnBaggingStrategy):
-    """ FEDOT's classification bagging operation implementation from sklearn
+    """ Classification bagging operation implementation
 
-            Args:
-                operation_type: 'str' selected operation as a base model in bagging
-                params: operation's init and fitting hyperparameters
+        Args:
+            operation_type: 'str' selected operation as a base model in bagging
+            params: operation's init and fitting hyperparameters
     """
 
     def __init__(self, operation_type, params: Optional[OperationParameters] = None):
@@ -182,8 +148,10 @@ class SkLearnBaggingClassificationStrategy(SkLearnBaggingStrategy):
     def predict(self, trained_operation, predict_data: InputData) -> OutputData:
         if self.output_mode in ['default', 'labels']:
             prediction = trained_operation.predict(predict_data.features)
+
         elif self.output_mode in ['probs', 'full_probs'] and predict_data.task:
             prediction = trained_operation.predict_proba(predict_data.features)
+
         else:
             raise ValueError(f'Output model {self.output_mode} is not supported')
 
@@ -191,11 +159,11 @@ class SkLearnBaggingClassificationStrategy(SkLearnBaggingStrategy):
 
 
 class SkLearnBaggingRegressionStrategy(SkLearnBaggingStrategy):
-    """ FEDOT's regression bagging operation implementation from sklearn
+    """ Regression bagging operation implementation
 
-            Args:
-                operation_type: 'str' selected operation as a base model in bagging
-                params: operation's init and fitting hyperparameters
+        Args:
+            operation_type: 'str' selected operation as a base model in bagging
+            params: operation's init and fitting hyperparameters
     """
 
     def __init__(self, operation_type, params: Optional[OperationParameters] = None):
