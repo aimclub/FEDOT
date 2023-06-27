b2e6e0b8f7454998b9b9ff3e544bd9fdba627663
Pakulin Sergei <pakylin3@mail.ru>
2023-06-27 12:25:55 +0300

    set n_jobs to 1

--- examples/advanced/multiobj_optimisation.py
+++ examples/advanced/multiobj_optimisation.py
@@ -4,7 +4,7 @@ from fedot.api.main import Fedot
 from fedot.core.utils import fedot_project_root
 
 
-def run_classification_multiobj_example(visualization=False, timeout=1, with_tuning=True):
+def run_classification_multiobj_example(visualization=False, timeout:float = 1., n_jobs: int = -1, with_tuning=True):
     train_data = pd.read_csv(f'{fedot_project_root()}/examples/data/Hill_Valley_with_noise_Training.data')
     test_data = pd.read_csv(f'{fedot_project_root()}/examples/data/Hill_Valley_with_noise_Testing.data')
     target = test_data['class']
@@ -13,8 +13,7 @@ def run_classification_multiobj_example(visualization=False, timeout=1, with_tun
 
     metric_names = ['f1', 'node_number']
     auto_model = Fedot(problem=problem, timeout=timeout, preset='best_quality', seed=42,
-                       metric=metric_names,
-                       with_tuning=with_tuning)
+                       n_jobs=n_jobs, metric=metric_names, with_tuning=with_tuning)
     auto_model.fit(features=train_data, target='class')
     prediction = auto_model.predict_proba(features=test_data)
     print(auto_model.get_metrics(target))
