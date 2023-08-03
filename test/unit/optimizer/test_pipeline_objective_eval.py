0d94e193b7e3aba2435111670b1bf8a30d292b58
Sergey Kasyanov <kus7466@gmail.com>
2023-08-03 12:03:40 +0300

    Fix another test

--- test/unit/optimizer/test_pipeline_objective_eval.py
+++ test/unit/optimizer/test_pipeline_objective_eval.py
@@ -157,7 +157,9 @@ def test_pipeline_objective_evaluate_with_invalid_metrics(classification_dataset
 def test_pipeline_objective_evaluate_for_timeseries_cv(folds, actual_value):
     forecast_len, validation_blocks, time_series = configure_experiment()
     objective = MetricsObjective(RegressionMetricsEnum.MSE)
-    data_producer = DataSourceSplitter(folds, validation_blocks).build(time_series)
+    data_producer = DataSourceSplitter(cv_folds=folds,
+                                       validation_blocks=validation_blocks,
+                                       limit_validation_blocks_count=False).build(time_series)
     simple_pipeline = get_simple_ts_pipeline()
     objective_evaluate = PipelineObjectiveEvaluate(objective, data_producer, validation_blocks=validation_blocks)
     metric_value = objective_evaluate.evaluate(simple_pipeline).value
