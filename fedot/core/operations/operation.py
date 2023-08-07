b9be1d8eb9ee0cdd91bcbebaaf25e683fc125024
valer1435 <valp998@gmail.com>
2023-08-07 18:04:22 +0300

    fix unexpected inplace operations

--- fedot/core/operations/operation.py
+++ fedot/core/operations/operation.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from copy import deepcopy
 from typing import Optional, Union, Dict, Any
 
 from golem.core.log import default_log
@@ -80,6 +81,7 @@ class Operation:
         Returns:
             tuple: trained operation and prediction on train data
         """
+        data = deepcopy(data)
         self._init(data.task, params=params, n_samples_data=data.features.shape[0])
 
         self.fitted_operation = self._eval_strategy.fit(train_data=data)
@@ -119,6 +121,7 @@ class Operation:
     def _predict(self, fitted_operation, data: InputData, params: Optional[OperationParameters] = None,
                  output_mode: str = 'default', is_fit_stage: bool = False):
 
+        data = deepcopy(data)
         is_main_target = data.supplementary_data.is_main_target
         data_flow_length = data.supplementary_data.data_flow_length
         self._init(data.task, output_mode=output_mode, params=params, n_samples_data=data.features.shape[0])
