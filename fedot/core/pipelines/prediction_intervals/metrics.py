f4be34cde2a58606e5ad0886ab91397acf3250b2
Sergey Kasyanov <kus7466@gmail.com>
2023-08-22 12:44:28 +0300

    Refactor metrics with numpy functions

--- fedot/core/pipelines/prediction_intervals/metrics.py
+++ fedot/core/pipelines/prediction_intervals/metrics.py
@@ -5,40 +5,18 @@ def quantile_loss(y_true: np.array, y_pred: np.array, quantile: float):
     """Qauntile loss of Pinball loss function."""
 
     res = y_true - y_pred
-    metrics = np.empty(shape=[0])
-    for x in res:
-        if x >= 0:
-            metrics = np.append(metrics, quantile * x)
-        else:
-            metrics = np.append(metrics, (quantile - 1) * x)
-
-    return np.mean(metrics)
+    res[res >= 0] = res[res >= 0] * quantile
+    res[res < 0] = res[res < 0] * (quantile - 1)
+    return np.mean(res)
 
 
 def picp(true: np.array, low: np.array, up: np.array):
     """Prediction interval coverage probability metric. See, e.g., https://www.mdpi.com/2673-4826/2/1/2"""
-
-    s = 0
-    length = len(true)
-    for i in range(length):
-        if true[i] >= low[i] and true[i] <= up[i]:
-            s += 1
-
-    return s / length
+    return np.mean((true >= low) & (true <= up))
 
 
 def interval_score(true: np.array, low: np.array, up: np.array, alpha: float = 0.1, weight: float = 2):
     """Interval score metric, see https://arxiv.org/pdf/2007.05709.pdf"""
-
-    aiw = (up - low).mean()
-    array = np.array([])
-    length = len(true)
-    for i in range(length):
-        if true[i] < low[i]:
-            array = np.append(array, low[i] - true[i])
-        elif true[i] > up[i]:
-            array = np.append(array, true[i] - up[i])
-        else:
-            array = np.append(array, 0)
-
-    return aiw + weight / alpha * array.mean()
+    aiw = np.mean(up - low)
+    array = (true < low) * (low - true) + (true > up) * (true - up)
+    return aiw + weight / alpha * np.mean(array)
