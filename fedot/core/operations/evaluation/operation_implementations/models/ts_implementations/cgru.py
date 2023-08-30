Fix cgru normalization

fedot/core/operations/evaluation/operation_implementations/models/ts_implementations/cgru.py
@@ -150,12 +150,7 @@ class CGRUImplementation(ModelImplementation):
         return f_scaled, t_scaled
 
     def _scale(self, array: np.ndarray):
-        if self.std != 0:
-            return (array - self.mu) / self.std
-        else:
-            # if std is 0 then array has the only unique value
-            # the _scale returns 0 - near to true
-            return array - self.mu
+        return (array - self.mu) / (self.std + 1e-6)
 
     def _inverse_scale(self, array: np.ndarray):
         return array * self.std + self.mu