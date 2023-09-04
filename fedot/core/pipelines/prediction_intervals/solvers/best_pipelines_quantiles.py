7284e15ad769d954c45293a29fcb1ae6837f52a1
GrigoriJasnovidov <91132144+GrigoriJasnovidov@users.noreply.github.com>
2023-09-04 15:24:15 +0300

    Prediction Intervals (#1149)
    * add prediction intervals functionality
    
    * add unit tests
    
    * add example
    
    * remove redundant type-hints in doc strings
    
    * delete empty strings in solvers/mutation_of_best_pipeline.py
    
    * add params_with_replacement in test_solver_mutations.py
    
    * shorten fixture params in test_solver_mutations.py
    
    * add test data in csv and script for generating/model+data
    
    * update example
    
    * correct return in main/PredictionIntervals forecast
    
    * delete defualt value in metrics quantile loss
    
    * delete redundant cwc in metrics
    
    * delete redundant np(...) in metrics
    
    * add detailed error message in utils/check_init_params
    
    * add exception if predictions is ocasionaly [] in solver_mutation
    
    * Refactor metrics with numpy functions
    
    * Replace some logic with numpy functions
    
    * Fix
    
    * remove copy_model; instead copy last generation and other attributes
    
    * improve get_last_generations_function
    
    * improve get_last_generation
    
    * updated unit tests
    
    * improve example
    
    * move figwidth/figheight to def plot_prediction_intervals
    
    * delete redundant transformations in utils/compute_prediction_intervals
    
    * add details for utils/check_init_params
    
    * fix self.horizon issues
    
    * add check ts_test length in visualization/plot_prediction_intervals
    
    * improve ts_mutation/get_mutations
    
    * add plt.show() in visualiztion/plot_prediction_intervals
    
    * starting removing utils/pipeline_simple_structure
    
    * extend get_distance_between functionality
    
    * rewrite utils/get_different_pipeplines
    
    * update test_mutation
    
    * delete redundant utils/simple_pipeline_structure
    
    * delete validataion_blocks parameter for QL tuners
    
    * use all possiblie mutations in get_ts_mutation/ts_mutation
    
    * update ts_mutation/get_different_mutations
    
    * move operations definition for mutation method to PredictionIntervalsParams
    
    * remove arima from operations
    
    * expand model list for mutation method
    
    * add comment on operations list
    
    * add TODOs
    
    * pep8 issues
    
    * improve utils/check_init_params
    
    * add restriction to while sycle in ts_mutation/get_different_pipeline
    
    * improve horizon and forecast definitions in main
    
    * correct choice of best individual
    
    * pep issues
    
    ---------
    
    Co-authored-by: Sergey Kasyanov <kus7466@gmail.com>

---
+++ fedot/core/pipelines/prediction_intervals/solvers/best_pipelines_quantiles.py
@@ -0,0 +1,65 @@
+from typing import Union, List
+
+from golem.core.log import LoggerAdapter
+from golem.core.optimisers.opt_history_objects.individual import Individual
+from fedot.core.data.data import InputData
+from fedot.core.pipelines.adapters import PipelineAdapter
+from fedot.core.pipelines.ts_wrappers import out_of_sample_ts_forecast
+
+from fedot.core.pipelines.prediction_intervals.utils import get_different_pipelines
+
+
+def solver_best_pipelines_quantiles(train_input: InputData,
+                           generation: List[Individual],
+                           logger: LoggerAdapter,
+                           horizon: int,
+                           number_models: Union[int, str],
+                           show_progress: bool):
+    """This function realizes 'best_pipelines_quantiles' method.
+
+    Args:
+        train_input: train time series
+        generation: last generation of Fedot-class object
+        logger: prediction interval logger
+        horizon: horizon to build forecast
+        number_models: number pipelines from last generation to use
+        show_progress: flag to show progress
+        logger: prediction intervals logger
+
+    Returns:
+        a list of predictions to build prediction intervals.
+    """
+
+    # take best pipelines from the last generation
+    all_pipelines = get_different_pipelines(generation)
+    number_avaliable_pipelines = len(all_pipelines)
+    predictions = []
+    s = 1
+    message = f'All {number_avaliable_pipelines} avaliable pipelines will be used for training.'
+
+    if number_models == 'max':
+        index_iterations = number_avaliable_pipelines
+        if show_progress:
+            logger.info(message)
+    elif number_models > number_avaliable_pipelines:
+        index_iterations = number_avaliable_pipelines
+        if show_progress:
+            logger.info('number_models > number of avaliable pipelines. ' + message)
+    else:
+        index_iterations = number_models
+        if show_progress:
+            logger.info(f'{number_models} best pipelines will be used for training.')
+
+    for ind in all_pipelines[:index_iterations]:
+
+        pipeline = PipelineAdapter().restore(ind.graph)
+        if show_progress:
+            logger.info(f'Fitting pipeline №{s}')
+            s += 1
+            pipeline.show()
+
+        pipeline.fit(train_input)
+        preds = out_of_sample_ts_forecast(pipeline=pipeline, input_data=train_input, horizon=horizon)
+        predictions.append(preds)
+
+    return predictions
---
+++ fedot/core/pipelines/prediction_intervals/solvers/mutation_of_best_pipeline.py
@@ -0,0 +1,110 @@
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from typing import List
+
+from golem.core.log import LoggerAdapter
+from golem.core.optimisers.opt_history_objects.individual import Individual
+from fedot.core.data.data import InputData
+from fedot.core.composer.metrics import RMSE
+from fedot.core.pipelines.adapters import PipelineAdapter
+from fedot.core.pipelines.ts_wrappers import out_of_sample_ts_forecast
+
+from fedot.core.pipelines.prediction_intervals.ts_mutation import get_mutations, get_different_mutations
+from fedot.core.pipelines.prediction_intervals.pipeline_constraints import first_prediction_constraint, \
+    deviance_constraint
+
+
+def solver_mutation_of_best_pipeline(train_input: InputData,
+                                     ind: Individual,
+                                     horizon: int,
+                                     number_mutations: int,
+                                     mutations_choice: str,
+                                     n_jobs: int,
+                                     show_progress: bool,
+                                     forecast: int,
+                                     operations: List[str],
+                                     logger: LoggerAdapter,
+                                     discard_inapropriate_pipelines: bool,
+                                     keep_percentage: float):
+    """This function realizes 'mutation_of_best_pipeline' method.
+
+    Args:
+        train_input: train time series
+        ind: individual to make mutaions
+        logger: prediction interval logger
+        horizon: horizon to build forecast
+        number_mutations: number mutations to use
+        mutations_choice: choose mutations with ('with_replacement') or without replacement ('different')
+        n_jobs: n_jobs
+        show_progress: flag to show progress
+        logger: prediction intervals logger
+        discard_inapropriate_pipelines: flag to keep unreliable pipelines
+        keep_percentage: percentage of mutations to keep regarding the RMSE-metric of their performance over train ts.
+
+    Returns:
+        a list of predictions to build prediction intervals.
+    """
+
+    if show_progress:
+        logger.info('Creating mutations of final pipeline...')
+
+    if mutations_choice == 'different':
+        mutations_of_best_pipeline = get_different_mutations(individual=ind,
+                                                             number_mutations=number_mutations,
+                                                             operations=operations,
+                                                             logger=logger)
+    elif mutations_choice == 'with_replacement':
+        mutations_of_best_pipeline = get_mutations(individual=ind,
+                                                   number_mutations=number_mutations,
+                                                   operations=operations)
+
+    raw_predictions = []
+    metric_values = []
+    first_pred_constraints = []
+    deviance_pred_constraints = []
+    s = 1
+    for p in mutations_of_best_pipeline:
+        pipeline = PipelineAdapter().restore(p.graph)
+        if show_progress:
+            logger.info(f'Pipeline number {s}')
+            s += 1
+            pipeline.show()
+            start_time = time.time()
+        pipeline.fit(train_input)
+        pred = out_of_sample_ts_forecast(pipeline=pipeline, input_data=train_input, horizon=horizon)
+        metric_value = RMSE.get_value(pipeline=pipeline, reference_data=train_input, validation_blocks=2)
+        if show_progress:
+            end_time = time.time()
+            logger.info(f'fitting time {end_time-start_time} sec')
+            logger.info(f'RMSE-metric: {metric_value}')
+            fig, ax = plt.subplots()
+            ax.plot(range(len(pred)), pred)
+        raw_predictions.append(pred)
+
+        # for each mutation we compute its charactersitcs that used later on to eliminate aproiri bad pipelines
+        if discard_inapropriate_pipelines:
+            fpc = first_prediction_constraint(ts_train=train_input.features, forecast=forecast, prediction=pred)
+            dc = deviance_constraint(ts_train=train_input.features, prediction=pred)
+            metric_value = RMSE.get_value(pipeline=pipeline, reference_data=train_input, validation_blocks=2)
+            metric_values.append(metric_value)
+            first_pred_constraints.append(fpc)
+            deviance_pred_constraints.append(dc)
+
+    # here we remove apriori bad pipelines based on the following characteristics:
+    # first_pred_constraints: dismiss pipelines with first forecasted value that are too far from the model_forecast
+    # deviance_pred_constraints: dismiss pipelines, such their forecasts oscillate too much
+    # we remove a specified percentage of pipelines with biggest RMSE metric.
+    if discard_inapropriate_pipelines:
+        predictions = []
+        maximal_metric_value = np.quantile(np.array(metric_values), keep_percentage)
+        for i, m in enumerate(mutations_of_best_pipeline):
+            if first_pred_constraints[i] and deviance_pred_constraints[i] and metric_values[i] < maximal_metric_value:
+                predictions.append(raw_predictions[i])
+    else:
+        predictions = raw_predictions
+
+    if len(predictions) == 0:
+        raise ValueError('Variable predictions is empty list. Try to increase number_mutations to solve this problem.')
+
+    return predictions
