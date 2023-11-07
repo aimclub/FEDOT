0c557570e802059b1364940f4a5001e3b9adf092
Sergey Kasyanov <kus7466@gmail.com>
2023-11-07 16:01:46 +0300

    fix of fix

--- fedot/core/operations/evaluation/operation_implementations/implementation_interfaces.py
+++ fedot/core/operations/evaluation/operation_implementations/implementation_interfaces.py
@@ -88,9 +88,7 @@ class EncodedInvariantImplementation(DataOperationImplementation):
         self.ids_to_process = ids_to_process
         self.bool_ids = bool_ids
         if len(ids_to_process) > 0:
-            if features.ndim == 1:
-                features = np.reshape(features, (-1, 1))
-            features_to_process = np.array(features[:, ids_to_process])
+            features_to_process = np.array(features[:, ids_to_process]) if features.ndim > 1 else features
             self.operation.fit(features_to_process)
         return self.operation
 
@@ -121,9 +119,7 @@ class EncodedInvariantImplementation(DataOperationImplementation):
         :param features: tabular data for processing
         :return transformed_features: transformed features table
         """
-        if features.ndim == 1:
-            features = np.reshape(features, (-1, 1))
-        features_to_process = np.array(features[:, self.ids_to_process])
+        features_to_process = np.array(features[:, self.ids_to_process]) if features.ndim > 1 else features.copy()
         transformed_part = self.operation.transform(features_to_process)
 
         # If there are no binary features in the dataset
@@ -164,7 +160,7 @@ class EncodedInvariantImplementation(DataOperationImplementation):
 
         # For every column in table make check
         for column_id in range(0, columns_amount):
-            column = features[:, column_id] if columns_amount > 1 else features
+            column = features[:, column_id] if columns_amount > 1 else features.copy()
             if len(np.unique(column)) > 2:
                 non_bool_ids.append(column_id)
             else:
