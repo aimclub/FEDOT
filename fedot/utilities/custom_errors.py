9427e516786266957dc9b0c0cf254f19e0ef988c
DRMPN <mr.gorden.smit@gmail.com>
2023-11-13 12:10:30 +0300

    check

--- fedot/utilities/custom_errors.py
+++ fedot/utilities/custom_errors.py
@@ -3,9 +3,13 @@ from inspect import stack
 
 class MethodNotImplementError(NotImplementedError):
     def __init__(self, _class_):
-        super(MethodNotImplementError, self).__init__(f'Method {stack()[1][3]} is not implemented in {_class_}.')
+        super(MethodNotImplementError, self).__init__(
+            f"Method {stack()[1][3]} is not implemented in {_class_}."
+        )
 
 
 class AbstractMethodNotImplementError(NotImplementedError):
     def __init__(self):
-        super(AbstractMethodNotImplementError, self).__init__('Trying to invoke abstract method.')
+        super(AbstractMethodNotImplementError, self).__init__(
+            "Trying to invoke abstract method."
+        )
