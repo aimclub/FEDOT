860a7e39aa4233d14b2d56ca6deedb93a3e9f021
Sergey Kasyanov <kus7466@gmail.com>
2023-12-04 13:14:56 +0300

    remove old function

--- test/integration/models/test_atomized_model.py
+++ test/integration/models/test_atomized_model.py
@@ -11,7 +11,7 @@ from fedot.core.data.data import InputData
 from fedot.core.operations.atomized_model import AtomizedModel
 from fedot.core.pipelines.node import PipelineNode
 from fedot.core.pipelines.pipeline import Pipeline
-from fedot.core.utils import fedot_project_root, make_pipeline_generator
+from fedot.core.utils import fedot_project_root
 from test.integration.utilities.test_pipeline_import_export import create_correct_path, create_func_delete_files
 
 
