3c2a552ea962bcbd347122ccc42a8b287fdb7db6
kasyanovse <kus7466@gmail.com>
2023-12-28 15:18:06 +0300

    experiment

--- fedot/core/operations/evaluation/operation_implementations/data_operations/ts_transformations.py
+++ fedot/core/operations/evaluation/operation_implementations/data_operations/ts_transformations.py
@@ -118,6 +118,9 @@ class LaggedImplementation(DataOperationImplementation):
             """
         max_allowed_window_size = len(time_series) - forecast_length
 
+        self.log.info((f"Window size of lagged transformation was changed "
+                       f"by WindowSizeSelector from {self.params.get('window_size')} to {new}"))
+
         if self.window_size == 0:
             selector = WindowSizeSelector(method=WindowSizeSelectorMethodsEnum.HAC, window_range=(5, 60))
             new = int(selector.apply(time_series) * time_series.shape[0] * 0.01)
