29f101322ac7cde8089d2285f89c8ccc80dcc7c7
kasyanovse <kus7466@gmail.com>
2024-01-27 10:05:31 +0300

    fix integration tests

--- fedot/core/operations/evaluation/operation_implementations/data_operations/topological/fast_topological_extractor.py
+++ fedot/core/operations/evaluation/operation_implementations/data_operations/topological/fast_topological_extractor.py
@@ -17,27 +17,23 @@ class FastTopologicalFeaturesImplementation(DataOperationImplementation):
         self.max_homology_dimension = params.get('max_homology_dimension')
         self.metric = params.get('metric')
         self.n_jobs = params.get('n_jobs')
-        self.feature_funs = (lambda x: np.quantile(x, (0.1, 0.25, 0.5, 0.75, 0.9)), )
-        self._shape = None
+        self.quantiles = (0.1, 0.25, 0.5, 0.75, 0.9)
+        self._shape = len(self.quantiles)
         self._window_size = None
 
     def fit(self, input_data: InputData):
-        if self._window_size is None:
-            self._window_size = int(input_data.features.shape[1] * self.window_size_as_share)
-            self._window_size = max(self._window_size, 2)
-            self._window_size = min(self._window_size, input_data.features.shape[1] - 2)
-
-        # define shape of features after transforming on the one data sample
-        sample = input_data.features[0, :].ravel()
-        features = np.concatenate([fun(sample) for fun in self.feature_funs])
-        self._shape = features.shape[0]
+        self._window_size = int(input_data.features.shape[1] * self.window_size_as_share)
+        self._window_size = max(self._window_size, 2)
+        self._window_size = min(self._window_size, input_data.features.shape[1] - 2)
         return self
 
     def transform(self, input_data: InputData) -> OutputData:
         with Parallel(n_jobs=self.n_jobs, prefer='processes') as parallel:
             topological_features = parallel(delayed(self._extract_features)(data)
                                             for data in input_data.features)
-        return np.array(topological_features)
+        result = np.array(topological_features)
+        np.nan_to_num(result, copy=False, nan=0, posinf=0, neginf=0)
+        return result
 
     def _extract_features(self, x):
         x_sliced = [x[i:self._window_size + i] for i in range(x.shape[0] - self._window_size + 1)]
@@ -47,12 +43,9 @@ class FastTopologicalFeaturesImplementation(DataOperationImplementation):
                              metric=self.metric,
                              n_threads=1,
                              collapse_edges=False)["dgms"]
-        result = list()
-        for xp in x_processed:
+        result = np.zeros(self._shape * (self.max_homology_dimension + 1))
+        for i, xp in enumerate(x_processed):
             if xp.shape[0] > 0:
-                xp = xp[:, 1] - xp[:, 0]
-                for fun in self.feature_funs:
-                    result.append(fun(xp))
-            else:
-                result.append(np.zeros(self._shape))
-        return np.concatenate(result)
+                result[i * self._shape:(i + 1) * self._shape] = np.quantile(xp[:, 1] - xp[:, 0], self.quantiles,
+                                                                            overwrite_input=True, method='hazen')
+        return result
