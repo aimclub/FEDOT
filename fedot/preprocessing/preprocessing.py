f684be44feafbf46028dba8fc1bb17cb0d21dd54
Andrey Stebenkov <a.stebenkov75@yandex.ru>
2024-07-30 18:57:49 +0300

    Hotfix for bug

--- fedot/preprocessing/preprocessing.py
+++ fedot/preprocessing/preprocessing.py
@@ -207,6 +207,7 @@ class DataPreprocessor(BasePreprocessor):
         if is_fit_stage:
             self._find_features_lacking_nans(data, source_name)
         self._take_only_correct_features(data, source_name)
+
         if is_fit_stage:
             data = self._drop_rows_with_nan_in_target(data)
 
@@ -220,6 +221,7 @@ class DataPreprocessor(BasePreprocessor):
             data.target = self._apply_target_encoding(data, source_name)
         else:
             self.types_correctors[source_name].convert_data_for_predict(data)
+            data.target = self._apply_target_encoding(data, source_name)
 
         # TODO andreygetmanov target encoding must be obligatory for all data types
         if data_type_is_text(data):
