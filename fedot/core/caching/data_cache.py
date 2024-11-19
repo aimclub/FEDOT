cfaef4f5b3ea367307186e4da3588648e99b70f1
DRMPN <mr.gorden.smit@gmail.com>
2024-11-20 00:49:30 +0300

    feat: change the logic to save the entire OutputData instead

--- fedot/core/caching/data_cache.py
+++ fedot/core/caching/data_cache.py
@@ -21,18 +21,20 @@ class DataCache(BaseCache):
     def __init__(self, cache_dir: Optional[str] = None, custom_pid=None):
         super().__init__(DataCacheDB(cache_dir, custom_pid))
 
-    def save_prediction(self, prediction: np.ndarray, uid: str):
+    def save_predicted(self, pipeline: "Pipeline", outputData: OutputData, fold_id: Optional[int] = None):
         """
         Save the prediction for a given UID.
 
         :param prediction (np.ndarray): The prediction to be saved.
         :param uid (str): The unique identifier for the prediction.
         """
+        uid = self._create_uid(pipeline, fold_id)
         try:
-            self._db.add_prediction([(uid, prediction)])
+            self._db.add_prediction(uid, outputData)
         except Exception as ex:
             unexpected_exc = not (
-                isinstance(ex, sqlite3.DatabaseError) and "disk is full" in str(ex)
+                isinstance(
+                    ex, sqlite3.DatabaseError) and "disk is full" in str(ex)
             )
             self.log.warning(
                 f"Predictions can not be saved: {ex}. Continue",
@@ -40,50 +42,15 @@ class DataCache(BaseCache):
                 raise_if_test=unexpected_exc,
             )
 
-    def load_prediction(self, uid: str) -> np.ndarray:
+    def load_predicted(self, pipeline: "Pipeline", fold_id: Optional[int] = None) -> np.ndarray:
         """
         Load the prediction data for the given unique identifier.
         :param uid (str): The unique identifier of the prediction data.
         :return np.ndarray: The loaded prediction data.
         """
-        predict = self._db.get_prediction(uid)
-        # TODO: restore OutputData from predict
-        return predict
-
-    def save_data(
-        self,
-        pipeline: "Pipeline",
-        outputData: OutputData,
-        fold_id: Optional[int] = None,
-    ):
-        """
-        Save the pipeline data to the cache.
-
-        :param pipeline: The pipeline data to be cached.
-        :type pipeline: Pipeline
-        :param outputData: The output data to be saved.
-        :type outputData: OutputData
-        :param fold_id: Optional part of the cache item UID (can be used to specify the number of CV fold).
-        :type fold_id: Optional[int]
-        """
         uid = self._create_uid(pipeline, fold_id)
-        # TODO: save OutputData as a whole to the cache
-        self.save_prediction(outputData.predict, uid)
-
-    def try_load_data(
-        self, pipeline: "Pipeline", fold_id: Optional[int] = None
-    ) -> OutputData:
-        # create parameter dosctring
-        """
-        Try to load data for the given pipeline and fold ID.
-
-        :param  pipeline (Pipeline): The pipeline for which to load the data.
-        :param fold_id (Optional[int]): The fold ID for which to load the data. Defaults to None.
-        :return OutputData: The loaded data.
-        """
-        # TODO: implement loading of pipeline data
-        uid = self._create_uid(pipeline, fold_id)
-        self.load_prediction(uid)
+        outputData = self._db.get_prediction(uid)
+        return outputData
 
     def _create_uid(
         self,
