635b988c8d314756bfa6f26f6c8f15b99f98d6bb
Dmitry Gilemkhanov <153055427+dmitryglhf@users.noreply.github.com>
2025-01-15 18:51:43 +0300

    rollback task_assumptions

--- fedot/api/api_utils/assumptions/task_assumptions.py
+++ fedot/api/api_utils/assumptions/task_assumptions.py
@@ -91,15 +91,6 @@ class RegressionAssumptions(TaskAssumptions):
     @property
     def builders(self):
         return {
-            'gbm_linear': PipelineBuilder().
-            add_branch('catboostreg', 'xgboostreg', 'lgbmreg').join_branches('ridge'),
-            'xgb_lgbm_linear': PipelineBuilder().
-            add_branch('xgboostreg', 'lgbmreg').join_branches('ridge'),
-            'gbm_rf_linear': PipelineBuilder().
-            add_branch('catboostreg', 'xgboostreg', 'lgbmreg', 'rfr').join_branches('ridge'),
-            'catboostreg': PipelineBuilder().add_node('catboostreg'),
-            'xgboostreg': PipelineBuilder().add_node('xgboostreg'),
-            'lgbmreg': PipelineBuilder().add_node('lgbmreg'),
             'rfr': PipelineBuilder().add_node('rfr'),
             'ridge': PipelineBuilder().add_node('ridge'),
         }
@@ -121,17 +112,9 @@ class ClassificationAssumptions(TaskAssumptions):
     @property
     def builders(self):
         return {
-            'gbm_linear': PipelineBuilder().
-            add_branch('catboost', 'xgboost', 'lgbm').join_branches('logit'),
-            'xgb_lgbm_linear': PipelineBuilder().
-            add_branch('xgboost', 'lgbm').join_branches('logit'),
-            'gbm_rf_linear': PipelineBuilder().
-            add_branch('catboost', 'xgboost', 'lgbm', 'rf').join_branches('logit'),
-            'catboost': PipelineBuilder().add_node('catboost'),
-            'xgboost': PipelineBuilder().add_node('xgboost'),
-            'lgbm': PipelineBuilder().add_node('lgbm'),
             'rf': PipelineBuilder().add_node('rf'),
             'logit': PipelineBuilder().add_node('logit'),
+            'catboost': PipelineBuilder().add_node('catboost'),
         }
 
     def ensemble_operation(self) -> str:
