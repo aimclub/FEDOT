0933d136a31a3faa9b017fe31fd9c2046840b573
DRMPN <mr.gorden.smit@gmail.com>
2025-02-10 23:30:46 +0300

    refactor: Simplify caching logic and improve database error handling

--- fedot/core/caching/data_cache.py
+++ fedot/core/caching/data_cache.py
@@ -1,15 +1,8 @@
-
-from typing import TYPE_CHECKING, List, Optional, Union
-
-
-import numpy as np
+from typing import Optional
 
 from fedot.core.caching.base_cache import BaseCache
 from fedot.core.caching.data_cache_db import DataCacheDB
 from fedot.core.data.data import OutputData
-
-if TYPE_CHECKING:
-    from fedot.core.pipelines.pipeline import Pipeline
 
 
 class DataCache(BaseCache):
@@ -19,46 +12,30 @@ class DataCache(BaseCache):
     :param cache_dir: path to the place where cache files should be stored.
     """
 
+    FIT_TYPE = "fit"
+    PRED_TYPE = "pred"
+
     def __init__(self, cache_dir: Optional[str] = None, custom_pid=None):
         super().__init__(DataCacheDB(cache_dir, custom_pid))
 
-    def save_node_fit_prediction(self, descriptive_id: str, output_mode: str, fold_id: int, outputData: OutputData):
-        """
-        Save the prediction results of a fitted node to the cache.
-        """
-        if "ransac" in descriptive_id:
-            return
-        type = "fit"
-        uid = f"{type}_{descriptive_id}_{output_mode}_{fold_id}"
-        self._save_prediction(uid, type, outputData)
-
-    def load_node_fit_prediction(self, descriptive_id: str, output_mode: str, fold_id: int):
-        """
-        Save the prediction results of a fitted node to the cache.
-        """
-        if "ransac" in descriptive_id:
-            return
-        type = "fit"
-        uid = f"{type}_{descriptive_id}_{output_mode}_{fold_id}"
-        return self._load_prediction(uid, type)
-
-    def save_node_prediction(self, descriptive_id: str, output_mode: str, fold_id: int, outputData: OutputData):
+    def save_node_prediction(
+            self, descriptive_id: str, output_mode: str, fold_id: int, outputData: OutputData, is_fit: bool = False):
         """
         Save the prediction results of a node.
         """
         if "ransac" in descriptive_id:
             return
-        type = "pred"
+        type = self.FIT_TYPE if is_fit else self.PRED_TYPE
         uid = f"{type}_{descriptive_id}_{output_mode}_{fold_id}"
         self._save_prediction(uid, type, outputData)
 
-    def load_node_prediction(self, descriptive_id: str, output_mode: str, fold_id: int):
+    def load_node_prediction(self, descriptive_id: str, output_mode: str, fold_id: int, is_fit: bool = False):
         """
         Load the prediction results of a node.
         """
         if "ransac" in descriptive_id:
             return
-        type = "pred"
+        type = self.FIT_TYPE if is_fit else self.PRED_TYPE
         uid = f"{type}_{descriptive_id}_{output_mode}_{fold_id}"
         return self._load_prediction(uid, type)
 
@@ -70,26 +47,3 @@ class DataCache(BaseCache):
         outputData = self._db.get_prediction(uid, type)
         self.log.debug(f"--- {'MISS' if outputData is None else 'HIT'} prediction cache: {uid}")
         return outputData
-
-    def _create_uid(
-        self,
-        pipeline: "Pipeline",  # TODO: pipeline or node
-        fold_id: Optional[int] = None,
-    ) -> str:
-        """
-        Generate a unique identifier for a pipeline.
-
-        :param pipeline (Pipeline): The pipeline for which the unique identifier is generated.
-        :param fold_id (Optional[int]): The fold ID (default: None).
-        :return str: The unique identifier generated for the pipeline.
-        """
-        base_uid = ""
-        from fedot.core.pipelines.pipeline import Pipeline
-        if isinstance(pipeline, Pipeline):
-            for node in pipeline.nodes:
-                base_uid += f"{node.descriptive_id}_"
-        else:
-            base_uid += f"{pipeline.descriptive_id}_"
-        if fold_id is not None:
-            base_uid += f"{fold_id}"
-        return base_uid
--- fedot/core/caching/data_cache_db.py
+++ fedot/core/caching/data_cache_db.py
@@ -1,7 +1,7 @@
 import pickle
 import sqlite3
 from contextlib import closing
-from typing import Optional
+from typing import Optional, List, Tuple
 
 from fedot.core.caching.base_cache_db import BaseCacheDB
 from fedot.core.data.data import OutputData
@@ -15,18 +15,41 @@ class DataCacheDB(BaseCacheDB):
     :param cache_dir: path to the place where cache files should be stored.
     """
 
+    CREATE_STATS_TABLE_QUERY = """
+    CREATE TABLE IF NOT EXISTS stats (
+        id TEXT PRIMARY KEY,
+        retrieve_count INTEGER DEFAULT 0
+    );
+    """
+
+    CREATE_MAIN_TABLE_QUERY = """
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        id TEXT PRIMARY KEY,
+        prediction BLOB
+    );
+    """
+
+    INSERT_STATS_QUERY = """
+    INSERT INTO stats (id, retrieve_count)
+    VALUES (?, COALESCE((SELECT retrieve_count FROM stats WHERE id = ?), 0) + 1)
+    ON CONFLICT(id) DO UPDATE SET
+    retrieve_count = retrieve_count + 1
+    """
+
     def __init__(self, cache_dir: Optional[str] = None, custom_pid=None):
         use_stats = True
-        super().__init__("predictions", cache_dir, use_stats=use_stats,
-                         stats_keys=['fit_hit', 'fit_total',
-                                     'pred_hit', 'pred_total'])
+        super().__init__("predictions",
+                         cache_dir,
+                         use_stats=use_stats,
+                         stats_keys=['fit_hit', 'fit_total', 'pred_hit', 'pred_total'],
+                         custom_pid=custom_pid)
         self._init_db()
         if use_stats:
             self._init_db_stats()
 
-    def add_prediction(self, uid: str, type: str, outputData: OutputData):
+    def add_prediction(self, uid: str, type: str, outputData: OutputData) -> None:
         """
-        Stores predicted `outputData` in binary fromat in a SQL table.
+        Stores predicted `outputData` in binary format in a SQL table.
         """
         try:
             with closing(sqlite3.connect(self.db_path)) as conn:
@@ -35,7 +58,7 @@ class DataCacheDB(BaseCacheDB):
                     pickled_data = sqlite3.Binary(pickle.dumps(outputData, pickle.HIGHEST_PROTOCOL))
                     cur.execute(f"INSERT OR IGNORE INTO {self._main_table} VALUES (?, ?);", [uid, pickled_data])
         except sqlite3.Error as e:
-            print(f"SQLite add {type} error: {e}")
+            self.log.error(f"SQLite add {type} error: {e}")
 
     def get_prediction(self, uid: str, type: str) -> Optional[OutputData]:
         """
@@ -57,10 +80,10 @@ class DataCacheDB(BaseCacheDB):
                         self._inc_eff(cur, f'{type}_total')
             return result
         except sqlite3.Error as e:
-            print(f"SQLite get {type} Error: {e}")
+            self.log.error(f"SQLite get {type} Error: {e}")
             return None
 
-    def retrieve_stats(self):
+    def retrieve_stats(self) -> Optional[List[Tuple[str, int]]]:
         """
         Retrieves statistics for all non-zero prediction cache entries.
         """
@@ -69,30 +92,23 @@ class DataCacheDB(BaseCacheDB):
                 with closing(sqlite3.connect(self.db_path)) as conn:
                     with conn:
                         cur = conn.cursor()
-                        query = f"SELECT id, retrieve_count FROM stats;"
+                        query = "SELECT id, retrieve_count FROM stats;"
                         cur.execute(query)
                         return cur.fetchall()
         except sqlite3.Error as e:
-            print(f"SQLite get Error: {e}")
+            self.log.error(f"SQLite get Error: {e}")
             return None
 
-    def _inc_stats(self, cur: sqlite3.Cursor, id: str):
+    def _inc_stats(self, cur: sqlite3.Cursor, id: str) -> None:
         """
         Increases the retrieve count for a specific prediction cache entry.
         """
         try:
-            query = """
-            INSERT INTO stats (id, retrieve_count)
-            VALUES (?, COALESCE((SELECT retrieve_count FROM stats WHERE id = ?), 0) + 1)
-            ON CONFLICT(id) DO UPDATE SET
-            retrieve_count = retrieve_count + 1
-            """
-            cur.execute(query, (id, id,))
+            cur.execute(self.INSERT_STATS_QUERY, (id, id,))
         except sqlite3.Error as e:
-            print(f"SQLite get Error: {e}")
-            return None
+            self.log.error(f"SQLite inc stats Error: {e}")
 
-    def _init_db_stats(self):
+    def _init_db_stats(self) -> None:
         """
         Initializes the database statistics table.
         """
@@ -101,12 +117,11 @@ class DataCacheDB(BaseCacheDB):
                 with closing(sqlite3.connect(self.db_path)) as conn:
                     with conn:
                         cur = conn.cursor()
-                        query = f"CREATE TABLE IF NOT EXISTS stats (id TEXT PRIMARY KEY, retrieve_count INTEGER DEFAULT 0);"
-                        cur.execute(query)
+                        cur.execute(self.CREATE_STATS_TABLE_QUERY)
         except sqlite3.Error as e:
-            print(f"SQLite get Error: {e}")
+            self.log.error(f"SQLite init db stats Error: {e}")
 
-    def _init_db(self):
+    def _init_db(self) -> None:
         """
         Initializes the main database working table.
         """
@@ -114,13 +129,6 @@ class DataCacheDB(BaseCacheDB):
             with closing(sqlite3.connect(self.db_path)) as conn:
                 with conn:
                     cur = conn.cursor()
-                    cur.execute(
-                        (
-                            f"CREATE TABLE IF NOT EXISTS {self._main_table} ("
-                            "id TEXT PRIMARY KEY,"
-                            "prediction BLOB"
-                            ");"
-                        )
-                    )
+                    cur.execute(self.CREATE_MAIN_TABLE_QUERY.format(table_name=self._main_table))
         except sqlite3.Error as e:
-            print(f"SQLite init error: {e}")
+            self.log.error(f"SQLite init db error: {e}")
