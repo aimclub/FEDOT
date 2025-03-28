e5ceb4431cc23f98a95de780f57486cf4b566969
Dmitry Gilemkhanov <dima.rize@yandex.ru>
2025-03-28 18:27:58 +0300

    docs: available models update

--- fedot/api/builder.py
+++ fedot/api/builder.py
@@ -333,12 +333,15 @@ class FedotBuilder:
 
                 .. details:: Tabular models available for composing:
 
+                    - ``adareg`` -> AdaBoost Regressor
                     - ``catboost`` -> Catboost Classifier
                     - ``catboostreg`` -> Catboost Regressor
                     - ``knn`` -> K-nearest neighbors Classifier
                     - ``knnreg`` -> K-nearest neighbors Regressor
+                    - ``lasso`` -> Lasso Linear Regressor
                     - ``lgbm`` -> Light Gradient Boosting Machine Classifier
                     - ``lgbmreg`` -> Light Gradient Boosting Machine Regressor
+                    - ``linear`` -> Linear Regression Regressor
                     - ``logit`` -> Logistic Regression Classifier
                     - ``rf`` -> Random Forest Classifier
                     - ``rfr`` -> Random Forest Regressor
