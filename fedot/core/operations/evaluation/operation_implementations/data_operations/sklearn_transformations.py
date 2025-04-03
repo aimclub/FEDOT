e14594f1ee2cdba6e1a9d34400ba8fe9013df2ec
Dmitry Gilemkhanov <dima.rize@yandex.ru>
2025-04-03 15:49:51 +0300

    enh: added bagging implementation

--- fedot/core/operations/evaluation/operation_implementations/data_operations/sklearn_transformations.py
+++ fedot/core/operations/evaluation/operation_implementations/data_operations/sklearn_transformations.py
@@ -1,11 +1,16 @@
 import random
-from typing import Optional
+from typing import Optional, Union
 
 import numpy as np
 import pandas as pd
+import dask.array as da
+from dask_ml.decomposition import PCA as DaskPCA
+from dask_ml.utils import check_array
 from sklearn.decomposition import FastICA, KernelPCA, PCA
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures, StandardScaler
+from sklearn.linear_model import LinearRegression as linreg
+
 
 from fedot.core.constants import PCA_MIN_THRESHOLD_TS
 from fedot.core.data.data import InputData, OutputData, data_type_is_table
@@ -15,7 +20,17 @@ from fedot.core.operations.evaluation.operation_implementations. \
     implementation_interfaces import DataOperationImplementation, EncodedInvariantImplementation
 from fedot.core.operations.operation_parameters import OperationParameters
 from fedot.core.repository.dataset_types import DataTypesEnum
+from fedot.core.repository.tasks import TaskTypesEnum
 from fedot.preprocessing.data_types import TYPE_TO_ID
+
+
+BAGGING_METHOD = {
+    'mean': np.mean,
+    'median': np.median,
+    'max': np.max,
+    'min': np.min,
+    'weighted': linreg
+}
 
 
 class ComponentAnalysisImplementation(DataOperationImplementation):
@@ -50,12 +65,13 @@ class ComponentAnalysisImplementation(DataOperationImplementation):
             # TODO: remove a workaround by refactoring other operations in troubled pipelines (e.g. topo)
             # workaround for NaN-containing arrays during pca fitting, especially for fast_ica
             # fast_ica cannot fit with features represented by a rather sparse matrix
+            features = self.preprocess_input(input_data)
             try:
-                self.pca.fit(input_data.features)
+                self.pca.fit(features)
             except Exception as e:
                 self.log.info(f'Switched from {type(self.pca).__name__} to default PCA on fit stage due to {e}')
                 self.pca = PCA()
-                self.pca.fit(input_data.features)
+                self.pca.fit(features)
 
         return self.pca
 
@@ -69,16 +85,26 @@ class ComponentAnalysisImplementation(DataOperationImplementation):
         Returns:
             data with transformed features attribute
         """
+        features = self.preprocess_input(input_data)
 
         if self.number_of_features > 1:
-            transformed_features = self.pca.transform(input_data.features)
+            transformed_features = self.pca.transform(features)
         else:
-            transformed_features = input_data.features
+            transformed_features = features
 
         # Update features
+        transformed_features = self.postprocess_input(transformed_features)
         output_data = self._convert_to_output(input_data, transformed_features)
         self.update_column_types(output_data)
         return output_data
+
+    @staticmethod
+    def preprocess_input(input_data: InputData) -> Union[np.ndarray, pd.DataFrame]:
+        return input_data.features
+
+    @staticmethod
+    def postprocess_input(transformed_features: Union[np.ndarray, pd.DataFrame]) -> Union[np.ndarray, pd.DataFrame]:
+        return transformed_features
 
     def check_and_correct_params(self, is_ts_data: bool = False):
         """
@@ -92,7 +118,10 @@ class ComponentAnalysisImplementation(DataOperationImplementation):
         elif n_components == 'mle':
             # Check that n_samples correctly map with n_features
             if self.number_of_samples < self.number_of_features:
-                self.params.update(n_components=0.5)
+                if not isinstance(self.pca, DaskPCA):
+                    self.params.update(n_components=0.5)
+                else:
+                    self.params.update(n_components=None)
         if is_ts_data and (n_components * self.number_of_features) < PCA_MIN_THRESHOLD_TS:
             self.params.update(n_components=PCA_MIN_THRESHOLD_TS / self.number_of_features)
 
@@ -125,6 +154,38 @@ class PCAImplementation(ComponentAnalysisImplementation):
             self.params.update(**default_params)
         self.pca = PCA(**self.params.to_dict())
         self.number_of_features = None
+
+
+class DaskPCAImplementation(ComponentAnalysisImplementation):
+    """
+    Class for applying PCA from sklearn
+
+    Args:
+        params: OperationParameters with the hyperparameters
+    """
+
+    def __init__(self, params: Optional[OperationParameters] = None):
+        super().__init__(params)
+        if not self.params:
+            # Default parameters
+            default_params = {'svd_solver': 'full', 'n_components': 'mle'}
+            self.params.update(**default_params)
+        self.pca = DaskPCA(**self.params.to_dict())
+        self.number_of_features = None
+
+    @staticmethod
+    def preprocess_input(input_data: InputData) -> Union[np.ndarray, pd.DataFrame]:
+        features = input_data.features
+        if isinstance(input_data.features, pd.DataFrame):
+            features = features.values
+        features = check_array(features, accept_sparse=True, dtype=None)
+        if not isinstance(features, da.Array):
+            features = da.array(features)
+        return features
+
+    @staticmethod
+    def postprocess_input(transformed_features):
+        return transformed_features.compute()
 
 
 class KernelPCAImplementation(ComponentAnalysisImplementation):
@@ -433,3 +494,72 @@ class ImputationImplementation(DataOperationImplementation):
         features_imputers = {'imputer_categorical': self.params_cat,
                              'imputer_numerical': self.params_num}
         return OperationParameters(**features_imputers)
+
+
+class BaggingEnsemble(DataOperationImplementation):
+    """Class ensemble predictions.
+
+    """
+
+    def __init__(self, params: Optional[OperationParameters] = None):
+        super().__init__(params)
+        self.bagging_method = params.get('method', 'weighted')
+        self.stride = params.get('stride', 1)
+        self.add_global_features = params.get('add_global_features', True)
+        self.is_linreg_ensemble = self.bagging_method.__contains__('weighted')
+
+    def __repr__(self):
+        return 'Statistical Class for TS representation'
+
+    def _preprocess_predict_for_task(self, input_data):
+        self.is_clf_task = input_data.task.task_type == TaskTypesEnum.classification
+        is_forecasting_task = input_data.task.task_type == TaskTypesEnum.ts_forecasting
+        is_target_table = len(input_data.target.shape) > 1
+        self.is_clf_task_with_regression_ensemble = all([self.is_linreg_ensemble,
+                                                         self.is_clf_task])
+        is_forecasting_task_with_lagged_target = all([is_target_table,
+                                                      is_forecasting_task])
+        if is_forecasting_task_with_lagged_target:
+            # take last column from target (horizon)
+            input_data.target = input_data.target[-1]
+        if self.is_clf_task_with_regression_ensemble:
+            # concatenate probs output from different models
+            input_data.features = input_data.features.reshape(input_data.features.shape[0], -1)
+
+        return input_data
+
+    def fit(self, input_data: InputData):
+        """ Method fit model on a dataset
+
+        :param input_data: data with features, target and ids to process
+        """
+        input_data = self._preprocess_predict_for_task(input_data)
+        if self.is_linreg_ensemble:
+            self.method_impl = BAGGING_METHOD[self.bagging_method]()
+            self.method_impl.fit(input_data.features, input_data.target)
+        else:
+            self.method_impl = BAGGING_METHOD[self.bagging_method]
+        return self
+
+    def transform(self, input_data: InputData) -> OutputData:
+        """ Method make prediction
+
+        :param input_data: data with features, target and ids to process
+        """
+        input_data = self._preprocess_predict_for_task(input_data)
+        if not self.is_linreg_ensemble:
+            ensembled_predict = self.method_impl(input_data.features, axis=1)
+        else:
+            ensembled_predict = self.method_impl.predict(input_data.features)
+        ensembled_predict = np.round(abs(ensembled_predict)) \
+            if self.is_clf_task_with_regression_ensemble else ensembled_predict
+        return ensembled_predict
+
+    def transform_for_fit(self, input_data: InputData) -> OutputData:
+        """ Method make prediction while graph fitting.
+        Allows to implement predict method different from main predict method
+        if another behaviour for fit graph stage is needed.
+
+        :param input_data: data with features, target and ids to process
+        """
+        return self.transform(input_data)
