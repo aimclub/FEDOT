51f23ebd73f30ac9fa50027f50bf0d64f2360189
DRMPN <mr.gorden.smit@gmail.com>
2025-04-08 17:00:08 +0300

    chore: add use_stats parameter to examples

--- examples/simple/classification/api_classification.py
+++ examples/simple/classification/api_classification.py
@@ -2,19 +2,23 @@ from fedot import Fedot
 from fedot.core.utils import fedot_project_root, set_random_seed
 
 
-def run_classification_example(timeout: float = None, visualization=False, with_tuning=True):
+def run_classification_example(
+        timeout: float = None, visualization: bool = False, with_tuning: bool = True, preset: str = "best_quality",
+        use_stats: bool = False):
     problem = 'classification'
     train_data_path = f'{fedot_project_root()}/examples/real_cases/data/scoring/scoring_train.csv'
     test_data_path = f'{fedot_project_root()}/examples/real_cases/data/scoring/scoring_test.csv'
 
-    baseline_model = Fedot(problem=problem, timeout=timeout)
+    baseline_model = Fedot(problem=problem, timeout=timeout, use_stats=use_stats)
     baseline_model.fit(features=train_data_path, target='target', predefined_model='rf')
 
     baseline_model.predict(features=test_data_path)
     print(baseline_model.get_metrics())
 
-    auto_model = Fedot(problem=problem, timeout=timeout, n_jobs=-1, preset='best_quality',
-                       max_pipeline_fit_time=5, metric=['roc_auc', 'precision'], with_tuning=with_tuning)
+    auto_model = Fedot(
+        problem=problem, timeout=timeout, n_jobs=-1, preset=preset, max_pipeline_fit_time=5,
+        metric=['roc_auc', 'precision'],
+        with_tuning=with_tuning, use_stats=use_stats)
     auto_model.fit(features=train_data_path, target='target')
     prediction = auto_model.predict_proba(features=test_data_path)
 
