b79c8d1a65326d1eb4141529e8c0ba61fd75ec6f
martilut <jeykolton@gmail.com>
2025-05-14 12:56:32 +0300

    Added TabPFN Strategy

---
+++ fedot/core/operations/evaluation/tabpfn.py
@@ -0,0 +1,67 @@
+from typing import Optional
+
+import numpy as np
+
+from fedot.core.data.data import InputData, OutputData
+from fedot.core.operations.evaluation.evaluation_interfaces import EvaluationStrategy, is_multi_output_task
+from fedot.core.operations.evaluation.operation_implementations.models.tabpfn import \
+    FedotTabPFNClassificationImplementation, FedotTabPFNRegressionImplementation
+from fedot.core.operations.operation_parameters import OperationParameters
+from fedot.core.repository.tasks import TaskTypesEnum
+from fedot.utilities.random import ImplementationRandomStateHandler
+
+
+class TabPFNStrategy(EvaluationStrategy):
+    _operations_by_types = {
+        'tabpfn': FedotTabPFNClassificationImplementation,
+        'tabpfnreg': FedotTabPFNRegressionImplementation,
+    }
+
+    def __init__(self, operation_type: str, params: Optional[OperationParameters] = None):
+        self.operation_impl = self._convert_to_operation(operation_type)
+        super().__init__(operation_type, params)
+
+    def fit(self, train_data: InputData):
+        if train_data.task.task_type == TaskTypesEnum.ts_forecasting:
+            raise ValueError('Time series forecasting not supported for TabPFN')
+
+        operation_implementation = self.operation_impl(self.params_for_fit)
+
+        with ImplementationRandomStateHandler(implementation=operation_implementation):
+            operation_implementation.fit(train_data)
+
+        return operation_implementation
+
+    def predict(self, trained_operation, predict_data: InputData) -> OutputData:
+        raise NotImplementedError()
+
+
+class TabPFNClassificationStrategy(TabPFNStrategy):
+    def __init__(self, operation_type: str, params: Optional[OperationParameters] = None):
+        super().__init__(operation_type, params)
+
+    def predict(self, trained_operation, predict_data: InputData) -> OutputData:
+        if self.output_mode == 'labels':
+            prediction = trained_operation.predict(predict_data)
+        elif self.output_mode in ['probs', 'full_probs', 'default']:
+            n_classes = len(trained_operation.classes_)
+            prediction = trained_operation.predict_proba(predict_data)
+            if n_classes < 2:
+                raise ValueError('Data set contain only 1 target class. Please reformat your data.')
+            elif n_classes == 2 and self.output_mode != 'full_probs' and len(prediction.shape) > 1:
+                prediction = prediction[:, 1]
+        else:
+            raise ValueError(f'Output model {self.output_mode} is not supported')
+
+        converted = self._convert_to_output(prediction, predict_data)
+        return converted
+
+
+class  TabPFNRegressionStrategy(TabPFNStrategy):
+    def __init__(self, operation_type: str, params: Optional[OperationParameters] = None):
+        super().__init__(operation_type, params)
+
+    def predict(self, trained_operation, predict_data: InputData) -> OutputData:
+        prediction = trained_operation.predict(predict_data)
+        converted = self._convert_to_output(prediction, predict_data)
+        return converted
