2684211862ea4ff8e5652bafc3080c88b0ba75fd
Artem Lunev <artem45689@mail.ru>
2026-04-13 07:19:11 +0300

    feat: add tabicl model

--- fedot/core/pipelines/tuning/search_space.py
+++ fedot/core/pipelines/tuning/search_space.py
@@ -969,6 +969,50 @@ class PipelineSearchSpace(SearchSpace):
                     'sampling-scope': [[True, False]],
                     'type': 'categorical'
                 },
+            },
+            'tabicl': {
+                'n_estimators': {
+                    'hyperopt-dist': hp.uniformint,
+                    'sampling-scope': [1, 16],
+                    'type': 'discrete'
+                },
+                'softmax_temperature': {
+                    'hyperopt-dist': hp.uniform,
+                    'sampling-scope': [0.0, 1.0],
+                    'type': 'continuous'
+                },
+                'average_logits': {
+                    'hyperopt-dist': hp.choice,
+                    'sampling-scope': [[True, False]],
+                    'type': 'categorical'
+                },
+                'outlier_threshold': {
+                    'hyperopt-dist': hp.uniform,
+                    'sampling-scope': [2.0, 6.0],
+                    'type': 'continuous'
+                },
+            },
+            'tabiclreg': {
+                'n_estimators': {
+                    'hyperopt-dist': hp.uniformint,
+                    'sampling-scope': [1, 16],
+                    'type': 'discrete'
+                },
+                'softmax_temperature': {
+                    'hyperopt-dist': hp.uniform,
+                    'sampling-scope': [0.0, 1.0],
+                    'type': 'continuous'
+                },
+                'average_logits': {
+                    'hyperopt-dist': hp.choice,
+                    'sampling-scope': [[True, False]],
+                    'type': 'categorical'
+                },
+                'outlier_threshold': {
+                    'hyperopt-dist': hp.uniform,
+                    'sampling-scope': [2.0, 6.0],
+                    'type': 'continuous'
+                },
             }
         }
 
