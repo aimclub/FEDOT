1d88b0c8cc5d1bc20e908fea2245bc7261532002
Nikolay Nikitin <nicl.nno@gmail.com>
2026-09-09 12:32:35 +0300

    docs: use lightweight Read the Docs environment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,7 +97,22 @@ autodoc_default_options = {
 autoclass_content = 'class'
 autodoc_typehints = 'description'
 autodoc_typehints_format = 'short'
-autodoc_mock_imports = ['objgraph', 'memory_profiler', 'gprof2dot', 'snakeviz', 'h2o', 'tpot']
+autodoc_mock_imports = [
+    'cv2',
+    'gensim',
+    'gprof2dot',
+    'gph',
+    'h2o',
+    'memory_profiler',
+    'nltk',
+    'objgraph',
+    'ripser',
+    'snakeviz',
+    'tabpfn',
+    'tensorflow',
+    'torch',
+    'tpot',
+]
 python_use_unqualified_type_names = True
 
 intersphinx_mapping = {
